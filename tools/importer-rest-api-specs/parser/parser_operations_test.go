770af2d318c63590ecdecb2505b2be276baa3502
tombuildsstuff <git@ibuildstuff.co.uk>
2021-05-20 11:15:51 +0200

    r/importer-rest-api-specs/parser: implementing TestParseOperationSingleWithNoTag

--- tools/importer-rest-api-specs/parser/parser_operations_test.go
+++ tools/importer-rest-api-specs/parser/parser_operations_test.go
@@ -666,8 +666,72 @@ func TestParseOperationSingleWithMultipleTags(t *testing.T) {
 }
 
 func TestParseOperationSingleWithNoTag(t *testing.T) {
-	// TODO: a placeholder for now
-	t.Fail()
+	parsed, err := Load("testdata/", "operations_single_with_no_tag.json", true)
+	if err != nil {
+		t.Fatalf("loading: %+v", err)
+	}
+
+	result, err := parsed.Parse("Example", "2020-01-01")
+	if err != nil {
+		t.Fatalf("parsing: %+v", err)
+	}
+	if result == nil {
+		t.Fatal("result was nil")
+	}
+	if len(result.Resources) != 1 {
+		t.Fatalf("expected 1 resource but got %d", len(result.Resources))
+	}
+
+	// this should get parsed out from the OperationId into the tag Hello
+	hello, ok := result.Resources["Hello"]
+	if !ok {
+		t.Fatalf("no resources were output with the tag Hello")
+	}
+
+	if len(hello.Constants) != 0 {
+		t.Fatalf("expected no Constants but got %d", len(hello.Constants))
+	}
+	if len(hello.Models) != 0 {
+		t.Fatalf("expected no Models but got %d", len(hello.Models))
+	}
+	if len(hello.Operations) != 1 {
+		t.Fatalf("expected 1 Operation but got %d", len(hello.Operations))
+	}
+	if len(hello.ResourceIds) != 0 {
+		t.Fatalf("expected no ResourceIds but got %d", len(hello.ResourceIds))
+	}
+
+	world, ok := hello.Operations["HeadWorld"]
+	if !ok {
+		t.Fatalf("no resources were output with the name HeadWorld")
+	}
+	if world.Method != "HEAD" {
+		t.Fatalf("expected a HEAD operation but got %q", world.Method)
+	}
+	if len(world.ExpectedStatusCodes) != 1 {
+		t.Fatalf("expected 1 status code but got %d", len(world.ExpectedStatusCodes))
+	}
+	if world.ExpectedStatusCodes[0] != 200 {
+		t.Fatalf("expected the status code to be 200 but got %d", world.ExpectedStatusCodes[0])
+	}
+	if world.RequestObjectName != nil {
+		t.Fatalf("expected no request object but got %q", *world.RequestObjectName)
+	}
+	if world.ResponseObjectName != nil {
+		t.Fatalf("expected no response object but got %q", *world.ResponseObjectName)
+	}
+	if world.ResourceIdName != nil {
+		t.Fatalf("expected no ResourceId but got %q", *world.ResourceIdName)
+	}
+	if world.UriSuffix == nil {
+		t.Fatal("expected world.UriSuffix to have a value")
+	}
+	if *world.UriSuffix != "/things" {
+		t.Fatalf("expected world.UriSuffix to be `/things` but got %q", *world.UriSuffix)
+	}
+	if world.LongRunning {
+		t.Fatal("expected a non-long running operation but it was long running")
+	}
 }
 
 func TestParseOperationMultipleBasedOnTheSameResourceId(t *testing.T) {
