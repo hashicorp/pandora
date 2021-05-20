9a07ec2ede0238a646a0d3bbbe09a1c45c901ff6
Tom Harvey <tombuildsstuff@users.noreply.github.com>
2021-05-20 10:53:53 +0200

    Update tools/importer-rest-api-specs/parser/parser_constants_test.go
    Co-authored-by: Steve <11830746+jackofallops@users.noreply.github.com>

--- tools/importer-rest-api-specs/parser/parser_constants_test.go
+++ tools/importer-rest-api-specs/parser/parser_constants_test.go
@@ -287,7 +287,7 @@ func TestParseConstantsIntegersInlinedAsInts(t *testing.T) {
 	}
 	v, ok = favouriteTable.Values["3"]
 	if !ok {
-		t.Fatalf("resource.Constants['TableNumber'] didn't contain the key 'Panda'")
+		t.Fatalf("expected the value for resource.Constants['TableNumber'].Values['3'] to be '3' but got %q", v)
 	}
 	if v != "3" {
 		t.Fatalf("expected the value for resource.Constants['TableNumber'].Values['3'] to be '3' but got %q", v)
