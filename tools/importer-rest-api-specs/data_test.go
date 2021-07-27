a17dd5b53b6412df0168dc084044108223ae97d0
tombuildsstuff <git@ibuildstuff.co.uk>
2021-07-27 12:14:42 +0200

    tools/importer-rest-api-specs: adding a test to check the existing data can be generated
    This is useful when the Swagger updates and we want to confirm the existing data is
    still valid

---
+++ tools/importer-rest-api-specs/data_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestExistingDataCanBeGenerated(t *testing.T) {
+	input := GenerationData()
+
+	swaggerGitSha, err := determineGitSha(swaggerDirectory)
+	if err != nil {
+		t.Fatalf("determining Git SHA at %q: %+v", swaggerDirectory, err)
+	}
+
+	for _, data := range input {
+		t.Run(fmt.Sprintf("%s-%s", data.ServiceName, data.ApiVersion), func(t *testing.T) {
+			generationData := data
+			if err := run(generationData, *swaggerGitSha); err != nil {
+				t.Fatalf("error: %+v", err)
+			}
+		})
+	}
+}
