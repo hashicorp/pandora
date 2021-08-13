b878b41a94ce24c31b6f42eb4dd9113ca9605a98
tombuildsstuff <git@ibuildstuff.co.uk>
2021-08-13 10:03:32 +0200

    data: removing the data plane placeholder data
    This isn't used right now

--- data/Pandora.Definitions.DataPlane/AppConfiguration/v1_0/KeyValues/ResourceId-KeyValueId.cs
+++
@@ -1,9 +0,0 @@
-using Pandora.Definitions.Interfaces;
-
-namespace Pandora.Definitions.DataPlane.AppConfiguration.v1_0.KeyValues
-{
-    internal class KeyValueId : ResourceID
-    {
-        public string ID() => "/kv/{key}";
-    }
-}
