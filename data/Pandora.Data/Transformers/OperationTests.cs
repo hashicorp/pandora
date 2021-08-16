5826b4cd8b432680d7e05b9ae10781806f69a652
tombuildsstuff <git@ibuildstuff.co.uk>
2021-08-16 12:10:57 +0200

    data: updating the test to check properties by name rather than assuming the order

--- data/Pandora.Data/Transformers/OperationTests.cs
+++ data/Pandora.Data/Transformers/OperationTests.cs
@@ -178,14 +178,16 @@ namespace Pandora.Data.Transformers
             Assert.AreEqual(1, actual.ExpectedStatusCodes.Count);
             Assert.AreEqual(200, actual.ExpectedStatusCodes.First());
             Assert.AreEqual(2, actual.Options.Count);
-            Assert.AreEqual("First", actual.Options[0].Name);
-            Assert.AreEqual(true, actual.Options[0].Required);
-            Assert.AreEqual(OptionDefinitionType.String, actual.Options[0].Type);
-            Assert.AreEqual("first", actual.Options[0].QueryStringName);
-            Assert.AreEqual("SecondVal", actual.Options[1].Name);
-            Assert.AreEqual(false, actual.Options[1].Required);
-            Assert.AreEqual(OptionDefinitionType.Integer, actual.Options[1].Type);
-            Assert.AreEqual("second", actual.Options[1].QueryStringName);
+
+            var firstOption = actual.Options.First(o => o.Name == "First");
+            Assert.AreEqual(true, firstOption.Required);
+            Assert.AreEqual(OptionDefinitionType.String, firstOption.Type);
+            Assert.AreEqual("first", firstOption.QueryStringName);
+            
+            var secondOption = actual.Options.First(o => o.Name == "SecondVal");
+            Assert.AreEqual(false, secondOption.Required);
+            Assert.AreEqual(OptionDefinitionType.Integer, secondOption.Type);
+            Assert.AreEqual("second", secondOption.QueryStringName);
         }
 
         [TestCase]
