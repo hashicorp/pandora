2a65e936b4127977b1578505621c9b83f1dfaafb
tombuildsstuff <git@ibuildstuff.co.uk>
2021-10-11 14:22:07 +0200

    tools/importer-rest-api-specs: DynamicsTelemetry is a Fake RP, so we can ignore it
    Ctx: https://github.com/Azure/azure-rest-api-specs/pull/5161#issuecomment-486705231

--- tools/importer-rest-api-specs/parser/load_and_parse.go
+++ tools/importer-rest-api-specs/parser/load_and_parse.go
@@ -116,10 +116,11 @@ func writeToFile(metadata map[string]resourceUriMetadata) {
 
 func serviceShouldBeIgnored(name string) bool {
 	servicesToIgnore := []string{
-		"Blockchain",        // https://github.com/Azure-Samples/blockchain/blob/1b712d6d05cca8da17bdd1894de8c3d25905685d/abs/migration-guide.md
-		"DevSpaces",         // https://azure.microsoft.com/en-us/updates/azure-dev-spaces-is-retiring-on-31-october-2023/
-		"IoTSpaces",         // https://github.com/Azure/azure-rest-api-specs/pull/13993
-		"ServiceFabricMesh", // https://azure.microsoft.com/en-us/updates/azure-service-fabric-mesh-preview-retirement/
+		"Blockchain",        // EOL - https://github.com/Azure-Samples/blockchain/blob/1b712d6d05cca8da17bdd1894de8c3d25905685d/abs/migration-guide.md
+		"DevSpaces",         // EOL - https://azure.microsoft.com/en-us/updates/azure-dev-spaces-is-retiring-on-31-october-2023/
+		"DynamicsTelemetry", // Fake RP - https://github.com/Azure/azure-rest-api-specs/pull/5161#issuecomment-486705231
+		"IoTSpaces",         // EOL - https://github.com/Azure/azure-rest-api-specs/pull/13993
+		"ServiceFabricMesh", // EOL - https://azure.microsoft.com/en-us/updates/azure-service-fabric-mesh-preview-retirement/
 	}
 	for _, v := range servicesToIgnore {
 		if strings.EqualFold(name, v) {
