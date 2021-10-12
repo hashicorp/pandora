90b0d0f4389280afc8872d14ba47301385bd547e
tombuildsstuff <git@ibuildstuff.co.uk>
2021-10-12 12:49:16 +0200

    tools/importer-rest-api-specs: ignoring ADHybridHealthService for now

--- tools/importer-rest-api-specs/parser/load_and_parse.go
+++ tools/importer-rest-api-specs/parser/load_and_parse.go
@@ -116,6 +116,7 @@ func writeToFile(metadata map[string]resourceUriMetadata) {
 
 func serviceShouldBeIgnored(name string) bool {
 	servicesToIgnore := []string{
+		"ADHybridHealthService", // TODO: is this EOL? Contains a Constant of an empty string: https://github.com/Azure/azure-rest-api-specs/blob/3eaa729b3686f20817145e771a8ab707c739dbbd/specification/adhybridhealthservice/resource-manager/Microsoft.ADHybridHealthService/stable/2014-01-01/ADHybridHealthService.json#L460-L471
 		"Blockchain",        // EOL - https://github.com/Azure-Samples/blockchain/blob/1b712d6d05cca8da17bdd1894de8c3d25905685d/abs/migration-guide.md
 		"DevSpaces",         // EOL - https://azure.microsoft.com/en-us/updates/azure-dev-spaces-is-retiring-on-31-october-2023/
 		"DynamicsTelemetry", // Fake RP - https://github.com/Azure/azure-rest-api-specs/pull/5161#issuecomment-486705231
