43e9f929d55412a71b6c4d28fe86aeaca62010de
tombuildsstuff <git@ibuildstuff.co.uk>
2021-10-18 12:00:45 +0200

    automation: automatically regenerating the Config when the Swagger changes
    This commit adds a step to regenerate the Config whenever changes to the Swagger
    data get merged - which means that we'll automatically generate new Versions and
    Services when the feature-flags in those tools are enabled.
    
    Dependent on / must be rebased on top of #372

--- scripts/conditionally-commit-codegen-changes.sh
+++ scripts/conditionally-commit-codegen-changes.sh
@@ -2,7 +2,7 @@
 
 if [[ $(git status --porcelain | wc -l) -gt 0 ]]; then
   git add --all
-  git commit -m "data: regenerating based on the latest Swagger"
+  git commit -m "$1"
   echo "::set-output name=has_changes_to_push::true"
 else
   echo "::set-output name=has_changes_to_push::false"
