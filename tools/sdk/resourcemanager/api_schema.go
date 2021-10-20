9f6d0399f83f123aac3ffd6a81e3d040b9aed498
Kyriakos Oikonomakos <kyriakos@hashicorp.com>
2021-10-20 07:25:41 +0100

    remove dead code

--- tools/sdk/resourcemanager/api_schema.go
+++ tools/sdk/resourcemanager/api_schema.go
@@ -40,26 +40,6 @@ type ApiSchemaDetails struct {
 	ResourceIds map[string]ResourceIdDefinition `json:"resourceIds"`
 }
 
-type ResourceConstantDetails struct {
-	Values    map[string]string `json:"values"`
-	FieldType ConstantType      `json:"field_type"`
-}
-
-type ConstantFieldType string
-
-type ResourceId struct {
-	// Constants are a map[Name]ResourceConstantDetails for the Constants used in this Resource ID
-	Constants map[string]ResourceConstantDetails `json:"constants"`
-
-	// Segments are an ordered list of segments which comprise this Resource ID
-	Segments []ResourceIdSegment `json:"segments"`
-
-	// Name holds the resource's name in the parser code. Should be in Title Case.
-	Name string `json:"name"`
-}
-
-type SegmentType string
-
 type ConstantDetails struct {
 	// CaseInsensitive specifies that this Constant can be returned insensitively
 	// and should be rewritten on the Client side to be consistent
