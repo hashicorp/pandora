129610323bad6188c02b2af81d3c10584543919f
tombuildsstuff <git@tom.ibuildstuff.eu>
2021-12-15 09:32:17 +0100

    tooling: introducing a wrapper project to launch and run the Go SDK Generator
    This'll be used when running in automation, where the Go SDK will be published to it's own repository,
    the Bash file is primarily to configure the environment so this can be run locally as well as (in the
    future) run in CI.

---
+++ scripts/automation-generate-go-sdk.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function main {
+  local dataApiAssemblyPath="data/Pandora.Api/bin/Debug/net6.0/Pandora.Api.dll"
+  local outputDirectory="generated/" # TODO: replace with the submodule in time
+
+  echo "Installing the Go SDK Generator into the GOBIN.."
+  cd ./tools/generator-go-sdk
+  go install .
+  cd ../../
+
+  echo "Building Wrapper.."
+  cd ./tools/wrapper-go-sdk-generator
+  go build -o wrapper-go-sdk-generator
+
+  echo "Running Wrapper.."
+  ./wrapper-go-sdk-generator \
+    -data-api-assembly-path="../../$dataApiAssemblyPath"\
+    -output-dir="../../$outputDirectory"
+}
+
+main
