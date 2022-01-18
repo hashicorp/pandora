04172a278cc6140fbbf827a9282a636dc1c1c4c0
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-01-18 15:47:44 +0100

    tools/importer-rest-api-specs: scaffolding the existing data differ
    This is intended to allow us to diff API Definitions within the Data API when doing an Import to check
    for any fields which have been renamed, are breaking changes etc.
    
    Whilst a placeholder, in time this'll allow us to hook this up end-to-end - this is intentionally not
    feature toggled since this functionality is opt-in (by providing a URI to the Data API) so disabled
    by default.

---
+++ tools/importer-rest-api-specs/differ/stages.go
@@ -0,0 +1,14 @@
+package differ
+
+import "github.com/hashicorp/pandora/tools/importer-rest-api-specs/parser"
+
+type ApplyStage interface {
+	Apply(existing parser.ParsedData, parsed parser.ParsedData) (*parser.ParsedData, error)
+}
+
+var ApplyStages = []ApplyStage{
+	// TODO: DefaultImpliedValuesStage{}, - where there's a default implied value in the API
+	// TODO: DetectBreakingChangesStage{},
+	// TODO: FieldRenamerStage{}, - handling where a field has a different Name and JsonName (casing-aside)
+	// TODO: ResourceIdRenamerStage{}, - handling where a ResourceID has been renamed
+}
