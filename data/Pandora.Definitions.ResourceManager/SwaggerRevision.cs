2e2e8e5ad7a5dbf1b8d178f6a70d7f3bba041ad4
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-01-19 11:19:04 +0100

    data/Pandora.Definitions.ResourceManager: refactoring to use file-based namespaces

--- data/Pandora.Definitions.ResourceManager/SwaggerRevision.cs
+++ data/Pandora.Definitions.ResourceManager/SwaggerRevision.cs
@@ -1,4 +1,3 @@
-namespace Pandora.Definitions.ResourceManager
-{
-    // Generated from Swagger revision "8a061f1e9031450b9eb5546d242f2a28c93eaa69"
-}
+namespace Pandora.Definitions.ResourceManager;
+
+// Generated from Swagger revision "8a061f1e9031450b9eb5546d242f2a28c93eaa69"
