279614724b73a4821bc32ba9abbd85128a19cd3d
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-04-23 10:34:16 +0200

    tools/generator-go-sdk: splitting the version stage and templater

--- tools/generator-go-sdk/generator/stage_version.go
+++ tools/generator-go-sdk/generator/stage_version.go
@@ -9,22 +9,3 @@ func (s *ServiceGenerator) version(data ServiceGeneratorData) error {
 
 	return nil
 }
-
-var _ templater = versionTemplater{}
-
-type versionTemplater struct {
-}
-
-func (c versionTemplater) template(data ServiceGeneratorData) (*string, error) {
-	template := fmt.Sprintf(`package %[1]s
-
-import "fmt"
-
-const defaultApiVersion = "%[2]s"
-
-func userAgent() string {
-	return fmt.Sprintf("pandora/%[1]s/%%s", defaultApiVersion)
-}
-`, data.packageName, data.apiVersion)
-	return &template, nil
-}
---
+++ tools/generator-go-sdk/generator/templater_version.go
@@ -0,0 +1,22 @@
+package generator
+
+import "fmt"
+
+var _ templater = versionTemplater{}
+
+type versionTemplater struct {
+}
+
+func (c versionTemplater) template(data ServiceGeneratorData) (*string, error) {
+	template := fmt.Sprintf(`package %[1]s
+
+import "fmt"
+
+const defaultApiVersion = "%[2]s"
+
+func userAgent() string {
+	return fmt.Sprintf("pandora/%[1]s/%%s", defaultApiVersion)
+}
+`, data.packageName, data.apiVersion)
+	return &template, nil
+}
