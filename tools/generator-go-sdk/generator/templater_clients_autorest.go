aad1fb327cfbbf74a6d01455c73e3e30ac187d38
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-04-23 10:32:37 +0200

    tools/go-sdk-generator: splitting the clients stage and templater

--- tools/generator-go-sdk/generator/templater_clients_autorest.go
+++ tools/generator-go-sdk/generator/templater_clients_autorest.go
@@ -2,14 +2,6 @@ package generator
 
 import "fmt"
 
-func (s *ServiceGenerator) clients(data ServiceGeneratorData) error {
-	if err := s.writeToPath(data.outputPath, "client.go", clientsTemplater{}, data); err != nil {
-		return fmt.Errorf("templating client: %+v", err)
-	}
-
-	return nil
-}
-
 var _ templater = clientsTemplater{}
 
 type clientsTemplater struct {
