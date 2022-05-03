eeb9401a69bd8fbdf9f1c5204d3fe38e12e1dd35
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-05-03 16:44:59 +0200

    automation: running `make fmt` on the generated output for consistency purposes

--- scripts/automation-generate-go-sdk.sh
+++ scripts/automation-generate-go-sdk.sh
@@ -25,6 +25,11 @@ function runWrapper {
   ./wrapper-go-sdk-generator \
     -data-api-assembly-path="../../$dataApiAssemblyPath"\
     -output-dir="../../$outputDirectory"
+
+  cd "${DIR}"
+  cd "${outputDirectory}"
+  echo "Running 'make fmt' on the generated code.."
+  make fmt
 }
 
 function prepareGoSdk {
@@ -48,11 +53,21 @@ function conditionallyCommitAndPushGoSdk {
   cd "$workingDirectory"
   if [[ $(git status --porcelain | wc -l) -gt 0 ]]; then
     echo "Committing and Pushing the changes"
+
+    # commit the generated changes
     git checkout -b "$branch"
     git config --global user.name "Github Actions"
     git config --global user.email "<>"
     git add --all
     git commit -m "Updating based on $sha"
+
+    # then update the dependencies
+    go mod tidy
+    if [[ $(git status --porcelain | wc -l) -gt 0 ]]; then
+      git add --all
+      git commit -m "Updating dependencies based on $sha"
+    fi
+
     # NOTE: we're intentionally force-pushing here in-case this PR is
     # open and other changes (e.g. to the generator) get included
     git push origin "$branch" -f
