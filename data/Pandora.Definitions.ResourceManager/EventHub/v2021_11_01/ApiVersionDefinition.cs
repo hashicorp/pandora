794a994b1bb68f1424b303075d6a0793c8e4eb54
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-05-05 19:12:38 +0200

    data: updating the source for the new EventHub API version after rebasing

--- data/Pandora.Definitions.ResourceManager/EventHub/v2021_11_01/ApiVersionDefinition.cs
+++ data/Pandora.Definitions.ResourceManager/EventHub/v2021_11_01/ApiVersionDefinition.cs
@@ -6,6 +6,7 @@ public partial class Definition : ApiVersionDefinition
 {
     public string ApiVersion => "2021-11-01";
     public bool Preview => false;
+    public Source Source => Source.ResourceManagerRestApiSpecs;
 
     public IEnumerable<ResourceDefinition> Resources => new List<ResourceDefinition>
     {
