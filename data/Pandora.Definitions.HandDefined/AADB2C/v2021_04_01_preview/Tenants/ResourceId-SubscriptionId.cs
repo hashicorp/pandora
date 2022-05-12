f647bd3679c5d424aa853202ee990e89d6e73c3a
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-05-12 18:07:50 +0200

    data/HandDefined: switching to use the new helpers

--- data/Pandora.Definitions.HandDefined/AADB2C/v2021_04_01_preview/Tenants/ResourceId-SubscriptionId.cs
+++ data/Pandora.Definitions.HandDefined/AADB2C/v2021_04_01_preview/Tenants/ResourceId-SubscriptionId.cs
@@ -5,24 +5,12 @@ namespace Pandora.Definitions.HandDefined.AADB2C.v2021_04_01_preview.Tenants;
 
 internal class SubscriptionId : ResourceID
 {
-    public string? CommonAlias => null;
+    public string? CommonAlias => "Subscription";
 
     public string ID => "/subscriptions/{subscriptionId}";
 
     public List<ResourceIDSegment> Segments => new List<ResourceIDSegment>
     {
-        new()
-        {
-            Name = "subscriptions",
-            Type = ResourceIDSegmentType.Static,
-            FixedValue = "subscriptions"
-        },
-
-        new()
-        {
-            Name = "subscriptionId",
-            Type = ResourceIDSegmentType.SubscriptionId
-        },
 
     };
 }
