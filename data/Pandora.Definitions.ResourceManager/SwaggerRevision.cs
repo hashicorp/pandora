28cb87b7123e980e5f37c4a946d5839abd07870a
hc-github-team-tf-azure <>
2022-06-23 11:32:46 +0000

    data: regenerating based on the latest Swagger

--- data/Pandora.Definitions.ResourceManager/SwaggerRevision.cs
+++ data/Pandora.Definitions.ResourceManager/SwaggerRevision.cs
@@ -5,4 +5,4 @@ namespace Pandora.Definitions.ResourceManager;
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
 
 
-// Generated from Swagger revision "eb606ec7a7abadc78ded1423ddbea9e8f49e72c3"
+// Generated from Swagger revision "bf82b74acfa933165e8e30d3403195043d1149d5"
