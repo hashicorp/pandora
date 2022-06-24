ca28aa1d47274669a49d8d7116519d2f56e209a0
hc-github-team-tf-azure <>
2022-06-24 05:34:28 +0000

    data: regenerating based on the latest Swagger

---
+++ data/Pandora.Definitions.ResourceManager/ApplicationInsights/v2020_11_20/ApplicationInsights/Operation-WorkbookTemplatesCreateOrUpdate.cs
@@ -0,0 +1,25 @@
+using Pandora.Definitions.Attributes;
+using Pandora.Definitions.CustomTypes;
+using Pandora.Definitions.Interfaces;
+using Pandora.Definitions.Operations;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+
+namespace Pandora.Definitions.ResourceManager.ApplicationInsights.v2020_11_20.ApplicationInsights;
+
+internal class WorkbookTemplatesCreateOrUpdateOperation : Operations.PutOperation
+{
+    public override Type? RequestObject() => typeof(WorkbookTemplateModel);
+
+    public override ResourceID? ResourceId() => new WorkbooktemplateId();
+
+    public override Type? ResponseObject() => typeof(WorkbookTemplateModel);
+
+
+}
