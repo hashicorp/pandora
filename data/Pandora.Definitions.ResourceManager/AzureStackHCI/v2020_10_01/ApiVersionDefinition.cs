7750298c3264a585136b4259b701dcbcd7326dfd
hc-github-team-tf-azure <>
2022-06-30 05:23:35 +0000

    data: regenerating based on the latest Swagger

---
+++ data/Pandora.Definitions.ResourceManager/AzureStackHCI/v2020_10_01/ApiVersionDefinition.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using Pandora.Definitions.Interfaces;
+
+namespace Pandora.Definitions.ResourceManager.AzureStackHCI.v2020_10_01;
+public partial class Definition : ApiVersionDefinition
+{
+    public string ApiVersion => "2020-10-01";
+    public bool Preview => false;
+    public Source Source => Source.ResourceManagerRestApiSpecs;
+
+    public IEnumerable<ResourceDefinition> Resources => new List<ResourceDefinition>
+    {
+        new Clusters.Definition(),
+    };
+}
