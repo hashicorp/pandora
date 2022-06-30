36d5deaf4d980696d149d2f346592ff1341af435
hc-github-team-tf-azure <>
2022-06-30 05:39:09 +0000

    data: regenerating based on the latest Swagger

---
+++ data/Pandora.Definitions.ResourceManager/NetApp/v2021_10_01/Volumes/Constant-ReplicationSchedule.cs
@@ -0,0 +1,17 @@
+using Pandora.Definitions.Attributes;
+using System.ComponentModel;
+
+namespace Pandora.Definitions.ResourceManager.NetApp.v2021_10_01.Volumes;
+
+[ConstantType(ConstantTypeAttribute.ConstantType.String)]
+internal enum ReplicationScheduleConstant
+{
+    [Description("daily")]
+    Daily,
+
+    [Description("hourly")]
+    Hourly,
+
+    [Description("_10minutely")]
+    OneZerominutely,
+}
