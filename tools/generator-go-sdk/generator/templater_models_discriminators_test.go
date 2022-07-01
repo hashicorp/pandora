fcc4bc8bc44d49ec1cd3fbfa644e7285257dd1d7
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-07-01 17:39:53 +0100

    tools/generator-go-sdk: ensuring that types hidden within parent types are unmarshaled/assigned as required
    This commit fixes an issue where fields within a Parent Type wouldn't be
    either Assigned or Unmarshalled into the correct Implementation - by checking
    for fields within the Parents as required.

--- tools/generator-go-sdk/generator/templater_models_discriminators_test.go
+++ tools/generator-go-sdk/generator/templater_models_discriminators_test.go
@@ -236,3 +236,151 @@ func (s Train) MarshalJSON() ([]byte, error) {
 `, "''", "`")
 	assertTemplatedCodeMatches(t, expected, *actual)
 }
+
+func TestTemplaterModelsImplementationInheritedFromParentType(t *testing.T) {
+	actual, err := modelsTemplater{
+		name: "FirstImplementation",
+		model: resourcemanager.ModelDetails{
+			Fields: map[string]resourcemanager.FieldDetails{
+				"Serialization": {
+					Required: true,
+					JsonName: "serialization",
+					ObjectDefinition: resourcemanager.ApiObjectDefinition{
+						Type:          resourcemanager.ReferenceApiObjectDefinitionType,
+						ReferenceName: stringPointer("Serialization"),
+					},
+				},
+			},
+			ParentTypeName: stringPointer("First"),
+			TypeHintIn:     stringPointer("Type"),
+			TypeHintValue:  stringPointer("first"),
+		},
+	}.template(ServiceGeneratorData{
+		packageName: "somepackage",
+		models: map[string]resourcemanager.ModelDetails{
+			"First": {
+				Fields: map[string]resourcemanager.FieldDetails{
+					"Type": {
+						Required: true,
+						JsonName: "type",
+						ObjectDefinition: resourcemanager.ApiObjectDefinition{
+							Type: resourcemanager.StringApiObjectDefinitionType,
+						},
+					},
+				},
+			},
+			"FirstImplementation": {
+				Fields: map[string]resourcemanager.FieldDetails{
+					"Serialization": {
+						Required: true,
+						JsonName: "serialization",
+						ObjectDefinition: resourcemanager.ApiObjectDefinition{
+							Type:          resourcemanager.ReferenceApiObjectDefinitionType,
+							ReferenceName: stringPointer("Serialization"),
+						},
+					},
+				},
+				ParentTypeName: stringPointer("First"),
+				TypeHintIn:     stringPointer("Type"),
+				TypeHintValue:  stringPointer("first"),
+			},
+			"Serialization": {
+				Fields: map[string]resourcemanager.FieldDetails{
+					"Type": {
+						Required: true,
+						JsonName: "type",
+						ObjectDefinition: resourcemanager.ApiObjectDefinition{
+							Type: resourcemanager.StringApiObjectDefinitionType,
+						},
+					},
+				},
+				TypeHintIn: stringPointer("Type"),
+			},
+			"JsonSerialization": {
+				Fields: map[string]resourcemanager.FieldDetails{
+					"Example": {
+						Required: true,
+						JsonName: "example",
+						ObjectDefinition: resourcemanager.ApiObjectDefinition{
+							Type: resourcemanager.StringApiObjectDefinitionType,
+						},
+					},
+				},
+				ParentTypeName: stringPointer("Serialization"),
+				TypeHintIn:     stringPointer("Type"),
+				TypeHintValue:  stringPointer("json"),
+			},
+		},
+		source: AccTestLicenceType,
+	})
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	expected := strings.ReplaceAll(`package somepackage
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+	"time"
+	"github.com/hashicorp/go-azure-helpers/lang/dates"
+	"github.com/hashicorp/go-azure-helpers/resourcemanager/edgezones"
+	"github.com/hashicorp/go-azure-helpers/resourcemanager/identity"
+	"github.com/hashicorp/go-azure-helpers/resourcemanager/systemdata"
+)
+
+// acctests licence placeholder
+
+var _ First = FirstImplementation{}
+
+type FirstImplementation struct {
+	Serialization Serialization ''json:"serialization"''
+
+	// Fields inherited from First
+}
+
+var _ json.Marshaler = FirstImplementation{}
+
+func (s FirstImplementation) MarshalJSON() ([]byte, error) {
+	type wrapper FirstImplementation
+	wrapped := wrapper(s)
+	encoded, err := json.Marshal(wrapped)
+	if err != nil {
+		return nil, fmt.Errorf("marshaling FirstImplementation: %+v", err)
+	}
+
+	var decoded map[string]interface{}
+	if err := json.Unmarshal(encoded, &decoded); err != nil {
+		return nil, fmt.Errorf("unmarshaling FirstImplementation: %+v", err)
+	}
+	decoded["type"] = "first"
+
+	encoded, err = json.Marshal(decoded)
+	if err != nil {
+		return nil, fmt.Errorf("re-marshaling FirstImplementation: %+v", err)
+	}
+
+	return encoded, nil
+}
+
+var _ json.Unmarshaler = &FirstImplementation{}
+
+func (s *FirstImplementation) UnmarshalJSON(bytes []byte) error {
+	var temp map[string]json.RawMessage
+	if err := json.Unmarshal(bytes, &temp); err != nil {
+		return fmt.Errorf("unmarshaling FirstImplementation into map[string]json.RawMessage: %+v", err)
+	}
+
+	if v, ok := temp["serialization"]; ok {
+		impl, err := unmarshalSerializationImplementation(v)
+		if err != nil {
+			return fmt.Errorf("unmarshaling field 'Serialization' for 'FirstImplementation': %+v", err)
+		}
+		s.Serialization = impl
+	}
+
+	return nil
+}
+`, "''", "`")
+	assertTemplatedCodeMatches(t, expected, *actual)
+}
