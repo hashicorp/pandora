9a83812cc5658a982f6b5fed2badd2ccf7deff04
hc-github-team-tf-azure <>
2022-07-08 05:55:33 +0000

    data: regenerating based on the latest Swagger

---
+++ data/Pandora.Definitions.ResourceManager/PostgreSql/v2020_01_01/ApiVersionDefinition.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using Pandora.Definitions.Interfaces;
+
+namespace Pandora.Definitions.ResourceManager.PostgreSql.v2020_01_01;
+public partial class Definition : ApiVersionDefinition
+{
+    public string ApiVersion => "2020-01-01";
+    public bool Preview => false;
+    public Source Source => Source.ResourceManagerRestApiSpecs;
+
+    public IEnumerable<ResourceDefinition> Resources => new List<ResourceDefinition>
+    {
+        new ServerKeys.Definition(),
+    };
+}
---
+++ data/Pandora.Definitions.ResourceManager/PostgreSql/v2021_06_01/FirewallRules/ResourceId-FirewallRuleId.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using Pandora.Definitions.Interfaces;
+
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+
+namespace Pandora.Definitions.ResourceManager.PostgreSql.v2021_06_01.FirewallRules;
+
+internal class FirewallRuleId : ResourceID
+{
+    public string? CommonAlias => null;
+
+    public string ID => "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.DBForPostgreSql/flexibleServers/{serverName}/firewallRules/{firewallRuleName}";
+
+    public List<ResourceIDSegment> Segments => new List<ResourceIDSegment>
+    {
+        ResourceIDSegment.Static("staticSubscriptions", "subscriptions"),
+        ResourceIDSegment.SubscriptionId("subscriptionId"),
+        ResourceIDSegment.Static("staticResourceGroups", "resourceGroups"),
+        ResourceIDSegment.ResourceGroup("resourceGroupName"),
+        ResourceIDSegment.Static("staticProviders", "providers"),
+        ResourceIDSegment.ResourceProvider("staticMicrosoftDBForPostgreSql", "Microsoft.DBForPostgreSql"),
+        ResourceIDSegment.Static("staticFlexibleServers", "flexibleServers"),
+        ResourceIDSegment.UserSpecified("serverName"),
+        ResourceIDSegment.Static("staticFirewallRules", "firewallRules"),
+        ResourceIDSegment.UserSpecified("firewallRuleName"),
+    };
+}
