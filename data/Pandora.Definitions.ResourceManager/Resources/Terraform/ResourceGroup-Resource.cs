b72c22f7b9184bad38cb041963c1a46640182c60
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-07-15 13:37:21 +0200

    data: scaffolding the Terraform Resource Definition based on the fields we currently support
    This is going to need to be a 3-part switch since we need to add `TerraformResources` to the `ApiVersionDefinition`
    implementations and then add it to the interface.

---
+++ data/Pandora.Definitions.ResourceManager/Resources/Terraform/ResourceGroup-Resource.cs
@@ -0,0 +1,21 @@
+using Pandora.Definitions.Interfaces;
+
+namespace Pandora.Definitions.ResourceManager.Resources.Terraform;
+
+public class ResourceGroupResource : TerraformResourceDefinition
+{
+    public string DisplayName => "Resource Group";
+    public ResourceID ResourceId => new v2020_06_01.ResourceGroups.ResourceGroupId();
+    public string ResourceLabel => "resource_group";
+    public string ResourceName => "ResourceGroup";
+    
+    public bool GenerateIDValidationFunction => true;
+    public bool GenerateSchema => true;
+
+    public MethodDefinition DeleteMethod => new MethodDefinition
+    {
+        Generate = true,
+        Method = typeof(v2020_06_01.ResourceGroups.DeleteOperation),
+        TimeoutInMinutes = 30,
+    };
+}
