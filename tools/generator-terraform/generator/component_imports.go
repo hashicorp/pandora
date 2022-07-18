cb2f0eb20b6b7bbc454b062f0e00d42988b2cdcf
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-07-18 10:26:36 +0200

    tools/generator-terraform: adding `response` to the imports

--- tools/generator-terraform/generator/component_imports.go
+++ tools/generator-terraform/generator/component_imports.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/hashicorp/go-azure-helpers/lang/response"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonids"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/%[1]s/%[2]s/%[3]s"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
