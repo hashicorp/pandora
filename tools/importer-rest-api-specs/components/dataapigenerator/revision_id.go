27859d75108defee827bc4bc362e50fc2a8b3f1e
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-07-20 13:45:07 +0200

    tools/importer-rest-api-specs: refactoring the revision id templater out into it's own file
    The method calling this will become internal shortly, but simplest to split this out

--- tools/importer-rest-api-specs/components/dataapigenerator/revision_id.go
+++ tools/importer-rest-api-specs/components/dataapigenerator/revision_id.go
@@ -14,12 +14,3 @@ func OutputRevisionId(workingDirectory, namespace, swaggerGitSha string) error {
 
 	return nil
 }
-
-func codeForRevisionIdFile(namespace, swaggerGitSha string) string {
-	return fmt.Sprintf(`namespace %[1]s;
-
-%[3]s
-
-// Generated from Swagger revision %[2]q
-`, namespace, swaggerGitSha, restApiSpecsLicence)
-}
