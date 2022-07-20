e750a7ad622c1e8580e36320e321343b1f0e15a8
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-07-20 13:50:52 +0200

    tools/importer-rest-api-specs: refactoring the api version definition into it's own templater

--- tools/importer-rest-api-specs/components/dataapigenerator/definition_version.go
+++ tools/importer-rest-api-specs/components/dataapigenerator/definition_version.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"os"
 	"path"
-	"sort"
 	"strings"
 
 	"github.com/hashicorp/pandora/tools/importer-rest-api-specs/models"
@@ -54,51 +53,4 @@ func (g PandoraDefinitionGenerator) GenerateVersionDefinitionAndRecreateDirector
 	}
 
 	return nil
-}
-
-func codeForApiVersionDefinition(namespace, apiVersion string, isPreview bool, resources map[string]models.AzureApiResource) string {
-	names := make([]string, 0)
-	for name, value := range resources {
-		// skip ones which are filtered out
-		if len(value.Operations) == 0 {
-			continue
-		}
-
-		names = append(names, name)
-	}
-
-	sort.Strings(names)
-
-	lines := make([]string, 0)
-	for _, name := range names {
-		lines = append(lines, fmt.Sprintf("\t\tnew %s.Definition(),", name))
-	}
-
-	return fmt.Sprintf(`using System.Collections.Generic;
-using Pandora.Definitions.Interfaces;
-
-namespace %[1]s;
-
-public partial class Definition : ApiVersionDefinition
-{
-	public string ApiVersion => %[2]q;
-	public bool Preview => %[3]t;
-    public Source Source => Source.ResourceManagerRestApiSpecs;
-	
-	public IEnumerable<ResourceDefinition> Resources => new List<ResourceDefinition>
-	{
-%[4]s
-	};
-}
-`, namespace, apiVersion, isPreview, strings.Join(lines, "\n"))
-}
-
-func codeForApiVersionDefinitionSetting(namespace string) string {
-	return fmt.Sprintf(`namespace %[1]s;
-
-public partial class Definition
-{
-	public bool Generate => true;
-}
-`, namespace)
 }
