fbeda6a33049e758d24d84e70618505e1c8a71c6
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-07-22 11:49:57 +0200

    refactor: updating the CLI to use github.com/mitchellh/cli
    This allows splitting the pipeline up

--- tools/importer-rest-api-specs/main.go
+++ tools/importer-rest-api-specs/main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"flag"
 	"log"
 	"os"
-	"strings"
-	"time"
 
-	"github.com/hashicorp/pandora/tools/importer-rest-api-specs/pipeline"
+	"github.com/hashicorp/pandora/tools/importer-rest-api-specs/cmd"
 
-	"github.com/hashicorp/go-hclog"
+	"github.com/mitchellh/cli"
 )
 
 const (
@@ -17,55 +14,24 @@ const (
 	swaggerDirectory         = "../../swagger"
 	resourceManagerConfig    = "../../config/resource-manager.hcl"
 	terraformDefinitionsPath = "../../config/resources/"
-
-	// only useful for testing without outputting everything
-	justParse = false
 )
 
 func main() {
 	// works around the OAIGen bug
 	os.Setenv("OAIGEN_DEDUPE", "false")
 
-	var dataApiEndpointVar string
-	justSegments := false
-
-	f := flag.NewFlagSet("importer-rest-api-specs", flag.ExitOnError)
-	f.StringVar(&dataApiEndpointVar, "data-api", "", "The Data API Endpoint (e.g. --data-api=http://localhost:5000")
-	f.BoolVar(&justSegments, "just-segments", false, "Should only the Segments be output?")
-	f.Parse(os.Args[1:])
-
-	var dataApiEndpoint *string
-	if dataApiEndpointVar == "" {
-		log.Printf("[DEBUG] No Data API Endpoint is specified - using local-only mode")
-		dataApiEndpoint = nil
-	} else {
-		log.Printf("[DEBUG] Using Data API Endpoint %q", dataApiEndpointVar)
-		dataApiEndpoint = &dataApiEndpointVar
+	c := cli.NewCLI("importer-rest-api-specs", "1.0.0")
+	c.Args = os.Args[1:]
+	c.Commands = map[string]cli.CommandFactory{
+		"import":   cmd.NewImportCommand(swaggerDirectory, resourceManagerConfig, terraformDefinitionsPath, outputDirectory),
+		"segments": cmd.NewSegmentsCommand(swaggerDirectory, resourceManagerConfig),
+		"validate": cmd.NewValidateCommand(swaggerDirectory, resourceManagerConfig, terraformDefinitionsPath),
 	}
 
-	logger := hclog.New(&hclog.LoggerOptions{
-		Level:  hclog.DefaultLevel,
-		Output: hclog.DefaultOutput,
-		TimeFn: time.Now,
-	})
-	if strings.TrimSpace(os.Getenv("DEBUG")) != "" {
-		logger.SetLevel(hclog.Trace)
-	}
-	input := pipeline.RunInput{
-		SwaggerDirectory:         swaggerDirectory,
-		ConfigFilePath:           resourceManagerConfig,
-		TerraformDefinitionsPath: terraformDefinitionsPath,
-		DataApiEndpoint:          dataApiEndpoint,
-		JustOutputSegments:       justSegments,
-		JustParseData:            justParse,
-		OutputDirectory:          outputDirectory,
-		Logger:                   logger,
-	}
-	if err := pipeline.Run(input); err != nil {
-		log.Printf("Error: %+v", err)
-		os.Exit(1)
-		return
+	exitStatus, err := c.Run()
+	if err != nil {
+		log.Println(err)
 	}
 
-	os.Exit(0)
+	os.Exit(exitStatus)
 }
