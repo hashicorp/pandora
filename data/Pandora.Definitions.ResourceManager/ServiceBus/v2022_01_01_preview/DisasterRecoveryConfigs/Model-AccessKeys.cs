de646dccf2dcd02974dce7b7ef7035c9c0d804bd
hc-github-team-tf-azure <>
2022-07-26 11:52:10 +0000

    data: regenerating based on the latest Swagger

---
+++ data/Pandora.Definitions.ResourceManager/ServiceBus/v2022_01_01_preview/DisasterRecoveryConfigs/Model-AccessKeys.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using Pandora.Definitions.Attributes;
+using Pandora.Definitions.Attributes.Validation;
+using Pandora.Definitions.CustomTypes;
+
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+
+namespace Pandora.Definitions.ResourceManager.ServiceBus.v2022_01_01_preview.DisasterRecoveryConfigs;
+
+
+internal class AccessKeysModel
+{
+    [JsonPropertyName("aliasPrimaryConnectionString")]
+    public string? AliasPrimaryConnectionString { get; set; }
+
+    [JsonPropertyName("aliasSecondaryConnectionString")]
+    public string? AliasSecondaryConnectionString { get; set; }
+
+    [JsonPropertyName("keyName")]
+    public string? KeyName { get; set; }
+
+    [JsonPropertyName("primaryConnectionString")]
+    public string? PrimaryConnectionString { get; set; }
+
+    [JsonPropertyName("primaryKey")]
+    public string? PrimaryKey { get; set; }
+
+    [JsonPropertyName("secondaryConnectionString")]
+    public string? SecondaryConnectionString { get; set; }
+
+    [JsonPropertyName("secondaryKey")]
+    public string? SecondaryKey { get; set; }
+}
---
+++ data/Pandora.Definitions.ResourceManager/ServiceBus/v2022_01_01_preview/Subscriptions/Constant-FilterType.cs
@@ -0,0 +1,14 @@
+using Pandora.Definitions.Attributes;
+using System.ComponentModel;
+
+namespace Pandora.Definitions.ResourceManager.ServiceBus.v2022_01_01_preview.Subscriptions;
+
+[ConstantType(ConstantTypeAttribute.ConstantType.String)]
+internal enum FilterTypeConstant
+{
+    [Description("CorrelationFilter")]
+    CorrelationFilter,
+
+    [Description("SqlFilter")]
+    SqlFilter,
+}
