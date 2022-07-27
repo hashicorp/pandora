b48e5c9682d522aeb98165824f86b0a2be402387
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-07-27 10:55:26 +0200

    tools/generator-terraform: test covering the attributes only func

--- tools/generator-terraform/generator/plugin_sdk_attributes_ordering_test.go
+++ tools/generator-terraform/generator/plugin_sdk_attributes_ordering_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/hashicorp/pandora/tools/sdk/resourcemanager"
 )
 
-func TestPluginSdkAttributes_FieldOrdering_TopLevelIsSorted(t *testing.T) {
+func TestPluginSdkAttributes_FieldOrdering_TopLevel(t *testing.T) {
 	// NOTE: Since this is a top-level model we /shouldn't/ output the top level Computed fields
 	// as such the expectation below intentionally doesn't include them - this is not a bug but
 	// the design of the Typed SDK at this time (requiring the use of the `Attributes` method).
@@ -235,6 +235,96 @@ map[string]*pluginsdk.Schema{
 }
 `
 	actual, err := helper.codeForModel(input, false)
+	if err != nil {
+		t.Fatalf("unexpected error: %+v", err)
+	}
+	assertTemplatedCodeMatches(t, expected, *actual)
+}
+
+func TestPluginSdkAttributes_FieldOrdering_TopLevelAttributesOnly(t *testing.T) {
+	// NOTE: Since this is a top-level model we /shouldn't/ output the top level Computed fields
+	// as such the expectation below intentionally doesn't include them - this is not a bug but
+	// the design of the Typed SDK at this time (requiring the use of the `Attributes` method).
+	input := resourcemanager.TerraformSchemaModelDefinition{
+		Fields: map[string]resourcemanager.TerraformSchemaFieldDefinition{
+			"optional_nested_item": {
+				ObjectDefinition: resourcemanager.TerraformSchemaFieldObjectDefinition{
+					Type:          resourcemanager.TerraformSchemaFieldTypeReference,
+					ReferenceName: stringPointer("NestedSchema"),
+				},
+				Optional: true,
+			},
+			"required_integer": {
+				ObjectDefinition: resourcemanager.TerraformSchemaFieldObjectDefinition{
+					Type: resourcemanager.TerraformSchemaFieldTypeInteger,
+				},
+				Required: true,
+			},
+			"optional_integer": {
+				ObjectDefinition: resourcemanager.TerraformSchemaFieldObjectDefinition{
+					Type: resourcemanager.TerraformSchemaFieldTypeInteger,
+				},
+				Optional: true,
+			},
+			"computed_integer": {
+				ObjectDefinition: resourcemanager.TerraformSchemaFieldObjectDefinition{
+					Type: resourcemanager.TerraformSchemaFieldTypeInteger,
+				},
+				Computed: true,
+			},
+			"required_string": {
+				ObjectDefinition: resourcemanager.TerraformSchemaFieldObjectDefinition{
+					Type: resourcemanager.TerraformSchemaFieldTypeString,
+				},
+				Required: true,
+			},
+			"optional_string": {
+				ObjectDefinition: resourcemanager.TerraformSchemaFieldObjectDefinition{
+					Type: resourcemanager.TerraformSchemaFieldTypeString,
+				},
+				Optional: true,
+			},
+			"computed_string": {
+				ObjectDefinition: resourcemanager.TerraformSchemaFieldObjectDefinition{
+					Type: resourcemanager.TerraformSchemaFieldTypeString,
+				},
+				Computed: true,
+			},
+		},
+	}
+	helper := pluginSdkAttributesHelpers{
+		schemaModels: map[string]resourcemanager.TerraformSchemaModelDefinition{
+			"NestedSchema": {
+				Fields: map[string]resourcemanager.TerraformSchemaFieldDefinition{
+					"nested_item": {
+						ObjectDefinition: resourcemanager.TerraformSchemaFieldObjectDefinition{
+							Type: resourcemanager.TerraformSchemaFieldTypeString,
+						},
+						Optional: true,
+					},
+					"computed_item": {
+						ObjectDefinition: resourcemanager.TerraformSchemaFieldObjectDefinition{
+							Type: resourcemanager.TerraformSchemaFieldTypeString,
+						},
+						Computed: true,
+					},
+				},
+			},
+		},
+	}
+	expected := `
+map[string]*pluginsdk.Schema{
+	"computed_integer": {
+		Computed: true,
+		Type: pluginsdk.TypeInt,
+	},
+	"computed_string": {
+		Computed: true,
+		Type: pluginsdk.TypeString,
+	},
+}
+`
+	actual, err := helper.codeForModelAttributesOnly(input)
 	if err != nil {
 		t.Fatalf("unexpected error: %+v", err)
 	}
