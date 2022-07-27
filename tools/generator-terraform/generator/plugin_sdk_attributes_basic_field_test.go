ad65d344cd21597fdca9fd9fb0e407fb36eca639
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-07-27 09:13:18 +0200

    tools/generator-terraform: tests covering references

--- tools/generator-terraform/generator/plugin_sdk_attributes_basic_field_test.go
+++ tools/generator-terraform/generator/plugin_sdk_attributes_basic_field_test.go
@@ -147,3 +147,205 @@ func TestPluginSdkAttributes_CodeForBasicFields(t *testing.T) {
 		})
 	}
 }
+
+func TestPluginSdkAttributes_CodeForReference(t *testing.T) {
+	testData := []struct {
+		input    resourcemanager.TerraformSchemaFieldDefinition
+		expected string
+	}{
+		{
+			input: resourcemanager.TerraformSchemaFieldDefinition{
+				ObjectDefinition: resourcemanager.TerraformSchemaFieldObjectDefinition{
+					Type:          resourcemanager.TerraformSchemaFieldTypeReference,
+					ReferenceName: stringPointer("NestedSchemaModel"),
+				},
+				Computed: false,
+				ForceNew: false,
+				Optional: false,
+				Required: true,
+			},
+			expected: `
+"my_field": {
+	Elem: &pluginsdk.Resource{
+		Schema: map[string]*pluginsdk.Schema{
+			"nested_field": {
+				Optional: true,
+				Type: pluginsdk.TypeString,
+			},
+		},
+	},
+	MaxItems: 1,
+	Required: true,
+	Type: pluginsdk.TypeList,
+}
+`,
+		},
+		{
+			input: resourcemanager.TerraformSchemaFieldDefinition{
+				ObjectDefinition: resourcemanager.TerraformSchemaFieldObjectDefinition{
+					Type:          resourcemanager.TerraformSchemaFieldTypeReference,
+					ReferenceName: stringPointer("NestedSchemaModel"),
+				},
+				Computed: false,
+				ForceNew: true,
+				Optional: false,
+				Required: true,
+			},
+			expected: `
+"my_field": {
+	Elem: &pluginsdk.Resource{
+		Schema: map[string]*pluginsdk.Schema{
+			"nested_field": {
+				Optional: true,
+				Type: pluginsdk.TypeString,
+			},
+		},
+	},
+	ForceNew: true,
+	MaxItems: 1,
+	Required: true,
+	Type: pluginsdk.TypeList,
+}
+`,
+		},
+		{
+			input: resourcemanager.TerraformSchemaFieldDefinition{
+				ObjectDefinition: resourcemanager.TerraformSchemaFieldObjectDefinition{
+					Type:          resourcemanager.TerraformSchemaFieldTypeReference,
+					ReferenceName: stringPointer("NestedSchemaModel"),
+				},
+				Computed: false,
+				ForceNew: false,
+				Optional: true,
+				Required: false,
+			},
+			expected: `
+"my_field": {
+	Elem: &pluginsdk.Resource{
+		Schema: map[string]*pluginsdk.Schema{
+			"nested_field": {
+				Optional: true,
+				Type: pluginsdk.TypeString,
+			},
+		},
+	},
+	MaxItems: 1,
+	Optional: true,
+	Type: pluginsdk.TypeList,
+}
+`,
+		},
+		{
+			input: resourcemanager.TerraformSchemaFieldDefinition{
+				ObjectDefinition: resourcemanager.TerraformSchemaFieldObjectDefinition{
+					Type:          resourcemanager.TerraformSchemaFieldTypeReference,
+					ReferenceName: stringPointer("NestedSchemaModel"),
+				},
+				Computed: false,
+				ForceNew: true,
+				Optional: true,
+				Required: false,
+			},
+			expected: `
+"my_field": {
+	Elem: &pluginsdk.Resource{
+		Schema: map[string]*pluginsdk.Schema{
+			"nested_field": {
+				Optional: true,
+				Type: pluginsdk.TypeString,
+			},
+		},
+	},
+	ForceNew: true,
+	MaxItems: 1,
+	Optional: true,
+	Type: pluginsdk.TypeList,
+}
+`,
+		},
+		{
+			input: resourcemanager.TerraformSchemaFieldDefinition{
+				ObjectDefinition: resourcemanager.TerraformSchemaFieldObjectDefinition{
+					Type:          resourcemanager.TerraformSchemaFieldTypeReference,
+					ReferenceName: stringPointer("NestedSchemaModel"),
+				},
+				Computed: true,
+				ForceNew: true,
+				Optional: true,
+				Required: false,
+			},
+			expected: `
+"my_field": {
+	Computed: true,
+	Elem: &pluginsdk.Resource{
+		Schema: map[string]*pluginsdk.Schema{
+			"nested_field": {
+				Optional: true,
+				Type: pluginsdk.TypeString,
+			},
+		},
+	},
+	ForceNew: true,
+	MaxItems: 1,
+	Optional: true,
+	Type: pluginsdk.TypeList,
+}
+`,
+		},
+		{
+			input: resourcemanager.TerraformSchemaFieldDefinition{
+				ObjectDefinition: resourcemanager.TerraformSchemaFieldObjectDefinition{
+					Type:          resourcemanager.TerraformSchemaFieldTypeReference,
+					ReferenceName: stringPointer("NestedSchemaModel"),
+				},
+				Computed: true,
+				ForceNew: false,
+				Optional: false,
+				Required: false,
+			},
+			expected: `
+"my_field": {
+	Computed: true,
+	Elem: &pluginsdk.Resource{
+		Schema: map[string]*pluginsdk.Schema{
+			"nested_field": {
+				Optional: true,
+				Type: pluginsdk.TypeString,
+			},
+		},
+	},
+	MaxItems: 1,
+	Type: pluginsdk.TypeList,
+}
+`,
+		},
+	}
+	for i, testCase := range testData {
+		helper := pluginSdkAttributesHelpers{
+			schemaModels: map[string]resourcemanager.TerraformSchemaModelDefinition{
+				"NestedSchemaModel": {
+					Fields: map[string]resourcemanager.TerraformSchemaFieldDefinition{
+						"nested_field": {
+							ObjectDefinition: resourcemanager.TerraformSchemaFieldObjectDefinition{
+								Type: resourcemanager.TerraformSchemaFieldTypeString,
+							},
+							Optional: true,
+						},
+					},
+				},
+			},
+		}
+		actual, err := helper.codeForPluginSdkAttribute("my_field", testCase.input)
+		if err != nil {
+			if testCase.expected == "" {
+				continue
+			}
+
+			t.Fatalf("unexpected error for index %d", i)
+		}
+		if testCase.expected == "" {
+			t.Fatalf("expected an error but didn't get one for index %d", i)
+		}
+		assertTemplatedCodeMatches(t, testCase.expected, *actual)
+	}
+}
