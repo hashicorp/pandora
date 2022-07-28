e4553c0057a3594af62492a7878c31400a2e030d
hc-github-team-tf-azure <>
2022-07-28 08:34:40 +0000

    data: regenerating based on the latest Swagger

---
+++ data/Pandora.Definitions.ResourceManager/Dashboard/v2022_08_01/PrivateEndpointConnection/Constant-PrivateEndpointServiceConnectionStatus.cs
@@ -0,0 +1,17 @@
+using Pandora.Definitions.Attributes;
+using System.ComponentModel;
+
+namespace Pandora.Definitions.ResourceManager.Dashboard.v2022_08_01.PrivateEndpointConnection;
+
+[ConstantType(ConstantTypeAttribute.ConstantType.String)]
+internal enum PrivateEndpointServiceConnectionStatusConstant
+{
+    [Description("Approved")]
+    Approved,
+
+    [Description("Pending")]
+    Pending,
+
+    [Description("Rejected")]
+    Rejected,
+}
