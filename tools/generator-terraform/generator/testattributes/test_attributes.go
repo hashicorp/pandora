b82525a63db2e4cc18998ea5d90913c66baa4b95
Matthew Frahry <mbfrahry@gmail.com>
2022-07-29 14:29:59 -0700

    Finalize adding test attributes

--- tools/generator-terraform/generator/testattributes/test_attributes.go
+++ tools/generator-terraform/generator/testattributes/test_attributes.go
@@ -4,14 +4,19 @@ import (
 	"fmt"
 	"sort"
 
+	"github.com/hashicorp/hcl/v2/hclwrite"
+
 	"github.com/zclconf/go-cty/cty"
 
-	"github.com/hashicorp/hcl/v2/hclwrite"
 	"github.com/hashicorp/pandora/tools/sdk/resourcemanager"
 )
 
-func codeForTestConfig(input resourcemanager.TerraformSchemaModelDefinition) (string, error) {
-	// fields should be sorted Required -> Optional -> Computed, and alphabetically within each category
+type TestAttributesHelpers struct {
+	SchemaModels map[string]resourcemanager.TerraformSchemaModelDefinition
+	File         *hclwrite.File
+}
+
+func (h TestAttributesHelpers) GetAttributesForTests(input resourcemanager.TerraformSchemaModelDefinition, hclBody hclwrite.Body, requiredOnly bool) error {
 	requiredFields := make([]string, 0)
 	optionalFields := make([]string, 0)
 	for fieldName, details := range input.Fields {
@@ -19,39 +24,49 @@ func codeForTestConfig(input resourcemanager.TerraformSchemaModelDefinition) (st
 			requiredFields = append(requiredFields, fieldName)
 			continue
 		}
-
-		if details.Optional {
-			optionalFields = append(optionalFields, fieldName)
-			continue
-		}
-
-		return "", fmt.Errorf("field %q is neither required/optional/computed", fieldName)
-	}
-	sort.Strings(requiredFields)
-	sort.Strings(optionalFields)
-
-	sortedNames := make([]string, 0)
-	sortedNames = append(sortedNames, requiredFields...)
-	sortedNames = append(sortedNames, optionalFields...)
-
-	f := hclwrite.NewEmptyFile()
-	rootBody := f.Body()
-	for _, fieldName := range sortedNames {
-		field := input.Fields[fieldName]
-		if field.Required {
-			switch field.ObjectDefinition.Type {
-			// todo randomize values
-			case resourcemanager.TerraformSchemaFieldTypeBoolean:
-				rootBody.SetAttributeValue(fieldName, cty.False)
-			case resourcemanager.TerraformSchemaFieldTypeFloat:
-				rootBody.SetAttributeValue(fieldName, cty.NumberFloatVal(10.1))
-			case resourcemanager.TerraformSchemaFieldTypeInteger:
-				rootBody.SetAttributeValue(fieldName, cty.NumberIntVal(15))
-			case resourcemanager.TerraformSchemaFieldTypeString:
-				rootBody.SetAttributeValue(fieldName, cty.StringVal("foo"))
+		if !requiredOnly {
+			if details.Optional {
+				optionalFields = append(optionalFields, fieldName)
+				continue
 			}
 		}
 	}
+	sort.Strings(requiredFields)
 
-	return fmt.Sprintf("%s", f.Bytes()), nil
+	sortedNames := make([]string, 0)
+	sortedNames = append(sortedNames, requiredFields...)
+
+	if !requiredOnly {
+		sort.Strings(optionalFields)
+		sortedNames = append(sortedNames, optionalFields...)
+	}
+
+	for _, fieldName := range sortedNames {
+		field := input.Fields[fieldName]
+		switch field.ObjectDefinition.Type {
+		// todo randomize values
+		case resourcemanager.TerraformSchemaFieldTypeBoolean:
+			hclBody.SetAttributeValue(fieldName, cty.False)
+		case resourcemanager.TerraformSchemaFieldTypeFloat:
+			hclBody.SetAttributeValue(fieldName, cty.NumberFloatVal(10.1))
+		case resourcemanager.TerraformSchemaFieldTypeInteger:
+			hclBody.SetAttributeValue(fieldName, cty.NumberIntVal(15))
+		case resourcemanager.TerraformSchemaFieldTypeString:
+			hclBody.SetAttributeValue(fieldName, cty.StringVal("foo"))
+		case resourcemanager.TerraformSchemaFieldTypeList, resourcemanager.TerraformSchemaFieldTypeSet:
+			hclBody.AppendNewline()
+			if field.ObjectDefinition.NestedObject.ReferenceName == nil {
+				return fmt.Errorf("missing name for reference")
+			}
+			reference, ok := h.SchemaModels[*field.ObjectDefinition.NestedObject.ReferenceName]
+			if !ok {
+				return fmt.Errorf("schema model %q was not found", *field.ObjectDefinition.NestedObject.ReferenceName)
+			}
+			if err := h.GetAttributesForTests(reference, *hclBody.AppendNewBlock(fieldName, nil).Body(), requiredOnly); err != nil {
+				return err
+			}
+			hclBody.AppendNewline()
+		}
+	}
+	return nil
 }
--- tools/generator-terraform/generator/testattributes/test_attributes_test.go
+++ tools/generator-terraform/generator/testattributes/test_attributes_test.go
@@ -1,12 +1,15 @@
 package testattributes
 
 import (
+	"fmt"
 	"testing"
+
+	"github.com/hashicorp/hcl/v2/hclwrite"
 
 	"github.com/hashicorp/pandora/tools/sdk/resourcemanager"
 )
 
-func TestTestAttributes_CodeForBasicFields(t *testing.T) {
+func TestRequiredTestAttributes_CodeForBasicFields(t *testing.T) {
 	input := resourcemanager.TerraformSchemaModelDefinition{
 		Fields: map[string]resourcemanager.TerraformSchemaFieldDefinition{
 			"required_bool_attribute": {
@@ -33,18 +36,173 @@ func TestTestAttributes_CodeForBasicFields(t *testing.T) {
 				},
 				Required: true,
 			},
+			"required_list_attribute": {
+				ObjectDefinition: resourcemanager.TerraformSchemaFieldObjectDefinition{
+					Type: resourcemanager.TerraformSchemaFieldTypeList,
+					NestedObject: &resourcemanager.TerraformSchemaFieldObjectDefinition{
+						Type:          resourcemanager.TerraformSchemaFieldTypeReference,
+						ReferenceName: stringPointer("NestedSchemaModel"),
+					},
+				},
+				Required: true,
+			},
+			"required_set_attribute": {
+				ObjectDefinition: resourcemanager.TerraformSchemaFieldObjectDefinition{
+					Type: resourcemanager.TerraformSchemaFieldTypeList,
+					NestedObject: &resourcemanager.TerraformSchemaFieldObjectDefinition{
+						Type:          resourcemanager.TerraformSchemaFieldTypeReference,
+						ReferenceName: stringPointer("NestedSchemaModel"),
+					},
+				},
+				Required: true,
+			},
 		},
 	}
 	expected := `
-	required_bool_attribute   = false
-	required_float_attribute  = 10.1
-	required_int_attribute    = 15
+	required_bool_attribute  = false
+	required_float_attribute = 10.1
+	required_int_attribute   = 15
+	required_list_attribute {
+		required_nested_bool   = false
+		required_nested_string = "foo"
+	}
+	required_set_attribute {
+		required_nested_bool   = false
+		required_nested_string = "foo"
+	}
 	required_string_attribute = "foo"
 `
 
-	actual, err := codeForTestConfig(input)
+	helper := TestAttributesHelpers{
+		File: hclwrite.NewEmptyFile(),
+		SchemaModels: map[string]resourcemanager.TerraformSchemaModelDefinition{
+			"NestedSchemaModel": {
+				Fields: map[string]resourcemanager.TerraformSchemaFieldDefinition{
+					"required_nested_string": {
+						ObjectDefinition: resourcemanager.TerraformSchemaFieldObjectDefinition{
+							Type: resourcemanager.TerraformSchemaFieldTypeString,
+						},
+						Required: true,
+					},
+					"required_nested_bool": {
+						ObjectDefinition: resourcemanager.TerraformSchemaFieldObjectDefinition{
+							Type: resourcemanager.TerraformSchemaFieldTypeBoolean,
+						},
+						Required: true,
+					},
+					"optional_nested_string": {
+						ObjectDefinition: resourcemanager.TerraformSchemaFieldObjectDefinition{
+							Type: resourcemanager.TerraformSchemaFieldTypeString,
+						},
+						Optional: true,
+					},
+				},
+			},
+		},
+	}
+	err := helper.GetAttributesForTests(input, *helper.File.Body(), true)
 	if err != nil {
 		t.Fatalf("unexpected error: %+v", err)
 	}
-	assertTemplatedCodeMatches(t, expected, actual)
+	assertTemplatedCodeMatches(t, expected, fmt.Sprintf("%s", helper.File.Bytes()))
+}
+
+func TestRequiredAndOptionalTestAttributes_CodeForBasicField(t *testing.T) {
+	input := resourcemanager.TerraformSchemaModelDefinition{
+		Fields: map[string]resourcemanager.TerraformSchemaFieldDefinition{
+			"required_bool_attribute": {
+				ObjectDefinition: resourcemanager.TerraformSchemaFieldObjectDefinition{
+					Type: "Boolean",
+				},
+				Required: true,
+			},
+			"required_float_attribute": {
+				ObjectDefinition: resourcemanager.TerraformSchemaFieldObjectDefinition{
+					Type: "Float",
+				},
+				Required: true,
+			},
+			"optional_int_attribute": {
+				ObjectDefinition: resourcemanager.TerraformSchemaFieldObjectDefinition{
+					Type: "Integer",
+				},
+				Optional: true,
+			},
+			"optional_string_attribute": {
+				ObjectDefinition: resourcemanager.TerraformSchemaFieldObjectDefinition{
+					Type: "String",
+				},
+				Optional: true,
+			},
+			"required_list_attribute": {
+				ObjectDefinition: resourcemanager.TerraformSchemaFieldObjectDefinition{
+					Type: resourcemanager.TerraformSchemaFieldTypeList,
+					NestedObject: &resourcemanager.TerraformSchemaFieldObjectDefinition{
+						Type:          resourcemanager.TerraformSchemaFieldTypeReference,
+						ReferenceName: stringPointer("NestedSchemaModel"),
+					},
+				},
+				Required: true,
+			},
+			"optional_set_attribute": {
+				ObjectDefinition: resourcemanager.TerraformSchemaFieldObjectDefinition{
+					Type: resourcemanager.TerraformSchemaFieldTypeList,
+					NestedObject: &resourcemanager.TerraformSchemaFieldObjectDefinition{
+						Type:          resourcemanager.TerraformSchemaFieldTypeReference,
+						ReferenceName: stringPointer("NestedSchemaModel"),
+					},
+				},
+				Optional: true,
+			},
+		},
+	}
+	expected := `
+	required_bool_attribute  = false
+	required_float_attribute = 10.1
+	required_list_attribute {
+		required_nested_bool   = false
+		required_nested_string = "foo"
+		optional_nested_string = "foo"
+	}
+	optional_int_attribute = 15
+	optional_set_attribute {
+		required_nested_bool   = false
+		required_nested_string = "foo"
+		optional_nested_string = "foo"
+	}
+	optional_string_attribute = "foo"
+`
+
+	helper := TestAttributesHelpers{
+		File: hclwrite.NewEmptyFile(),
+		SchemaModels: map[string]resourcemanager.TerraformSchemaModelDefinition{
+			"NestedSchemaModel": {
+				Fields: map[string]resourcemanager.TerraformSchemaFieldDefinition{
+					"required_nested_string": {
+						ObjectDefinition: resourcemanager.TerraformSchemaFieldObjectDefinition{
+							Type: resourcemanager.TerraformSchemaFieldTypeString,
+						},
+						Required: true,
+					},
+					"required_nested_bool": {
+						ObjectDefinition: resourcemanager.TerraformSchemaFieldObjectDefinition{
+							Type: resourcemanager.TerraformSchemaFieldTypeBoolean,
+						},
+						Required: true,
+					},
+					"optional_nested_string": {
+						ObjectDefinition: resourcemanager.TerraformSchemaFieldObjectDefinition{
+							Type: resourcemanager.TerraformSchemaFieldTypeString,
+						},
+						Optional: true,
+					},
+				},
+			},
+		},
+	}
+	err := helper.GetAttributesForTests(input, *helper.File.Body(), false)
+	if err != nil {
+		t.Fatalf("unexpected error: %+v", err)
+	}
+	assertTemplatedCodeMatches(t, expected, fmt.Sprintf("%s", helper.File.Bytes()))
 }
