b925bd62aea951691c5c001bd0e4c1eef79a729a
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-07-30 11:32:33 +0100

    tools/generator-terraform: conditional output of `ResourceWithUpdate`

--- tools/generator-terraform/generator/resource/component_definition_test.go
+++ tools/generator-terraform/generator/resource/component_definition_test.go
@@ -4,6 +4,8 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/hashicorp/pandora/tools/sdk/resourcemanager"
+
 	"github.com/hashicorp/pandora/tools/generator-terraform/generator/models"
 )
 
@@ -17,6 +19,30 @@ func TestComponentDefinition(t *testing.T) {
 	}
 	expected := strings.TrimSpace(`
 var _ sdk.Resource = ExampleResource{}
+
+type ExampleResource struct {}
+`)
+	assertTemplatedCodeMatches(t, expected, *actual)
+}
+
+func TestComponentDefinitionForMethodWithUpdate(t *testing.T) {
+	input := models.ResourceInput{
+		ResourceTypeName: "Example",
+		Details: resourcemanager.TerraformResourceDetails{
+			UpdateMethod: &resourcemanager.MethodDefinition{
+				MethodName:       "Update",
+				Generate:         true,
+				TimeoutInMinutes: 30,
+			},
+		},
+	}
+	actual, err := definitionForResource(input)
+	if err != nil {
+		t.Fatalf("error: %+v", err)
+	}
+	expected := strings.TrimSpace(`
+var _ sdk.Resource = ExampleResource{}
+var _ sdk.ResourceWithUpdate = ExampleResource{}
 
 type ExampleResource struct {}
 `)
