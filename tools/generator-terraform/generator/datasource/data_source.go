3e7b3b85f0b0380c216ee6115d9c2306f8b51ce7
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-07-31 10:31:35 +0100

    tools/generator-terraform: refactoring the data source setup to match the resource setup

---
+++ tools/generator-terraform/generator/datasource/data_source.go
@@ -0,0 +1,13 @@
+package datasource
+
+import (
+	"log"
+
+	"github.com/hashicorp/pandora/tools/generator-terraform/generator/models"
+)
+
+func DataSource(input models.DataSourceInput) error {
+	// TODO: conditionally output both a Singular and Plural Data Source?
+	log.Printf("TODO: Data Source stuff")
+	return nil
+}
