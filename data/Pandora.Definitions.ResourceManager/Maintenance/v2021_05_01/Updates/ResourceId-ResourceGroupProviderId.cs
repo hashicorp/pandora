119cc7f38c5a21f16318f72f631a9a70ba984e40
hc-github-team-tf-azure <>
2022-08-02 10:41:52 +0000

    data: regenerating based on the latest Swagger

---
+++ data/Pandora.Definitions.ResourceManager/Maintenance/v2021_05_01/Updates/ResourceId-ResourceGroupProviderId.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using Pandora.Definitions.Interfaces;
+
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+
+namespace Pandora.Definitions.ResourceManager.Maintenance.v2021_05_01.Updates;
+
+internal class ResourceGroupProviderId : ResourceID
+{
+    public string? CommonAlias => null;
+
+    public string ID => "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/{providerName}/{resourceParentType}/{resourceParentName}/{resourceType}/{resourceName}";
+
+    public List<ResourceIDSegment> Segments => new List<ResourceIDSegment>
+    {
+        ResourceIDSegment.Static("staticSubscriptions", "subscriptions"),
+        ResourceIDSegment.SubscriptionId("subscriptionId"),
+        ResourceIDSegment.Static("staticResourceGroups", "resourceGroups"),
+        ResourceIDSegment.ResourceGroup("resourceGroupName"),
+        ResourceIDSegment.Static("staticProviders", "providers"),
+        ResourceIDSegment.UserSpecified("providerName"),
+        ResourceIDSegment.UserSpecified("resourceParentType"),
+        ResourceIDSegment.UserSpecified("resourceParentName"),
+        ResourceIDSegment.UserSpecified("resourceType"),
+        ResourceIDSegment.UserSpecified("resourceName"),
+    };
+}
