0ed98bdcd3f99612a4717236446bf5a78c90bf57
hc-github-team-tf-azure <>
2022-08-03 10:42:12 +0000

    data: regenerating based on the latest Swagger

---
+++ data/Pandora.Definitions.ResourceManager/Insights/v2021_05_01_preview/ApiVersionDefinition.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using Pandora.Definitions.Interfaces;
+
+namespace Pandora.Definitions.ResourceManager.Insights.v2021_05_01_preview;
+
+public partial class Definition : ApiVersionDefinition
+{
+    public string ApiVersion => "2021-05-01-preview";
+    public bool Preview => true;
+    public Source Source => Source.ResourceManagerRestApiSpecs;
+
+    public IEnumerable<ResourceDefinition> Resources => new List<ResourceDefinition>
+    {
+        new AutoScaleSettings.Definition(),
+        new DiagnosticSettings.Definition(),
+        new DiagnosticSettingsCategories.Definition(),
+        new Insights.Definition(),
+        new ManagementGroupDiagnosticSettings.Definition(),
+        new Metrics.Definition(),
+        new SubscriptionDiagnosticSettings.Definition(),
+    };
+}
