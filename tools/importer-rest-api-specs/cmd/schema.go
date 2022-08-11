4d13f2645b5188c602de245bb96ec9fd8b38b414
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-08-11 18:21:52 +0200

    tools/importer-rest-api-specs: conditionally allowing to specify the service names which should be imported

--- tools/importer-rest-api-specs/cmd/schema.go
+++ tools/importer-rest-api-specs/cmd/schema.go
@@ -1,8 +1,10 @@
 package cmd
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/pandora/tools/importer-rest-api-specs/components/resources"
@@ -25,14 +27,26 @@ type SchemaCommand struct {
 }
 
 func (s SchemaCommand) Help() string {
-	return "please"
+	return "specify -services=Compute,Resource to limit to just that or don't for everything, you do you."
 }
 
 func (s SchemaCommand) Run(args []string) int {
+	var serviceNamesRaw string
+
+	f := flag.NewFlagSet("importer-rest-api-specs", flag.ExitOnError)
+	f.StringVar(&serviceNamesRaw, "services", "", "A list of comma separated Service named from the Data API to import")
+	f.Parse(args)
+
+	var serviceNames []string
+	if serviceNamesRaw != "" {
+		serviceNames = strings.Split(serviceNamesRaw, ",")
+	}
+
 	if err := run(Input{
 		apiServerEndpoint: "http://localhost:5000",
 		configDirectory:   "../../config/resources/",
 		providerPrefix:    "azurerm",
+		services:          serviceNames,
 	}); err != nil {
 		log.Printf("error: %+v", err)
 		return 1
@@ -42,13 +56,14 @@ func (s SchemaCommand) Run(args []string) int {
 }
 
 func (s SchemaCommand) Synopsis() string {
-	return "really great"
+	return "specify -services=Compute,Resource to limit to just that or don't for everything, you do you."
 }
 
 type Input struct {
 	apiServerEndpoint string
 	providerPrefix    string
 	configDirectory   string
+	services          []string
 }
 
 func run(input Input) error {
@@ -63,12 +78,20 @@ func run(input Input) error {
 	}
 
 	logger.Trace("Retrieving Services from Data API..")
-	services, err := services.GetResourceManagerServicesByName(client, []string{"Compute"})
+	var resourceManagerServices *services.ResourceManagerServices
+	if len(input.services) > 0 {
+		logger.Info(fmt.Sprintf("Loading only the Services %q", strings.Join(input.services, ", ")))
+		resourceManagerServices, err = services.GetResourceManagerServicesByName(client, input.services)
+	} else {
+		logger.Info("Loading all services.. this may take a while..")
+		resourceManagerServices, err = services.GetResourceManagerServices(client)
+	}
+
 	if err != nil {
 		return fmt.Errorf("retrieving resource manager services: %+v", err)
 	}
 
-	for serviceName, serviceDetails := range services.Services {
+	for serviceName, serviceDetails := range resourceManagerServices.Services {
 		terraformDefinitionsForService, ok := terraformDefinitions.Services[serviceName]
 		if !ok {
 			logger.Trace(fmt.Sprintf("No Terraform Definitions for Service %q - skipping", serviceName))
