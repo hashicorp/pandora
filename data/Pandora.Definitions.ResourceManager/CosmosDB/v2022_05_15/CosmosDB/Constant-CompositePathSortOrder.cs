c17e6e4a87d288bd1630f6190a07a758923211a2
hc-github-team-tf-azure <>
2022-08-24 07:26:09 +0000

    data: regenerating based on the latest Swagger

---
+++ data/Pandora.Definitions.ResourceManager/CosmosDB/v2022_05_15/CosmosDB/Constant-CompositePathSortOrder.cs
@@ -0,0 +1,14 @@
+using Pandora.Definitions.Attributes;
+using System.ComponentModel;
+
+namespace Pandora.Definitions.ResourceManager.CosmosDB.v2022_05_15.CosmosDB;
+
+[ConstantType(ConstantTypeAttribute.ConstantType.String)]
+internal enum CompositePathSortOrderConstant
+{
+    [Description("ascending")]
+    Ascending,
+
+    [Description("descending")]
+    Descending,
+}
---
+++ data/Pandora.Definitions.ResourceManager/CosmosDB/v2022_05_15/CosmosDB/Operation-SqlResourcesUpdateSqlDatabaseThroughput.cs
@@ -0,0 +1,35 @@
+using Pandora.Definitions.Attributes;
+using Pandora.Definitions.CustomTypes;
+using Pandora.Definitions.Interfaces;
+using Pandora.Definitions.Operations;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+
+namespace Pandora.Definitions.ResourceManager.CosmosDB.v2022_05_15.CosmosDB;
+
+internal class SqlResourcesUpdateSqlDatabaseThroughputOperation : Operations.PutOperation
+{
+    public override IEnumerable<HttpStatusCode> ExpectedStatusCodes() => new List<HttpStatusCode>
+        {
+                HttpStatusCode.Accepted,
+                HttpStatusCode.OK,
+        };
+
+    public override bool LongRunning() => true;
+
+    public override Type? RequestObject() => typeof(ThroughputSettingsUpdateParametersModel);
+
+    public override ResourceID? ResourceId() => new SqlDatabaseId();
+
+    public override Type? ResponseObject() => typeof(ThroughputSettingsGetResultsModel);
+
+    public override string? UriSuffix() => "/throughputSettings/default";
+
+
+}
