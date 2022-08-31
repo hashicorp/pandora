d3b78c93cd99a5cbfc95d0cf769c7bfcb4ac4165
hc-github-team-tf-azure <>
2022-08-31 08:32:31 +0000

    data: regenerating based on the latest Swagger

---
+++ data/Pandora.Definitions.ResourceManager/DataProtection/v2022_05_01/BackupPolicies/Model-RetentionTag.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using Pandora.Definitions.Attributes;
+using Pandora.Definitions.Attributes.Validation;
+using Pandora.Definitions.CustomTypes;
+
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+
+namespace Pandora.Definitions.ResourceManager.DataProtection.v2022_05_01.BackupPolicies;
+
+
+internal class RetentionTagModel
+{
+    [JsonPropertyName("eTag")]
+    public string? ETag { get; set; }
+
+    [JsonPropertyName("id")]
+    public string? Id { get; set; }
+
+    [JsonPropertyName("tagName")]
+    [Required]
+    public string TagName { get; set; }
+}
---
+++ data/Pandora.Definitions.ResourceManager/DataProtection/v2022_05_01/BackupPolicies/Model-ScheduleBasedTriggerContext.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using Pandora.Definitions.Attributes;
+using Pandora.Definitions.Attributes.Validation;
+using Pandora.Definitions.CustomTypes;
+
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+
+namespace Pandora.Definitions.ResourceManager.DataProtection.v2022_05_01.BackupPolicies;
+
+[ValueForType("ScheduleBasedTriggerContext")]
+internal class ScheduleBasedTriggerContextModel : TriggerContextModel
+{
+    [JsonPropertyName("schedule")]
+    [Required]
+    public BackupScheduleModel Schedule { get; set; }
+
+    [JsonPropertyName("taggingCriteria")]
+    [Required]
+    public List<TaggingCriteriaModel> TaggingCriteria { get; set; }
+}
