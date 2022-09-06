1868054349d84e0f9873a39b042eebdc3385a42c
jackofallops <ste@hashicorp.com>
2022-09-06 11:56:10 +0100

    make helpers case agnostic

--- tools/importer-rest-api-specs/components/parser/cleanup/pluralise_helper.go
+++ tools/importer-rest-api-specs/components/parser/cleanup/pluralise_helper.go
@@ -2,6 +2,7 @@ package cleanup
 
 import (
 	"github.com/gertd/go-pluralize"
+	"strings"
 )
 
 type irregularPlural = struct {
@@ -9,58 +10,72 @@ type irregularPlural = struct {
 	plural string
 }
 
-func GetSingular(input string) string {
+type caseType int
 
+const (
+	LOWER caseType = iota
+	UPPER
+	TITLE
+	CAMEL // This is likely the default in the context of TF and Azure? 
+)
+
+// GetSingular return the singular version of a given plural
+// return values are case preserved to the input.
+func GetSingular(input string) string {
+	casing := detectCasing(input)
 	for _, v := range invariablePlurals() {
-		if input == v {
-			return v
+		if strings.EqualFold(input, v) {
+			return returnCased(v, casing)
 		}
 	}
 
 	for _, v := range irregularPlurals() {
 		if input == v.plural {
-			return v.single
+			return returnCased(v.single, casing)
 		}
 	}
 
-	pluralize := pluralize.NewClient()
-	output := pluralize.Singular(input)
+	client := pluralize.NewClient()
+	output := client.Singular(input)
 
-	return output
+	return returnCased(output, casing)
 }
 
+// GetPlural returns the plural of a given word
+// return values are case preserved to the input
 func GetPlural(input string) string {
-
+	casing := detectCasing(input)
 	for _, v := range invariablePlurals() {
-		if input == v {
-			return v
+		if strings.EqualFold(input, v) {
+			return returnCased(v, casing)
 		}
 	}
 
 	for _, v := range irregularPlurals() {
-		if input == v.single {
-			return v.plural
+		if strings.EqualFold(input, v.single) {
+			return returnCased(v.plural, casing)
 		}
 	}
 
 	pluralize := pluralize.NewClient()
 	output := pluralize.Plural(input)
 
-	return output
+	return returnCased(output, casing)
 }
 
+// irregularPlurals is an exceptions list for plurals that are not satisfied by go-pluralize
+// This list is intended to be treated case insensitively by consumers
 func irregularPlurals() []irregularPlural {
 
 	pluralisationExceptions := []irregularPlural{
-		{"cache", "caches"},
 		{"Cache", "Caches"},
 	}
 	return pluralisationExceptions
 }
 
+// invariablePlurals is a list of words and names that should never be pluralised
 func invariablePlurals() []string {
 	return []string{
-		"redis",
 		"Redis",
 		"Compute",
 		"ContainerInstance",
@@ -69,4 +84,31 @@ func invariablePlurals() []string {
 		"PowerBIDedicated",
 		"ServiceLinker",
 	}
+}
+
+func detectCasing(input string) caseType {
+	switch {
+	case input == strings.ToUpper(input):
+		return UPPER
+	case input == strings.ToLower(input):
+		return LOWER
+	case input == strings.Title(input):
+		return TITLE
+	}
+	// Fallback?
+	return CAMEL
+}
+
+func returnCased(input string, casing caseType) string {
+	if casing == LOWER {
+		return strings.ToLower(input)
+	}
+	if casing == UPPER {
+		return strings.ToUpper(input)
+	}
+	if casing == TITLE {
+		return strings.Title(input)
+	}
+
+	return input
 }
