07cbb08ad851b9a93ec8bfac34caab3e0a388bb3
Matthew <mbfrahry@gmail.com>
2022-09-09 09:55:28 -0700

    Remove unused method

--- tools/importer-rest-api-specs/components/testattributes/test_attributes.go
+++ tools/importer-rest-api-specs/components/testattributes/test_attributes.go
@@ -2,15 +2,12 @@ package testattributes
 
 import (
 	"fmt"
-	"sort"
-	"strings"
-	"unicode"
-
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/hashicorp/hcl/v2/hclwrite"
 	"github.com/hashicorp/pandora/tools/sdk/resourcemanager"
 	"github.com/zclconf/go-cty/cty"
+	"sort"
 )
 
 type TestAttributesHelpers struct {
@@ -201,46 +198,4 @@ func addCommentToTestConfig(hclBody hclwrite.Body, comment string) {
 		},
 	}))
 	hclBody.AppendNewline()
-}
-
-func ConvertToSnakeCase(input string) string {
-	if v, ok := schemaFieldNameOverrides[strings.ToLower(input)]; ok {
-		return v
-	}
-
-	splitIdxMap := map[int]struct{}{}
-	var lastChar rune
-	for idx, char := range input {
-		switch {
-		case idx == 0:
-			splitIdxMap[idx] = struct{}{}
-		case unicode.IsUpper(lastChar) == unicode.IsUpper(char):
-		case unicode.IsUpper(lastChar):
-			splitIdxMap[idx-1] = struct{}{}
-		case unicode.IsUpper(char):
-			splitIdxMap[idx] = struct{}{}
-		}
-		lastChar = char
-	}
-	splitIdx := make([]int, 0, len(splitIdxMap))
-	for idx := range splitIdxMap {
-		splitIdx = append(splitIdx, idx)
-	}
-	sort.Ints(splitIdx)
-
-	inputRunes := []rune(input)
-	out := make([]string, len(splitIdx))
-	for i := range splitIdx {
-		if i == len(splitIdx)-1 {
-			out[i] = strings.ToLower(string(inputRunes[splitIdx[i]:]))
-			continue
-		}
-		out[i] = strings.ToLower(string(inputRunes[splitIdx[i]:splitIdx[i+1]]))
-	}
-	return strings.Join(out, "_")
-}
-
-var schemaFieldNameOverrides = map[string]string{
-	"etag": "etag",
-	"ip":   "ip_address",
 }
