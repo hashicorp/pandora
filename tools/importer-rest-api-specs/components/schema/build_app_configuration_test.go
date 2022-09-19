56469aa4d21260c6a37795ed9aa1e59189f19fd0
jackofallops <ste@hashicorp.com>
2022-09-19 06:26:41 +0100

    start porting over to helpers

--- tools/importer-rest-api-specs/components/schema/build_app_configuration_test.go
+++ tools/importer-rest-api-specs/components/schema/build_app_configuration_test.go
@@ -10,6 +10,10 @@ import (
 func TestBuildForAppConfigurationUsingRealData(t *testing.T) {
 	t.Skipf("TODO: update schema gen & re-enable this test")
 
+	r := resourceUnderTest{
+		Name: "configuration_store",
+	}
+
 	builder := Builder{
 		constants: map[string]resourcemanager.ConstantDetails{
 			"ActionsRequired": {
@@ -223,7 +227,7 @@ func TestBuildForAppConfigurationUsingRealData(t *testing.T) {
 						Optional: true,
 					},
 					"Properties": {
-						JsonName: "",
+						JsonName: "properties",
 						ObjectDefinition: resourcemanager.ApiObjectDefinition{
 							NestedItem:    nil,
 							ReferenceName: stringPointer("PrivateEndpointConnectionProperties"),
@@ -521,239 +525,64 @@ func TestBuildForAppConfigurationUsingRealData(t *testing.T) {
 		t.Errorf("expected 11 models but got %d", len(*actual))
 	}
 
-	topLevelModel := (*actual)["ConfigurationStore"]
-	if len(topLevelModel.Fields) != 14 {
-		t.Errorf("expected 14 fields, but got %d", len(topLevelModel.Fields))
-	}
-
-	name, ok := topLevelModel.Fields["Name"]
+	currentModel, ok := (*actual)["ConfigurationStore"]
 	if !ok {
-		t.Errorf("expected there to be a field 'Name' but didn't get one")
-	}
-	if name.HclName != "name" {
-		t.Errorf("expected the HclName for field 'Name' to be 'name' but got %q", name.HclName)
-	}
-	if name.ObjectDefinition.Type != resourcemanager.TerraformSchemaFieldTypeString {
-		t.Errorf("expected the field 'Name' to have the type `string` but got %q", string(name.ObjectDefinition.Type))
-	}
-	if !name.Required {
-		t.Errorf("expected the field 'Name' to be Required but it wasn't")
-	}
-	if !name.ForceNew {
-		t.Errorf("expected the field 'Name' to be ForceNew but it wasn't")
-	}
+		t.Errorf("expected there to be a model %q, but there wasn't", r.CurrentModel)
+	} else {
+		if len(currentModel.Fields) != 14 {
+			t.Errorf("expected 14 fields, but got %d", len(currentModel.Fields))
+		}
 
-	// TODO: source WriteOnly from the mappings
-	//if name.Optional || name.Computed || name.WriteOnly {
-	//	t.Errorf("expected the field 'Name' to be !Optional, !Computed and !WriteOnly but got %t / %t / %t", name.Optional, name.Computed, name.WriteOnly)
-	//}
+		r.checkFieldName(t, currentModel)
+		r.checkFieldLocation(t, currentModel)
+		r.checkFieldTags(t, currentModel)
 
-	if name.Optional || name.Computed {
-		t.Errorf("expected the field 'Name' to be !Optional, !Computed but got %t / %t", name.Optional, name.Computed)
-	}
+		r.checkField(t, currentModel, expected{
+			FieldName: "CreateMode",
+			HclName:   "create_mode",
+			Optional:  true,
+			ForceNew:  true,
+			FieldType: resourcemanager.TerraformSchemaFieldTypeString,
+			Validation: &expectedValidation{
+				Type:               resourcemanager.TerraformSchemaValidationTypePossibleValues,
+				PossibleValueCount: 2,
+			},
+		})
+		r.checkField(t, currentModel, expected{
+			FieldName: "CreationDate",
+			HclName:   "creation_date",
+			Computed:  true,
+			FieldType: resourcemanager.TerraformSchemaFieldTypeDateTime,
+		})
+		r.checkField(t, currentModel, expected{
+			FieldName: "DisableLocalAuth",
+			HclName:   "local_auth_disabled",
+			Optional:  true,
+			FieldType: resourcemanager.TerraformSchemaFieldTypeBoolean,
+		})
+		r.checkField(t, currentModel, expected{
+			FieldName: "EnablePurgeProtection",
+			HclName:   "purge_protection_enabled",
+			Optional:  true,
+			ForceNew:  true,
+			FieldType: resourcemanager.TerraformSchemaFieldTypeBoolean,
+		})
 
-	location, ok := topLevelModel.Fields["Location"]
-	if !ok {
-		t.Errorf("expected there to be a field 'Location' but didn't get one")
-	}
-	if location.HclName != "location" {
-		t.Errorf("expected the HclName for field 'Location' to be 'location' but got %q", location.HclName)
-	}
-	if location.ObjectDefinition.Type != resourcemanager.TerraformSchemaFieldTypeLocation {
-		t.Errorf("expected the field 'Location' to have the type `location` but got %q", string(location.ObjectDefinition.Type))
-	}
-	// note: this differs from the model above, since this is implicitly required as a top level field
-	// even if it's defined as optional in the schema
-	if !location.Required {
-		t.Errorf("expected the field 'Location' to be Required but it wasn't")
-	}
-	if !location.ForceNew {
-		t.Errorf("expected the field 'Location' to be ForceNew but it wasn't")
-	}
-	// TODO: source WriteOnly from the mappings
-	//if location.Optional || location.Computed || location.WriteOnly {
-	//	t.Errorf("expected the field 'Location' to be !Optional, !Computed and !WriteOnly but got %t / %t / %t", location.Optional, location.Computed, location.WriteOnly)
-	//}
-	if location.Optional || location.Computed {
-		t.Errorf("expected the field 'Location' to be !Optional, !Computed but got %t / %t", location.Optional, location.Computed)
-	}
+		// ProvisioningState - should be filtered out of the TF - TODO Helper for check absent?
+		_, ok = currentModel.Fields["ProvisioningState"]
+		if ok {
+			t.Errorf("expected there not to be a field 'ProvisioningState' but found one")
+		}
 
-	tags, ok := topLevelModel.Fields["Tags"]
-	if !ok {
-		t.Errorf("expected there to be a field 'Tags' but didn't get one")
-	}
-	if tags.HclName != "tags" {
-		t.Errorf("expected the HclName for field 'Tags' to be 'tags' but got %q", tags.HclName)
-	}
-	if tags.ObjectDefinition.Type != resourcemanager.TerraformSchemaFieldTypeTags {
-		t.Errorf("expected the field 'Tags' to have the type `tags` but got %q", string(tags.ObjectDefinition.Type))
-	}
-	if !tags.Optional {
-		t.Errorf("expected the field 'Tags' to be Optional but it wasn't")
-	}
-	// TODO: source WriteOnly from the mappings
-	//if tags.Required || tags.Computed || tags.ForceNew || tags.WriteOnly {
-	//	t.Errorf("expected the field 'Tags' to be !Required, !Computed, !ForceNew and !WriteOnly but got %t / %t / %t / %t", tags.Required, tags.Computed, tags.ForceNew, tags.WriteOnly)
-	//}
-	if tags.Required || tags.Computed || tags.ForceNew {
-		t.Errorf("expected the field 'Tags' to be !Required, !Computed, !ForceNew but got %t / / %t / %t", tags.Required, tags.Computed, tags.ForceNew)
-	}
-
-	createMode, ok := topLevelModel.Fields["CreateMode"]
-	if !ok {
-		t.Errorf("expected there to be a field 'CreateMode' but didn't get one")
-	}
-	if createMode.HclName != "create_mode" {
-		t.Errorf("expected the HclName for field 'CreateMode' to be 'create_mode' but got %q", createMode.HclName)
-	}
-	if createMode.ObjectDefinition.Type != resourcemanager.TerraformSchemaFieldTypeReference {
-		t.Errorf("expected the field 'CreateMode' to have the type `reference` but got %q", string(createMode.ObjectDefinition.Type))
-	}
-	if createMode.ObjectDefinition.ReferenceName == nil {
-		t.Errorf("expected field 'Createmode' to have a reference to an Enum, but got 'nil'")
-	}
-	if *createMode.ObjectDefinition.ReferenceName != "CreateMode" {
-		t.Errorf("expected field 'Createmode' to reference model 'CreateMode', but got %q", *createMode.ObjectDefinition.ReferenceName)
-	}
-	if !createMode.Optional {
-		t.Errorf("expected the field 'CreateMode' to be Optional but it wasn't")
-	}
-	if !createMode.ForceNew {
-		// TODO - This should be ForceNew since it cannot be update post creation? Switch to t.Errorf later.
-		t.Errorf("expected the field 'CreateMode' to be ForceNew but it wasn't")
-	}
-
-	// TODO: source WriteOnly from the mappings
-	//if createMode.Optional || createMode.Computed || createMode.WriteOnly {
-	//	t.Errorf("expected the field 'createMode' to be !Optional, !Computed and !WriteOnly but got %t / %t / %t", createMode.Optional, createMode.Computed, createMode.WriteOnly)
-	//}
-
-	if createMode.Required || createMode.Computed {
-		t.Errorf("expected the field 'CreateMode' to be Optional, !Computed but got %t / %t", createMode.Optional, createMode.Computed)
-	}
-
-	// CreationDate - date / string?
-	creationDate, ok := topLevelModel.Fields["CreationDate"]
-	if !ok {
-		t.Errorf("expected there to be a field 'Name' but didn't get one")
-	}
-	if creationDate.HclName != "creation_date" {
-		t.Errorf("expected the HclName for field 'CreationDate' to be 'creation_date' but got %q", name.HclName)
-	}
-	if creationDate.ObjectDefinition.Type != resourcemanager.TerraformSchemaFieldTypeDateTime {
-		t.Errorf("expected the field 'CreationDate' to have the type `DateTime` but got %q", string(creationDate.ObjectDefinition.Type))
-	}
-
-	if creationDate.Required || creationDate.Optional {
-		// TODO - This should be Computed only, but coming through as Optional?
-		t.Errorf("expected the field 'CreationDate' to be !Required and !Optional but it got %t / %t", creationDate.Required, creationDate.Optional)
-	}
-
-	// TODO: source WriteOnly from the mappings
-	//if creationDate.Optional || creationDate.Required || creationDate.WriteOnly {
-	//	t.Errorf("expected the field 'CreationDate' to be !Optional, !Required and !WriteOnly but got %t / %t / %t", creationDate.Optional, creationDate.Computed, creationDate.WriteOnly)
-	//}
-
-	if !creationDate.Computed {
-		// TODO - This should be Computed only, but coming through as Optional?
-		t.Errorf("expected the field 'CreationDate' to be Computed but got %t", creationDate.Computed)
-	}
-
-	disableLocalAuth, ok := topLevelModel.Fields["DisableLocalAuth"]
-	if !ok {
-		t.Errorf("expected there to be a field 'DisableLocalAuth' but didn't get one")
-	}
-	if disableLocalAuth.HclName != "local_auth_disabled" {
-		// TODO - switch for t.Errorf when rename rule in place
-		t.Errorf("expected the HclName for field 'DisableLocalAuth' to be 'local_auth_disabled' but got %q", disableLocalAuth.HclName)
-	}
-	if disableLocalAuth.ObjectDefinition.Type != resourcemanager.TerraformSchemaFieldTypeBoolean {
-		t.Errorf("expected the field 'DisableLocalAuth' to have the type `bool` but got %q", string(disableLocalAuth.ObjectDefinition.Type))
-	}
-	if !disableLocalAuth.Optional {
-		t.Errorf("expected the field 'DisableLocalAuth' to be Optional but it wasn't")
-	}
-	if !disableLocalAuth.ForceNew {
-		t.Errorf("expected the field 'DisableLocalAuth' to be ForceNew but it wasn't")
-	}
-
-	// TODO: source WriteOnly from the mappings
-	//if name.Optional || name.Computed || name.WriteOnly {
-	//	t.Errorf("expected the field 'DisableLocalAuth' to be !Optional, !Computed and !WriteOnly but got %t / %t / %t", name.Optional, name.Computed, name.WriteOnly)
-	//}
-
-	if !disableLocalAuth.Optional {
-		t.Errorf("expected the field 'DisableLocalAuth' to be Optional but it wasn't")
-	}
-
-	if disableLocalAuth.Computed {
-		t.Errorf("expected the field 'DisableLocalAuth' to be !Computed but it was")
-	}
-
-	enablePurgeProtection, ok := topLevelModel.Fields["EnablePurgeProtection"]
-	if !ok {
-		t.Errorf("expected there to be a field 'EnablePurgeProtection' but didn't get one")
-	}
-	if enablePurgeProtection.HclName != "purge_protection_enabled" {
-		// TODO - switch for t.Errorf when rename rule in place
-		t.Errorf("expected the HclName for field 'EnablePurgeProtection' to be 'purge_protection_enabled' but got %q", enablePurgeProtection.HclName)
-	}
-	if enablePurgeProtection.ObjectDefinition.Type != resourcemanager.TerraformSchemaFieldTypeBoolean {
-		t.Errorf("expected the field 'EnablePurgeProtection' to have the type `bool` but got %q", string(enablePurgeProtection.ObjectDefinition.Type))
-	}
-	if !enablePurgeProtection.Optional {
-		t.Errorf("expected the field 'EnablePurgeProtection' to be Optional but it wasn't")
-	}
-	if !enablePurgeProtection.ForceNew {
-		t.Errorf("expected the field 'EnablePurgeProtection' to be ForceNew but it is")
-	}
-
-	// TODO: source WriteOnly from the mappings
-	//if name.Required || name.Computed || name.WriteOnly {
-	//	t.Errorf("expected the field 'EnablePurgeProtection' to be !Required, !Computed and !WriteOnly but got %t / %t / %t", name.Optional, name.Computed, name.WriteOnly)
-	//}
-
-	if !enablePurgeProtection.Optional {
-		t.Errorf("expected the field 'EnablePurgeProtection' to be Optional, but it wasn't")
-	}
-
-	if enablePurgeProtection.Computed {
-		t.Errorf("expected the field 'EnablePurgeProtection' to be !Computed but it was")
-	}
-
-	// ProvisioningState - should be filtered out of the TF
-	_, ok = topLevelModel.Fields["ProvisioningState"]
-	if ok {
-		t.Errorf("expected there not to be a field 'ProvisioningState' but found one")
+		r.checkField(t, currentModel, expected{
+			FieldName: "SoftDeleteRetentionInDays",
+			HclName:   "soft_delete_retention_in_days",
+			Optional:  true,
+			FieldType: resourcemanager.TerraformSchemaFieldTypeInteger,
+		})
 	}
 
 	// SoftDeleteRetentionInDays - int
-
-	softDeleteRetentionInDays, ok := topLevelModel.Fields["SoftDeleteRetentionInDays"]
-	if !ok {
-		t.Errorf("expected there to be a field 'SoftDeleteRetentionInDays' but didn't get one")
-	}
-	if softDeleteRetentionInDays.HclName != "soft_delete_retention_in_days" {
-		t.Errorf("expected the HclName for field 'SoftDeleteRetentionInDays' to be 'soft_delete_retention_in_days' but got %q", softDeleteRetentionInDays.HclName)
-	}
-	if softDeleteRetentionInDays.ObjectDefinition.Type != resourcemanager.TerraformSchemaFieldTypeInteger {
-		t.Errorf("expected the field 'SoftDeleteRetentionInDays' to have the type `int` but got %q", string(softDeleteRetentionInDays.ObjectDefinition.Type))
-	}
-	if softDeleteRetentionInDays.Required {
-		t.Errorf("expected the field 'SoftDeleteRetentionInDays' to be !Required but it was")
-	}
-	if softDeleteRetentionInDays.ForceNew {
-		t.Errorf("expected the field 'SoftDeleteRetentionInDays' to be !ForceNew but it was")
-	}
-
-	// TODO: source WriteOnly from the mappings
-	//if softDeleteRetentionInDays.Optional || softDeleteRetentionInDays.Computed || softDeleteRetentionInDays.WriteOnly {
-	//	t.Errorf("expected the field 'SoftDeleteRetentionInDays' to be !Optional, !Computed and !WriteOnly but got %t / %t / %t", softDeleteRetentionInDays.Optional, softDeleteRetentionInDays.Computed, softDeleteRetentionInDays.WriteOnly)
-	//}
-
-	if softDeleteRetentionInDays.Optional || softDeleteRetentionInDays.Computed {
-		t.Errorf("expected the field 'SoftDeleteRetentionInDays' to be !Optional, !Computed but got %t / %t", softDeleteRetentionInDays.Optional, softDeleteRetentionInDays.Computed)
-	}
 
 	// TODO - Nested Blocks...
 	// Encryption
