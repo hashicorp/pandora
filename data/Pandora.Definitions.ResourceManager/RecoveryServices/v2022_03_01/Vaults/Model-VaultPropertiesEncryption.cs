2e5bdbc80301e2a5210bcdbb70f04f15da10551f
hc-github-team-tf-azure <>
2022-09-20 12:57:41 +0000

    data: regenerating based on the latest Swagger

---
+++ data/Pandora.Definitions.ResourceManager/RecoveryServices/v2022_03_01/Vaults/Model-VaultPropertiesEncryption.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using Pandora.Definitions.Attributes;
+using Pandora.Definitions.Attributes.Validation;
+using Pandora.Definitions.CustomTypes;
+
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+
+namespace Pandora.Definitions.ResourceManager.RecoveryServices.v2022_03_01.Vaults;
+
+
+internal class VaultPropertiesEncryptionModel
+{
+    [JsonPropertyName("infrastructureEncryption")]
+    public InfrastructureEncryptionStateConstant? InfrastructureEncryption { get; set; }
+
+    [JsonPropertyName("kekIdentity")]
+    public CmkKekIdentityModel? KekIdentity { get; set; }
+
+    [JsonPropertyName("keyVaultProperties")]
+    public CmkKeyVaultPropertiesModel? KeyVaultProperties { get; set; }
+}
---
+++ data/Pandora.Definitions.ResourceManager/RecoveryServices/v2022_03_01/Vaults/Model-VaultPropertiesMoveDetails.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using Pandora.Definitions.Attributes;
+using Pandora.Definitions.Attributes.Validation;
+using Pandora.Definitions.CustomTypes;
+
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+
+namespace Pandora.Definitions.ResourceManager.RecoveryServices.v2022_03_01.Vaults;
+
+
+internal class VaultPropertiesMoveDetailsModel
+{
+    [DateFormat(DateFormatAttribute.DateFormat.RFC3339)]
+    [JsonPropertyName("completionTimeUtc")]
+    public DateTime? CompletionTimeUtc { get; set; }
+
+    [JsonPropertyName("operationId")]
+    public string? OperationId { get; set; }
+
+    [JsonPropertyName("sourceResourceId")]
+    public string? SourceResourceId { get; set; }
+
+    [DateFormat(DateFormatAttribute.DateFormat.RFC3339)]
+    [JsonPropertyName("startTimeUtc")]
+    public DateTime? StartTimeUtc { get; set; }
+
+    [JsonPropertyName("targetResourceId")]
+    public string? TargetResourceId { get; set; }
+}
