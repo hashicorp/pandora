14a9a0d66dedb9456ae3e9b39c56772cff944048
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-09-23 10:16:36 +0200

    data: removing the VM / VMSS resources for now
    These conflict/error since there's matching block names:
    https://github.com/hashicorp/pandora/actions/runs/3111154967/jobs/5043119797#step:5:121

--- data/Pandora.Definitions.ResourceManager/Compute/ServiceDefinition.cs
+++ data/Pandora.Definitions.ResourceManager/Compute/ServiceDefinition.cs
@@ -16,7 +16,5 @@ public partial class Service : ServiceDefinition
 
     public IEnumerable<TerraformResourceDefinition> TerraformResources => new List<TerraformResourceDefinition>
     {
-        new Terraform.VirtualMachineResource(),
-new Terraform.VirtualMachineScaleSetResource(),
     };
 }
