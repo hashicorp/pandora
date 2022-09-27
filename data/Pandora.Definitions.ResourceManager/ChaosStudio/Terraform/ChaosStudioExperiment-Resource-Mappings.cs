9a1186d65f9275572f67b33ab7dacb60c865a366
hc-github-team-tf-azure <>
2022-09-27 15:16:06 +0000

    data: regenerating based on the latest Swagger

--- data/Pandora.Definitions.ResourceManager/ChaosStudio/Terraform/ChaosStudioExperiment-Resource-Mappings.cs
+++ data/Pandora.Definitions.ResourceManager/ChaosStudio/Terraform/ChaosStudioExperiment-Resource-Mappings.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using Pandora.Definitions.Interfaces;
 using Pandora.Definitions.Mappings;
-// using Pandora.Definitions.ResourceManager.Compute.v2021_11_01.VirtualMachineScaleSets;
+using Pandora.Definitions.ResourceManager.ChaosStudio.v2022_07_01_preview.Experiments;
 
 namespace Pandora.Definitions.ResourceManager.ChaosStudio.Terraform;
 
@@ -10,13 +10,26 @@ public class ChaosStudioExperimentResourceMappings : TerraformMappingDefinition
     public List<MappingType> Mappings => new List<MappingType>
     {
         Mapping.FromSchema<ChaosStudioExperimentResourceSchema>(s => s.Name).ToResourceIdSegmentNamed("experimentName"),
-Mapping.FromSchema<ChaosStudioExperimentResourceSchema>(s => s.ResourceGroupName).ToResourceIdSegmentNamed("resourceGroupName"),
+        Mapping.FromSchema<ChaosStudioExperimentResourceSchema>(s => s.ResourceGroupName).ToResourceIdSegmentNamed("resourceGroupName"),
 
-        // Mapping.FromSchema<ChaosStudioExperimentResourceSchema>(s => s.Location).ToSdkField<ChaosStudioExperimentModel>(m => m.Location),
-        // Mapping.FromSchema<ChaosStudioExperimentResourceSchema>(s => s.Tags).ToSdkField<ChaosStudioExperimentModel>(m => m.Tags),
-        // Mapping.FromSchema<ChaosStudioExperimentResourceSchema>(s => s.UniqueId).ToSdkField<ChaosStudioExperimentModel>(m => m.Properties.UniqueId),
+        Mapping.FromSchema<ChaosStudioExperimentResourceSchema>(s => s.Identity).ToSdkField<ExperimentModel>(m => m.Identity).Direct(),
+        Mapping.FromSchema<ChaosStudioExperimentResourceSchema>(s => s.Location).ToSdkField<ExperimentModel>(m => m.Location).Direct(),
+        Mapping.FromSchema<ChaosStudioExperimentResourceSchema>(s => s.Selectors).ToSdkField<ExperimentPropertiesModel>(m => m.Selector).Direct(),
+        Mapping.FromSchema<ChaosStudioExperimentResourceSchema>(s => s.StartOnCreation).ToSdkField<ExperimentPropertiesModel>(m => m.StartOnCreation).Direct(),
+        Mapping.FromSchema<ChaosStudioExperimentResourceSchema>(s => s.Steps).ToSdkField<ExperimentPropertiesModel>(m => m.Step).Direct(),
+        Mapping.FromSchema<ChaosStudioExperimentResourceSchema>(s => s.Tags).ToSdkField<ExperimentModel>(m => m.Tags).Direct(),
 
-        // Mapping.FromSchema<ChaosStudioExperimentResourceSchema>(s => s.DataDisks).ToSdkModel<ChaosStudioExperimentDataDiskModel>(),
-        // Mapping.FromSchema<ChaosStudioExperimentDataDiskSchemaModel>(s => s.Name).ToSdkField<ChaosStudioExperimentDataDiskModel>(m => m.Name),
+        Mapping.FromSchema<ChaosStudioExperimentResourceSchema>(s => s.Identity).ToSdkField<ExperimentModel>(m => m.Identity).Direct(),
+        Mapping.FromSchema<ChaosStudioExperimentResourceSchema>(s => s.Selectors).ToSdkField<ExperimentPropertiesModel>(m => m.Selector).Direct(),
+        Mapping.FromSchema<ChaosStudioExperimentResourceSchema>(s => s.StartOnCreation).ToSdkField<ExperimentPropertiesModel>(m => m.StartOnCreation).Direct(),
+        Mapping.FromSchema<ChaosStudioExperimentResourceSchema>(s => s.Steps).ToSdkField<ExperimentPropertiesModel>(m => m.Step).Direct(),
+        Mapping.FromSchema<ChaosStudioExperimentResourceSchema>(s => s.Tags).ToSdkField<ExperimentModel>(m => m.Tags).Direct(),
+
+        Mapping.FromSchema<ChaosStudioExperimentResourceSchema>(s => s.Identity).ToSdkField<ExperimentModel>(m => m.Identity).Direct(),
+        Mapping.FromSchema<ChaosStudioExperimentResourceSchema>(s => s.Location).ToSdkField<ExperimentModel>(m => m.Location).Direct(),
+        Mapping.FromSchema<ChaosStudioExperimentResourceSchema>(s => s.Selectors).ToSdkField<ExperimentPropertiesModel>(m => m.Selector).Direct(),
+        Mapping.FromSchema<ChaosStudioExperimentResourceSchema>(s => s.StartOnCreation).ToSdkField<ExperimentPropertiesModel>(m => m.StartOnCreation).Direct(),
+        Mapping.FromSchema<ChaosStudioExperimentResourceSchema>(s => s.Steps).ToSdkField<ExperimentPropertiesModel>(m => m.Step).Direct(),
+        Mapping.FromSchema<ChaosStudioExperimentResourceSchema>(s => s.Tags).ToSdkField<ExperimentModel>(m => m.Tags).Direct(),
     };
 }
--- data/Pandora.Definitions.ResourceManager/ElasticSan/Terraform/ElasticSan-Resource-Mappings.cs
+++ data/Pandora.Definitions.ResourceManager/ElasticSan/Terraform/ElasticSan-Resource-Mappings.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using Pandora.Definitions.Interfaces;
 using Pandora.Definitions.Mappings;
-// using Pandora.Definitions.ResourceManager.Compute.v2021_11_01.VirtualMachineScaleSets;
+using Pandora.Definitions.ResourceManager.ElasticSan.v2021_11_20_preview.ElasticSans;
 
 namespace Pandora.Definitions.ResourceManager.ElasticSan.Terraform;
 
@@ -10,13 +10,34 @@ public class ElasticSanResourceMappings : TerraformMappingDefinition
     public List<MappingType> Mappings => new List<MappingType>
     {
         Mapping.FromSchema<ElasticSanResourceSchema>(s => s.Name).ToResourceIdSegmentNamed("elasticSanName"),
-Mapping.FromSchema<ElasticSanResourceSchema>(s => s.ResourceGroupName).ToResourceIdSegmentNamed("resourceGroupName"),
+        Mapping.FromSchema<ElasticSanResourceSchema>(s => s.ResourceGroupName).ToResourceIdSegmentNamed("resourceGroupName"),
 
-        // Mapping.FromSchema<ElasticSanResourceSchema>(s => s.Location).ToSdkField<ElasticSanModel>(m => m.Location),
-        // Mapping.FromSchema<ElasticSanResourceSchema>(s => s.Tags).ToSdkField<ElasticSanModel>(m => m.Tags),
-        // Mapping.FromSchema<ElasticSanResourceSchema>(s => s.UniqueId).ToSdkField<ElasticSanModel>(m => m.Properties.UniqueId),
+        Mapping.FromSchema<ElasticSanResourceSchema>(s => s.AvailabilityZones).ToSdkField<ElasticSanPropertiesModel>(m => m.AvailabilityZones).Direct(),
+        Mapping.FromSchema<ElasticSanResourceSchema>(s => s.BaseSizeTiB).ToSdkField<ElasticSanPropertiesModel>(m => m.BaseSizeTiB).Direct(),
+        Mapping.FromSchema<ElasticSanResourceSchema>(s => s.ExtendedCapacitySizeTiB).ToSdkField<ElasticSanPropertiesModel>(m => m.ExtendedCapacitySizeTiB).Direct(),
+        Mapping.FromSchema<ElasticSanResourceSchema>(s => s.Location).ToSdkField<ElasticSanModel>(m => m.Location).Direct(),
+        Mapping.FromSchema<ElasticSanResourceSchema>(s => s.Sku).ToSdkField<ElasticSanPropertiesModel>(m => m.Sku).Direct(),
+        Mapping.FromSchema<ElasticSanResourceSchema>(s => s.Tags).ToSdkField<ElasticSanModel>(m => m.Tags).Direct(),
+        Mapping.FromSchema<ElasticSanResourceSchema>(s => s.TotalIops).ToSdkField<ElasticSanPropertiesModel>(m => m.TotalIops).Direct(),
+        Mapping.FromSchema<ElasticSanResourceSchema>(s => s.TotalMBps).ToSdkField<ElasticSanPropertiesModel>(m => m.TotalMBps).Direct(),
+        Mapping.FromSchema<ElasticSanResourceSchema>(s => s.TotalSizeTiB).ToSdkField<ElasticSanPropertiesModel>(m => m.TotalSizeTiB).Direct(),
+        Mapping.FromSchema<ElasticSanResourceSchema>(s => s.TotalVolumeSizeGiB).ToSdkField<ElasticSanPropertiesModel>(m => m.TotalVolumeSizeGiB).Direct(),
+        Mapping.FromSchema<ElasticSanResourceSchema>(s => s.VolumeGroupCount).ToSdkField<ElasticSanPropertiesModel>(m => m.VolumeGroupCount).Direct(),
 
-        // Mapping.FromSchema<ElasticSanResourceSchema>(s => s.DataDisks).ToSdkModel<ElasticSanDataDiskModel>(),
-        // Mapping.FromSchema<ElasticSanDataDiskSchemaModel>(s => s.Name).ToSdkField<ElasticSanDataDiskModel>(m => m.Name),
+        Mapping.FromSchema<ElasticSanResourceSchema>(s => s.BaseSizeTiB).ToSdkField<ElasticSanUpdatePropertiesModel>(m => m.BaseSizeTiB).Direct(),
+        Mapping.FromSchema<ElasticSanResourceSchema>(s => s.ExtendedCapacitySizeTiB).ToSdkField<ElasticSanUpdatePropertiesModel>(m => m.ExtendedCapacitySizeTiB).Direct(),
+        Mapping.FromSchema<ElasticSanResourceSchema>(s => s.Tags).ToSdkField<ElasticSanUpdateModel>(m => m.Tags).Direct(),
+
+        Mapping.FromSchema<ElasticSanResourceSchema>(s => s.AvailabilityZones).ToSdkField<ElasticSanPropertiesModel>(m => m.AvailabilityZones).Direct(),
+        Mapping.FromSchema<ElasticSanResourceSchema>(s => s.BaseSizeTiB).ToSdkField<ElasticSanPropertiesModel>(m => m.BaseSizeTiB).Direct(),
+        Mapping.FromSchema<ElasticSanResourceSchema>(s => s.ExtendedCapacitySizeTiB).ToSdkField<ElasticSanPropertiesModel>(m => m.ExtendedCapacitySizeTiB).Direct(),
+        Mapping.FromSchema<ElasticSanResourceSchema>(s => s.Location).ToSdkField<ElasticSanModel>(m => m.Location).Direct(),
+        Mapping.FromSchema<ElasticSanResourceSchema>(s => s.Sku).ToSdkField<ElasticSanPropertiesModel>(m => m.Sku).Direct(),
+        Mapping.FromSchema<ElasticSanResourceSchema>(s => s.Tags).ToSdkField<ElasticSanModel>(m => m.Tags).Direct(),
+        Mapping.FromSchema<ElasticSanResourceSchema>(s => s.TotalIops).ToSdkField<ElasticSanPropertiesModel>(m => m.TotalIops).Direct(),
+        Mapping.FromSchema<ElasticSanResourceSchema>(s => s.TotalMBps).ToSdkField<ElasticSanPropertiesModel>(m => m.TotalMBps).Direct(),
+        Mapping.FromSchema<ElasticSanResourceSchema>(s => s.TotalSizeTiB).ToSdkField<ElasticSanPropertiesModel>(m => m.TotalSizeTiB).Direct(),
+        Mapping.FromSchema<ElasticSanResourceSchema>(s => s.TotalVolumeSizeGiB).ToSdkField<ElasticSanPropertiesModel>(m => m.TotalVolumeSizeGiB).Direct(),
+        Mapping.FromSchema<ElasticSanResourceSchema>(s => s.VolumeGroupCount).ToSdkField<ElasticSanPropertiesModel>(m => m.VolumeGroupCount).Direct(),
     };
 }
