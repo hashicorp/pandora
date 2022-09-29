29cd033e33ee4d9ae74eea58e488828706007a89
hc-github-team-tf-azure <>
2022-09-29 11:52:37 +0000

    data: regenerating based on the latest Swagger

--- data/Pandora.Definitions.ResourceManager/ChaosStudio/Terraform/ChaosStudioExperiment-Resource-Mappings.cs
+++ data/Pandora.Definitions.ResourceManager/ChaosStudio/Terraform/ChaosStudioExperiment-Resource-Mappings.cs
@@ -12,11 +12,22 @@ public class ChaosStudioExperimentResourceMappings : TerraformMappingDefinition
         Mapping.FromSchema<ChaosStudioExperimentResourceSchema>(s => s.Name).ToResourceIdSegmentNamed("experimentName"),
         Mapping.FromSchema<ChaosStudioExperimentResourceSchema>(s => s.ResourceGroupName).ToResourceIdSegmentNamed("resourceGroupName"),
 
+        Mapping.FromSchema<ChaosStudioExperimentResourceActionSchema>(s => s.Name).ToSdkField<ActionModel>(m => m.Name).Direct(),
+        Mapping.FromSchema<ChaosStudioExperimentResourceActionSchema>(s => s.Type).ToSdkField<ActionModel>(m => m.Type).Direct(),
+        Mapping.FromSchema<ChaosStudioExperimentResourceBranchSchema>(s => s.Action).ToSdkField<BranchModel>(m => m.Actions).Direct(),
+        Mapping.FromSchema<ChaosStudioExperimentResourceBranchSchema>(s => s.Name).ToSdkField<BranchModel>(m => m.Name).Direct(),
         Mapping.FromSchema<ChaosStudioExperimentResourceSchema>(s => s.Identity).ToSdkField<ExperimentModel>(m => m.Identity).Direct(),
         Mapping.FromSchema<ChaosStudioExperimentResourceSchema>(s => s.Location).ToSdkField<ExperimentModel>(m => m.Location).Direct(),
         Mapping.FromSchema<ChaosStudioExperimentResourceSchema>(s => s.Selector).ToSdkField<ExperimentPropertiesModel>(m => m.Selectors).Direct(),
         Mapping.FromSchema<ChaosStudioExperimentResourceSchema>(s => s.StartOnCreation).ToSdkField<ExperimentPropertiesModel>(m => m.StartOnCreation).Direct(),
         Mapping.FromSchema<ChaosStudioExperimentResourceSchema>(s => s.Step).ToSdkField<ExperimentPropertiesModel>(m => m.Steps).Direct(),
         Mapping.FromSchema<ChaosStudioExperimentResourceSchema>(s => s.Tags).ToSdkField<ExperimentModel>(m => m.Tags).Direct(),
+        Mapping.FromSchema<ChaosStudioExperimentResourceSelectorSchema>(s => s.Id).ToSdkField<SelectorModel>(m => m.Id).Direct(),
+        Mapping.FromSchema<ChaosStudioExperimentResourceSelectorSchema>(s => s.Target).ToSdkField<SelectorModel>(m => m.Targets).Direct(),
+        Mapping.FromSchema<ChaosStudioExperimentResourceSelectorSchema>(s => s.Type).ToSdkField<SelectorModel>(m => m.Type).Direct(),
+        Mapping.FromSchema<ChaosStudioExperimentResourceStepSchema>(s => s.Branch).ToSdkField<StepModel>(m => m.Branches).Direct(),
+        Mapping.FromSchema<ChaosStudioExperimentResourceStepSchema>(s => s.Name).ToSdkField<StepModel>(m => m.Name).Direct(),
+        Mapping.FromSchema<ChaosStudioExperimentResourceTargetReferenceSchema>(s => s.Id).ToSdkField<TargetReferenceModel>(m => m.Id).Direct(),
+        Mapping.FromSchema<ChaosStudioExperimentResourceTargetReferenceSchema>(s => s.Type).ToSdkField<TargetReferenceModel>(m => m.Type).Direct(),
     };
 }
