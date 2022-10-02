345c443985de0cfdf8fc102023201a502d39964c
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-10-02 12:53:09 +0200

    tools/generator-terraform: instantiating the flattened object prior to passing it into the set function if it's nil

--- tools/generator-terraform/generator/mappings/assignment_model_to_model.go
+++ tools/generator-terraform/generator/mappings/assignment_model_to_model.go
@@ -11,7 +11,7 @@ var _ assignmentType = modelToModelAssignmentLine{}
 type modelToModelAssignmentLine struct {
 }
 
-func (m modelToModelAssignmentLine) assignmentForCreateUpdateMapping(mapping resourcemanager.FieldMappingDefinition, _ resourcemanager.TerraformSchemaModelDefinition, sdkModel resourcemanager.ModelDetails, sdkConstant *assignmentConstantDetails, apiResourcePackageName string) (*string, error) {
+func (m modelToModelAssignmentLine) assignmentForCreateUpdateMapping(mapping resourcemanager.FieldMappingDefinition, _ resourcemanager.TerraformSchemaModelDefinition, sdkModel resourcemanager.ModelDetails, _ *assignmentConstantDetails, apiResourcePackageName string) (*string, error) {
 	if mapping.Type != resourcemanager.ModelToModelMappingDefinitionType {
 		return nil, fmt.Errorf("expected a ModelToModel Mapping but got %q", string(mapping.Type))
 	}
@@ -24,6 +24,10 @@ func (m modelToModelAssignmentLine) assignmentForCreateUpdateMapping(mapping res
 		return nil, fmt.Errorf("a ModelToModel mapping must be a Reference but got %q", string(sdkField.ObjectDefinition.Type))
 	}
 	outputModelName := *sdkField.ObjectDefinition.ReferenceName
+	sdkFieldType, err := sdkField.ObjectDefinition.GolangTypeName(&apiResourcePackageName)
+	if err != nil {
+		return nil, fmt.Errorf("determining Golang Type Name for SDK Field: %+v", err)
+	}
 
 	// the variable `output` is a pointer here, so we don't need to pass it by reference
 	output := fmt.Sprintf(`
@@ -33,10 +37,13 @@ func (m modelToModelAssignmentLine) assignmentForCreateUpdateMapping(mapping res
 `, mapping.ModelToModel.SchemaModelName, outputModelName, mapping.ModelToModel.SdkFieldName)
 	if sdkField.Optional {
 		output = fmt.Sprintf(`
+		if output.%[3]s == nil {
+			output.%[3]s = &%[4]s{}
+		}
 		if err := r.map%[1]sTo%[2]s(input, output.%[3]s); err != nil {
 			return fmt.Errorf("mapping Schema to SDK Field %%q / Model %%q: %%+v", %[2]q, %[3]q, err)
 		}
-`, mapping.ModelToModel.SchemaModelName, outputModelName, mapping.ModelToModel.SdkFieldName)
+`, mapping.ModelToModel.SchemaModelName, outputModelName, mapping.ModelToModel.SdkFieldName, *sdkFieldType)
 	}
 	return &output, nil
 }
@@ -56,7 +63,7 @@ func (m modelToModelAssignmentLine) assignmentForReadMapping(mapping resourceman
 	outputModelName := *sdkField.ObjectDefinition.ReferenceName
 	sdkFieldType, err := sdkField.ObjectDefinition.GolangTypeName(&apiResourcePackageName)
 	if err != nil {
-		return nil, fmt.Errorf("")
+		return nil, fmt.Errorf("determining Golang Type Name for SDK Field: %+v", err)
 	}
 
 	// the variable `output` is a pointer here, so we don't need to pass it by reference
