de499cfd91fb03900dce96ae58529d2babec02e3
hc-github-team-tf-azure <>
2022-10-25 09:22:10 +0000

    data: regenerating based on the latest Swagger

---
+++ data/Pandora.Definitions.ResourceManager/RecoveryServices/v2022_08_01/ApiVersionDefinition.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using Pandora.Definitions.Interfaces;
+
+namespace Pandora.Definitions.ResourceManager.RecoveryServices.v2022_08_01;
+
+public partial class Definition : ApiVersionDefinition
+{
+    public string ApiVersion => "2022-08-01";
+    public bool Preview => false;
+    public Source Source => Source.ResourceManagerRestApiSpecs;
+
+    public IEnumerable<ResourceDefinition> Resources => new List<ResourceDefinition>
+    {
+        new GetPrivateLinkResources.Definition(),
+        new ListPrivateLinkResources.Definition(),
+        new RecoveryServices.Definition(),
+        new RegisteredIdentities.Definition(),
+        new ReplicationUsages.Definition(),
+        new VaultCertificates.Definition(),
+        new VaultExtendedInfo.Definition(),
+        new VaultUsages.Definition(),
+        new Vaults.Definition(),
+    };
+}
---
+++ data/Pandora.Definitions.ResourceManager/RecoveryServices/v2022_08_01/VaultExtendedInfo/Model-VaultExtendedInfo.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using Pandora.Definitions.Attributes;
+using Pandora.Definitions.Attributes.Validation;
+using Pandora.Definitions.CustomTypes;
+
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+
+namespace Pandora.Definitions.ResourceManager.RecoveryServices.v2022_08_01.VaultExtendedInfo;
+
+
+internal class VaultExtendedInfoModel
+{
+    [JsonPropertyName("algorithm")]
+    public string? Algorithm { get; set; }
+
+    [JsonPropertyName("encryptionKey")]
+    public string? EncryptionKey { get; set; }
+
+    [JsonPropertyName("encryptionKeyThumbprint")]
+    public string? EncryptionKeyThumbprint { get; set; }
+
+    [JsonPropertyName("integrityKey")]
+    public string? IntegrityKey { get; set; }
+}
