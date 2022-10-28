f99f4aadde6ac39385656004b42946b51af6bfbe
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-10-28 11:55:08 +0200

    tools/importer-rest-api-specs: updating the ordering of `allow` and `allowed`, this means that `allowed` will no longer be trimmed to `ed`

--- tools/importer-rest-api-specs/components/schema/processors/field_name_rename_boolean.go
+++ tools/importer-rest-api-specs/components/schema/processors/field_name_rename_boolean.go
@@ -13,31 +13,32 @@ type fieldNameRenameBoolean struct{}
 
 func (fieldNameRenameBoolean) ProcessField(fieldName string, metadata FieldMetadata) (*string, error) {
 	if metadata.Model.Fields[fieldName].ObjectDefinition.Type == resourcemanager.BooleanApiObjectDefinitionType {
+		// change the Prefix -> Suffix
+		// NOTE: ordering matters due to `enabled` getting trimmed to `ed` if `enable` is trimmed before `enabled`
 		if strings.HasPrefix(strings.ToLower(fieldName), "enabled") {
 			updated := fmt.Sprintf("%sEnabled", fieldName[7:])
 			return &updated, nil
 		}
-		if strings.HasPrefix(strings.ToLower(fieldName), "disabled") {
-			updated := fmt.Sprintf("%sDisabled", fieldName[7:])
-			return &updated, nil
-		}
-		// flip `enable_X` / `disable_X` prefix
 		if strings.HasPrefix(strings.ToLower(fieldName), "enable") {
 			updated := fmt.Sprintf("%sEnabled", fieldName[6:])
+			return &updated, nil
+		}
+
+		if strings.HasPrefix(strings.ToLower(fieldName), "disabled") {
+			updated := fmt.Sprintf("%sDisabled", fieldName[7:])
 			return &updated, nil
 		}
 		if strings.HasPrefix(strings.ToLower(fieldName), "disable") {
 			updated := fmt.Sprintf("%sDisabled", fieldName[7:])
 			return &updated, nil
 		}
-		// change `allow_X` prefix -> `X_enabled`
-		if strings.HasPrefix(strings.ToLower(fieldName), "allow") {
-			updated := fmt.Sprintf("%sEnabled", fieldName[5:])
-			return &updated, nil
-		}
-		// change `allowed_X` prefix -> `X_enabled`
+
 		if strings.HasPrefix(strings.ToLower(fieldName), "allowed") {
 			updated := fmt.Sprintf("%sEnabled", fieldName[7:])
+			return &updated, nil
+		}
+		if strings.HasPrefix(strings.ToLower(fieldName), "allow") {
+			updated := fmt.Sprintf("%sEnabled", fieldName[5:])
 			return &updated, nil
 		}
 	}
