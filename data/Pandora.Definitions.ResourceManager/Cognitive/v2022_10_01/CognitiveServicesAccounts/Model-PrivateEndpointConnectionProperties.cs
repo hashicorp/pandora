68a279b0ecb868dcfab70f83d3e431229636e1d8
hc-github-team-tf-azure <>
2022-11-16 10:40:12 +0000

    data: regenerating based on the latest Swagger

---
+++ data/Pandora.Definitions.ResourceManager/Cognitive/v2022_10_01/CognitiveServicesAccounts/Model-PrivateEndpointConnectionProperties.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using Pandora.Definitions.Attributes;
+using Pandora.Definitions.Attributes.Validation;
+using Pandora.Definitions.CustomTypes;
+
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+
+namespace Pandora.Definitions.ResourceManager.Cognitive.v2022_10_01.CognitiveServicesAccounts;
+
+
+internal class PrivateEndpointConnectionPropertiesModel
+{
+    [JsonPropertyName("groupIds")]
+    public List<string>? GroupIds { get; set; }
+
+    [JsonPropertyName("privateEndpoint")]
+    public PrivateEndpointModel? PrivateEndpoint { get; set; }
+
+    [JsonPropertyName("privateLinkServiceConnectionState")]
+    [Required]
+    public PrivateLinkServiceConnectionStateModel PrivateLinkServiceConnectionState { get; set; }
+
+    [JsonPropertyName("provisioningState")]
+    public PrivateEndpointConnectionProvisioningStateConstant? ProvisioningState { get; set; }
+}
