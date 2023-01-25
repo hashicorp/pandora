1abb206b671e4a97c6fc57ce316ea00b421b876b
hc-github-team-tf-azure <>
2023-01-25 10:50:18 +0000

    data: regenerating based on the latest Swagger

--- data/Pandora.Definitions.ResourceManager/Resources/v2022_09_01/Deployments/ResourceId-DeploymentId.cs
+++ data/Pandora.Definitions.ResourceManager/Resources/v2022_09_01/Deployments/ResourceId-DeploymentId.cs
@@ -12,12 +12,14 @@ internal class DeploymentId : ResourceID
 {
     public string? CommonAlias => null;
 
-    public string ID => "/providers/Microsoft.Resources/deployments/{deploymentName}";
+    public string ID => "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/deployments/{deploymentName}";
 
     public List<ResourceIDSegment> Segments => new List<ResourceIDSegment>
     {
-        ResourceIDSegment.Static("staticProviders", "providers"),
-        ResourceIDSegment.ResourceProvider("staticMicrosoftResources", "Microsoft.Resources"),
+        ResourceIDSegment.Static("staticSubscriptions", "subscriptions"),
+        ResourceIDSegment.SubscriptionId("subscriptionId"),
+        ResourceIDSegment.Static("staticResourceGroups", "resourceGroups"),
+        ResourceIDSegment.ResourceGroup("resourceGroupName"),
         ResourceIDSegment.Static("staticDeployments", "deployments"),
         ResourceIDSegment.UserSpecified("deploymentName"),
     };
