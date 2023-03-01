8fc4a79bd5d492e99a36c4859b589322fe767b71
tombuildsstuff <git@tom.ibuildstuff.eu>
2023-03-01 10:12:47 +0100

    tools/generator-go-sdk: inverting the list of services using the old base layer

--- tools/generator-go-sdk/generator/settings.go
+++ tools/generator-go-sdk/generator/settings.go
@@ -5,24 +5,24 @@ import (
 )
 
 type Settings struct {
-	servicesUsingNewBaseLayer map[string]struct{}
+	servicesUsingOldBaseLayer map[string]struct{}
 }
 
-func (s *Settings) UseNewBaseLayerFor(serviceNames ...string) {
-	if s.servicesUsingNewBaseLayer == nil {
-		s.servicesUsingNewBaseLayer = map[string]struct{}{}
+func (s *Settings) UseOldBaseLayerFor(serviceNames ...string) {
+	if s.servicesUsingOldBaseLayer == nil {
+		s.servicesUsingOldBaseLayer = map[string]struct{}{}
 	}
 	for _, name := range serviceNames {
-		s.servicesUsingNewBaseLayer[name] = struct{}{}
+		s.servicesUsingOldBaseLayer[name] = struct{}{}
 	}
 }
 
 func (s *Settings) ShouldUseNewBaseLayer(serviceName, version string) bool {
-	if _, ok := s.servicesUsingNewBaseLayer[serviceName]; ok {
-		return true
+	if _, ok := s.servicesUsingOldBaseLayer[serviceName]; ok {
+		return false
 	}
-	if _, ok := s.servicesUsingNewBaseLayer[fmt.Sprintf("%s@%s", serviceName, version)]; ok {
-		return true
+	if _, ok := s.servicesUsingOldBaseLayer[fmt.Sprintf("%s@%s", serviceName, version)]; ok {
+		return false
 	}
-	return false
+	return true
 }
