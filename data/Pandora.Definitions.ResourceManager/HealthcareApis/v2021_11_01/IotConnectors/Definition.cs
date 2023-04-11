b0af2b523d75259c11f748668ddb837430952af1
hc-github-team-tf-azure <>
2023-04-11 15:52:29 +0000

    data: regenerating based on the latest Swagger

---
+++ data/Pandora.Definitions.ResourceManager/HealthcareApis/v2021_11_01/IotConnectors/Definition.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using Pandora.Definitions.Interfaces;
+
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+
+namespace Pandora.Definitions.ResourceManager.HealthcareApis.v2021_11_01.IotConnectors;
+
+internal class Definition : ResourceDefinition
+{
+    public string Name => "IotConnectors";
+    public IEnumerable<Interfaces.ApiOperation> Operations => new List<Interfaces.ApiOperation>
+    {
+        new CreateOrUpdateOperation(),
+        new DeleteOperation(),
+        new FhirDestinationsListByIotConnectorOperation(),
+        new GetOperation(),
+        new IotConnectorFhirDestinationCreateOrUpdateOperation(),
+        new IotConnectorFhirDestinationDeleteOperation(),
+        new IotConnectorFhirDestinationGetOperation(),
+        new ListByWorkspaceOperation(),
+        new UpdateOperation(),
+    };
+    public IEnumerable<System.Type> Constants => new List<System.Type>
+    {
+        typeof(IotIdentityResolutionTypeConstant),
+        typeof(ProvisioningStateConstant),
+    };
+    public IEnumerable<System.Type> Models => new List<System.Type>
+    {
+        typeof(IotConnectorModel),
+        typeof(IotConnectorPatchResourceModel),
+        typeof(IotConnectorPropertiesModel),
+        typeof(IotEventHubIngestionEndpointConfigurationModel),
+        typeof(IotFhirDestinationModel),
+        typeof(IotFhirDestinationPropertiesModel),
+        typeof(IotMappingPropertiesModel),
+    };
+}
