c0de48d0fe370b130c66ad96901154594d37e06d
hc-github-team-tf-azure <>
2023-04-19 11:09:51 +0000

    data: regenerating based on the latest Swagger

---
+++ data/Pandora.Definitions.ResourceManager/Advisor/v2023_01_01/AdvisorScore/Definition.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using Pandora.Definitions.Interfaces;
+
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+
+namespace Pandora.Definitions.ResourceManager.Advisor.v2023_01_01.AdvisorScore;
+
+internal class Definition : ResourceDefinition
+{
+    public string Name => "AdvisorScore";
+    public IEnumerable<Interfaces.ApiOperation> Operations => new List<Interfaces.ApiOperation>
+    {
+        new GetOperation(),
+        new ListOperation(),
+    };
+    public IEnumerable<System.Type> Constants => new List<System.Type>
+    {
+        typeof(AggregatedConstant),
+    };
+    public IEnumerable<System.Type> Models => new List<System.Type>
+    {
+        typeof(AdvisorScoreEntityModel),
+        typeof(AdvisorScoreEntityPropertiesModel),
+        typeof(AdvisorScoreResponseModel),
+        typeof(ScoreEntityModel),
+        typeof(TimeSeriesEntityTimeSeriesInlinedModel),
+    };
+}
---
+++ data/Pandora.Definitions.ResourceManager/DataProtection/v2023_05_01/ApiVersionDefinition-GenerationSetting.cs
@@ -0,0 +1,6 @@
+namespace Pandora.Definitions.ResourceManager.DataProtection.v2023_05_01;
+
+public partial class Definition
+{
+    public bool Generate => true;
+}
