79db700370b404cb4ee818f04a50e7e028c9b9b8
hc-github-team-tf-azure <>
2023-04-20 06:09:00 +0000

    data: regenerating based on the latest Swagger

---
+++ data/Pandora.Definitions.ResourceManager/Advisor/v2023_01_01/GenerateRecommendations/Definition.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using Pandora.Definitions.Interfaces;
+
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+
+namespace Pandora.Definitions.ResourceManager.Advisor.v2023_01_01.GenerateRecommendations;
+
+internal class Definition : ResourceDefinition
+{
+    public string Name => "GenerateRecommendations";
+    public IEnumerable<Interfaces.ApiOperation> Operations => new List<Interfaces.ApiOperation>
+    {
+        new RecommendationsGenerateOperation(),
+        new RecommendationsGetGenerateStatusOperation(),
+    };
+    public IEnumerable<System.Type> Constants => new List<System.Type>
+    {
+
+    };
+    public IEnumerable<System.Type> Models => new List<System.Type>
+    {
+
+    };
+}
