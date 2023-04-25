e0f9d8edf13707b38721fde7d06b61d5d6fa7b20
hc-github-team-tf-azure <>
2023-04-25 14:33:29 +0000

    data: regenerating based on the latest Swagger

---
+++ data/Pandora.Definitions.ResourceManager/HybridCompute/v2022_12_27/Machines/Constant-VMGuestPatchRebootStatus.cs
@@ -0,0 +1,26 @@
+using Pandora.Definitions.Attributes;
+using System.ComponentModel;
+
+namespace Pandora.Definitions.ResourceManager.HybridCompute.v2022_12_27.Machines;
+
+[ConstantType(ConstantTypeAttribute.ConstantType.String)]
+internal enum VMGuestPatchRebootStatusConstant
+{
+    [Description("Completed")]
+    Completed,
+
+    [Description("Failed")]
+    Failed,
+
+    [Description("NotNeeded")]
+    NotNeeded,
+
+    [Description("Required")]
+    Required,
+
+    [Description("Started")]
+    Started,
+
+    [Description("Unknown")]
+    Unknown,
+}
