c6611a6d29dcfff0842d385024c20e2198efb8ce
hc-github-team-tf-azure <>
2023-05-22 10:55:42 +0000

    data: regenerating based on the latest Swagger

---
+++ data/Pandora.Definitions.ResourceManager/KubernetesConfiguration/v2023_05_01/ApiVersionDefinition-GenerationSetting.cs
@@ -0,0 +1,6 @@
+namespace Pandora.Definitions.ResourceManager.KubernetesConfiguration.v2023_05_01;
+
+public partial class Definition
+{
+    public bool Generate => true;
+}
---
+++ data/Pandora.Definitions.ResourceManager/KubernetesConfiguration/v2023_05_01/ClusterExtensions/Definition.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using Pandora.Definitions.Interfaces;
+
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+
+namespace Pandora.Definitions.ResourceManager.KubernetesConfiguration.v2023_05_01.ClusterExtensions;
+
+internal class Definition : ResourceDefinition
+{
+    public string Name => "ClusterExtensions";
+    public IEnumerable<Interfaces.ApiOperation> Operations => new List<Interfaces.ApiOperation>
+    {
+        new ExtensionsCreateOperation(),
+        new ExtensionsDeleteOperation(),
+        new ExtensionsGetOperation(),
+        new ExtensionsListOperation(),
+        new ExtensionsUpdateOperation(),
+    };
+    public IEnumerable<System.Type> Constants => new List<System.Type>
+    {
+        typeof(AKSIdentityTypeConstant),
+        typeof(LevelTypeConstant),
+        typeof(ProvisioningStateConstant),
+    };
+    public IEnumerable<System.Type> Models => new List<System.Type>
+    {
+        typeof(ErrorAdditionalInfoModel),
+        typeof(ErrorDetailModel),
+        typeof(ExtensionModel),
+        typeof(ExtensionPropertiesModel),
+        typeof(ExtensionPropertiesAksAssignedIdentityModel),
+        typeof(ExtensionStatusModel),
+        typeof(PatchExtensionModel),
+        typeof(PatchExtensionPropertiesModel),
+        typeof(PlanModel),
+        typeof(ScopeModel),
+        typeof(ScopeClusterModel),
+        typeof(ScopeNamespaceModel),
+    };
+}
