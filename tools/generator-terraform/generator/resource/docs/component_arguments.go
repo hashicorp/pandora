3a0ee8a55f7e0863584cde1211659e48e513076f
tombuildsstuff <git@tom.ibuildstuff.eu>
2023-05-24 14:31:04 +0200

    refactor: the generation of each argument/line is now a separate function so this can be reused

--- tools/generator-terraform/generator/resource/docs/component_arguments.go
+++ tools/generator-terraform/generator/resource/docs/component_arguments.go
@@ -2,21 +2,12 @@ package docs
 
 import (
 	"fmt"
+	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 	"strings"
 
 	"github.com/hashicorp/pandora/tools/generator-terraform/generator/models"
 	"github.com/hashicorp/pandora/tools/sdk/resourcemanager"
 )
-
-var objectDefinitionsWhichShouldBeSurfacedAsBlocks = map[resourcemanager.TerraformSchemaFieldType]struct{}{
-	resourcemanager.TerraformSchemaFieldTypeReference: {},
-
-	// The Identity types should be output as blocks, since these are blocks within commonschema
-	resourcemanager.TerraformSchemaFieldTypeIdentitySystemAssigned:        {},
-	resourcemanager.TerraformSchemaFieldTypeIdentitySystemAndUserAssigned: {},
-	resourcemanager.TerraformSchemaFieldTypeIdentitySystemOrUserAssigned:  {},
-	resourcemanager.TerraformSchemaFieldTypeIdentityUserAssigned:          {},
-}
 
 func codeForArgumentsReference(input models.ResourceInput) (*string, error) {
 
@@ -38,74 +29,93 @@ The following arguments are supported:
 }
 
 func getArguments(model resourcemanager.TerraformSchemaModelDefinition, resourceName string) (*string, error) {
-
 	requiredLines := make([]string, 0)
 	optionalLines := make([]string, 0)
 
 	sortedFieldNames := sortFieldNamesAlphabetically(model)
 
+	// first process the Required Arguments
 	for _, fieldName := range sortedFieldNames {
 		field := model.Fields[fieldName]
 
-		// skip attributes
-		if field.Computed && !field.Optional && !field.Required {
+		// we only want Required attributes
+		if !field.Required || field.Optional || field.Computed {
 			continue
 		}
 
-		components := make([]string, 0)
-		components = append(components, fmt.Sprintf("* `%s` -", field.HclName))
-
-		if field.Required {
-			components = append(components, "(Required)")
-		} else if field.Optional {
-			components = append(components, "(Optional)")
+		docs, err := documentationLineForArgument(field, "")
+		if err != nil {
+			return nil, fmt.Errorf("building documentation line for required argument/field %q: %+v", fieldName, err)
 		}
-
-		// TODO: when it's a List/Set, we should output `A list of XXX` or `One or more of XXX` (or something)
-
-		// identify block
-		if _, ok := objectDefinitionsWhichShouldBeSurfacedAsBlocks[field.ObjectDefinition.Type]; ok {
-			fieldBeginsWithVowel, err := beginsWithVowel(field.HclName)
-			if err != nil {
-				return nil, err
-			}
-			if fieldBeginsWithVowel {
-				components = append(components, "An")
-			} else {
-				components = append(components, "A")
-			}
-			components = append(components, fmt.Sprintf("`%s` block as defined below.", field.HclName))
-		}
-
-		components = append(components, field.Documentation.Markdown)
-
-		// TODO update to include ranges
-		if field.ObjectDefinition.Type == resourcemanager.TerraformSchemaFieldTypeBoolean {
-			components = append(components, "Possible values are `true` and `false`.")
-		} else if field.Validation != nil {
-			if field.Validation.Type == resourcemanager.TerraformSchemaValidationTypePossibleValues {
-				if values := field.Validation.PossibleValues.Values; values != nil {
-					possibleValues := wordifyPossibleValues(values)
-					components = append(components, possibleValues)
-				}
-			}
-		}
-
-		// TODO set default if applicable?
-
-		if field.ForceNew {
-			components = append(components, fmt.Sprintf("Changing this forces a new %s to be created.", resourceName))
-		}
-
-		line := removeExtraSpaces(strings.Join(components, " "))
-
-		// sort required and optional
-		if field.Required {
-			requiredLines = append(requiredLines, line)
-		} else {
-			optionalLines = append(optionalLines, line)
-		}
+		requiredLines = append(requiredLines, *docs)
 	}
+
+	// then process the Optional Arguments
+	for _, fieldName := range sortedFieldNames {
+		field := model.Fields[fieldName]
+
+		// we only want Optional fields (which includes Optional + Computed fields)
+		if !field.Optional || field.Required {
+			continue
+		}
+
+		docs, err := documentationLineForArgument(field, resourceName)
+		if err != nil {
+			return nil, fmt.Errorf("building documentation line for optional argument/field %q: %+v", fieldName, err)
+		}
+		optionalLines = append(optionalLines, *docs)
+	}
+
 	out := strings.Join(append(requiredLines, optionalLines...), "\n\n")
 	return &out, nil
+}
+
+func documentationLineForArgument(field resourcemanager.TerraformSchemaFieldDefinition, resourceName string) (*string, error) {
+	components := make([]string, 0)
+	components = append(components, fmt.Sprintf("* `%s` -", field.HclName))
+
+	if field.Required {
+		components = append(components, "(Required)")
+	} else if field.Optional {
+		components = append(components, "(Optional)")
+	}
+
+	// TODO: when it's a List/Set, we should output `A list of XXX` or `One or more of XXX` (or something)
+
+	// identify block
+	if _, ok := objectDefinitionsWhichShouldBeSurfacedAsBlocks[field.ObjectDefinition.Type]; ok {
+		fieldBeginsWithVowel, err := beginsWithVowel(field.HclName)
+		if err != nil {
+			return nil, err
+		}
+		if fieldBeginsWithVowel {
+			components = append(components, "An")
+		} else {
+			components = append(components, "A")
+		}
+		components = append(components, fmt.Sprintf("`%s` block as defined below.", field.HclName))
+	}
+
+	components = append(components, field.Documentation.Markdown)
+
+	// TODO update to include ranges
+	if field.ObjectDefinition.Type == resourcemanager.TerraformSchemaFieldTypeBoolean {
+		components = append(components, "Possible values are `true` and `false`.")
+	} else if field.Validation != nil {
+		if field.Validation.Type == resourcemanager.TerraformSchemaValidationTypePossibleValues {
+			if values := field.Validation.PossibleValues.Values; values != nil {
+				possibleValues := wordifyPossibleValues(values)
+				components = append(components, possibleValues)
+			}
+		}
+	}
+
+	// TODO set default if applicable?
+
+	if field.ForceNew {
+		components = append(components, fmt.Sprintf("Changing this forces a new %s to be created.", resourceName))
+	}
+
+	line := removeExtraSpaces(strings.Join(components, " "))
+	return pointer.To(line), nil
 }
