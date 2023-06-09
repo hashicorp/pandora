1910349264546f039006bff98a2af7b0feea3307
tombuildsstuff <git@tom.ibuildstuff.eu>
2023-06-09 12:42:19 +0200

    tools/importer-rest-api-specs: refactoring the example usage generation
    This now parses the HCL blocks and updates the attributes

--- tools/importer-rest-api-specs/pipeline/task_generate_terraform_example_usage.go
+++ tools/importer-rest-api-specs/pipeline/task_generate_terraform_example_usage.go
@@ -2,11 +2,8 @@ package pipeline
 
 import (
 	"fmt"
-	"regexp"
-	"strings"
-
 	"github.com/hashicorp/go-hclog"
-	"github.com/hashicorp/hcl/v2/hclwrite"
+	"github.com/hashicorp/pandora/tools/importer-rest-api-specs/components/examples"
 	"github.com/hashicorp/pandora/tools/importer-rest-api-specs/models"
 	"github.com/hashicorp/pandora/tools/sdk/resourcemanager"
 )
@@ -19,8 +16,11 @@ func (t pipelineTask) generateTerraformExampleUsage(data *models.AzureApiDefinit
 
 			tfResources := make(map[string]resourcemanager.TerraformResourceDetails)
 			for resourceKey, resourceValue := range v.Terraform.Resources {
-				example := convertBasicTestToUsableExample(resourceValue.Tests)
-				resourceValue.Documentation.ExampleUsageHcl = example
+				example, err := examples.ResourceExampleFromTests(resourceValue.Tests)
+				if err != nil {
+					return nil, fmt.Errorf("building Example Usage from the Tests for Resource %q: %+v", resourceKey, err)
+				}
+				resourceValue.Documentation.ExampleUsageHcl = *example
 				tfResources[resourceKey] = resourceValue
 			}
 			v.Terraform.Resources = tfResources
@@ -29,46 +29,4 @@ func (t pipelineTask) generateTerraformExampleUsage(data *models.AzureApiDefinit
 	}
 	data.Resources = apiResources
 	return data, nil
-}
-
-func convertBasicTestToUsableExample(tests resourcemanager.TerraformResourceTestsDefinition) string {
-	example := tests.BasicConfiguration
-
-	example = switchOutTestSpecificValues(example)
-	example = switchOutLocalsUsage(example)
-
-	example = strings.TrimSpace(string(hclwrite.Format([]byte(example))))
-	return example
-}
-
-func switchOutLocalsUsage(input string) string {
-	output := input
-	re := regexp.MustCompile("[-][$][{](.*)[}]")
-	output = re.ReplaceAllLiteralString(output, "")
-
-	testVariables := map[string]string{
-		// TODO: add more
-		"random_integer":   "21",
-		"primary_location": "West Europe",
-	}
-	for variable, replacement := range testVariables {
-		output = strings.ReplaceAll(output, fmt.Sprintf("${local.%s}", variable), replacement)
-		output = strings.ReplaceAll(output, fmt.Sprintf("local.%s", variable), replacement)
-	}
-
-	output = strings.TrimSpace(string(hclwrite.Format([]byte(output))))
-	return output
-}
-
-func switchOutTestSpecificValues(input string) string {
-	output := input
-
-	output = strings.ReplaceAll(output, "acctest", "example")
-	output = strings.ReplaceAll(output, "acc", "example")
-	output = strings.ReplaceAll(output, "test", "example")
-
-	// azurerm_load_test is a special-case, which gets replaced with `azurerm_load_example`, so let's fix that
-	output = strings.ReplaceAll(output, "azurerm_load_example", "azurerm_load_test")
-
-	return output
 }
