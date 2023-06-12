aa0fe4582f658121eff312801e13a5eec661e14e
hc-github-team-tf-azure <>
2023-06-12 10:25:27 +0000

    data: regenerating based on the latest Swagger

---
+++ data/Pandora.Definitions.ResourceManager/Batch/v2023_05_01/BatchAccount/Constant-IPRuleAction.cs
@@ -0,0 +1,11 @@
+using Pandora.Definitions.Attributes;
+using System.ComponentModel;
+
+namespace Pandora.Definitions.ResourceManager.Batch.v2023_05_01.BatchAccount;
+
+[ConstantType(ConstantTypeAttribute.ConstantType.String)]
+internal enum IPRuleActionConstant
+{
+    [Description("Allow")]
+    Allow,
+}
