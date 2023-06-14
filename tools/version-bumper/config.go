4f81bcf232c426dac8187cd93e662838fee6715b
Steph <steph@hashicorp.com>
2023-06-14 08:08:26 +0200

    rename isPreviewVersion to isStableVersion and add legacy to it

--- tools/version-bumper/config.go
+++ tools/version-bumper/config.go
@@ -121,7 +121,7 @@ func reconcileWithAvailableServices(input services.Config, availableServices []A
 			}
 
 			// for now we don't want to auto-import Preview versions, only Stable releases
-			if isPreviewVersion(version) {
+			if isStableVersion(version) {
 				continue
 			}
 
@@ -160,7 +160,7 @@ func isVersionIsOlderThanExistingVersion(new string, existing string) (*bool, er
 		v := true
 		// if we're using an older preview (e.g. 2020-01-01-preview) but
 		// a GA version gets published, we should import the new version
-		if isPreviewVersion(existing) {
+		if isStableVersion(existing) {
 			v = false
 		}
 		return &v, nil
@@ -195,7 +195,7 @@ func normalizeDateString(input string) string {
 func filterToStableVersions(input []string) []string {
 	output := make([]string, 0)
 	for _, v := range input {
-		if isPreviewVersion(v) {
+		if isStableVersion(v) {
 			continue
 		}
 
@@ -206,10 +206,11 @@ func filterToStableVersions(input []string) []string {
 	return output
 }
 
-func isPreviewVersion(version string) bool {
+func isStableVersion(version string) bool {
 	val := strings.ToLower(version)
 	knownBetaSegments := []string{
 		"beta",
+		"legacy",
 		// privatepreview and publicpreview are implied
 		"preview",
 	}
