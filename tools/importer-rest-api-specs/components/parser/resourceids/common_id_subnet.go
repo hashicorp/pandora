2ce6c21b2880dc96011ef207dbbdc5b8bb086fac
stephybun <steph@hashicorp.com>
2023-06-22 07:31:52 +0200

    add common id for subnet and virtual network (#2496)

---
+++ tools/importer-rest-api-specs/components/parser/resourceids/common_id_subnet.go
@@ -0,0 +1,30 @@
+package resourceids
+
+import (
+	"github.com/hashicorp/pandora/tools/importer-rest-api-specs/models"
+	"github.com/hashicorp/pandora/tools/sdk/resourcemanager"
+)
+
+var _ commonIdMatcher = commonIdSubnet{}
+
+type commonIdSubnet struct{}
+
+func (c commonIdSubnet) id() models.ParsedResourceId {
+	name := "Subnet"
+	return models.ParsedResourceId{
+		CommonAlias: &name,
+		Constants:   map[string]resourcemanager.ConstantDetails{},
+		Segments: []resourcemanager.ResourceIdSegment{
+			models.StaticResourceIDSegment("subscriptions", "subscriptions"),
+			models.SubscriptionIDResourceIDSegment("subscriptionId"),
+			models.StaticResourceIDSegment("resourceGroups", "resourceGroups"),
+			models.ResourceGroupResourceIDSegment("resourceGroupName"),
+			models.StaticResourceIDSegment("providers", "providers"),
+			models.ResourceProviderResourceIDSegment("resourceProvider", "Microsoft.Network"),
+			models.StaticResourceIDSegment("virtualNetworks", "virtualNetworks"),
+			models.UserSpecifiedResourceIDSegment("virtualNetworkName"),
+			models.StaticResourceIDSegment("subnets", "subnets"),
+			models.UserSpecifiedResourceIDSegment("subnetName"),
+		},
+	}
+}
