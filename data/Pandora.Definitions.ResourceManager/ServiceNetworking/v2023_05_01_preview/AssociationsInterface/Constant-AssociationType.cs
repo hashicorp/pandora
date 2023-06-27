2cc566f246ad4ebfe04381fabf5849ce59899c8b
hc-github-team-tf-azure <>
2023-06-27 14:42:03 +0000

    data: regenerating based on the latest Swagger

---
+++ data/Pandora.Definitions.ResourceManager/ServiceNetworking/v2023_05_01_preview/AssociationsInterface/Constant-AssociationType.cs
@@ -0,0 +1,11 @@
+using Pandora.Definitions.Attributes;
+using System.ComponentModel;
+
+namespace Pandora.Definitions.ResourceManager.ServiceNetworking.v2023_05_01_preview.AssociationsInterface;
+
+[ConstantType(ConstantTypeAttribute.ConstantType.String)]
+internal enum AssociationTypeConstant
+{
+    [Description("subnets")]
+    Subnets,
+}
