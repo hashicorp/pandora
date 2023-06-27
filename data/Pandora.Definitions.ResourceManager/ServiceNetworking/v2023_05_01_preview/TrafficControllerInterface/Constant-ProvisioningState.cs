7fdb35953ad3a83524f4f578699f3510c9c9ea65
hc-github-team-tf-azure <105039713+hc-github-team-tf-azure@users.noreply.github.com>
2023-06-27 17:15:30 +0200

    data: regenerating based on the latest Swagger (#2703)
    Co-authored-by: hc-github-team-tf-azure <>

---
+++ data/Pandora.Definitions.ResourceManager/ServiceNetworking/v2023_05_01_preview/TrafficControllerInterface/Constant-ProvisioningState.cs
@@ -0,0 +1,29 @@
+using Pandora.Definitions.Attributes;
+using System.ComponentModel;
+
+namespace Pandora.Definitions.ResourceManager.ServiceNetworking.v2023_05_01_preview.TrafficControllerInterface;
+
+[ConstantType(ConstantTypeAttribute.ConstantType.String)]
+internal enum ProvisioningStateConstant
+{
+    [Description("Accepted")]
+    Accepted,
+
+    [Description("Canceled")]
+    Canceled,
+
+    [Description("Deleting")]
+    Deleting,
+
+    [Description("Failed")]
+    Failed,
+
+    [Description("Provisioning")]
+    Provisioning,
+
+    [Description("Succeeded")]
+    Succeeded,
+
+    [Description("Updating")]
+    Updating,
+}
