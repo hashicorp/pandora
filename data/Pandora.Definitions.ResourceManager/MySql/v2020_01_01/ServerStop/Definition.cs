3f9bc0309ec120931c4a739eba5cee4012363b04
hc-github-team-tf-azure <>
2023-07-26 19:28:58 +0000

    data: regenerating based on the latest Swagger

---
+++ data/Pandora.Definitions.ResourceManager/MySql/v2020_01_01/ServerStop/Definition.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using Pandora.Definitions.Interfaces;
+
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+
+namespace Pandora.Definitions.ResourceManager.MySql.v2020_01_01.ServerStop;
+
+internal class Definition : ResourceDefinition
+{
+    public string Name => "ServerStop";
+    public IEnumerable<Interfaces.ApiOperation> Operations => new List<Interfaces.ApiOperation>
+    {
+        new ServersStopOperation(),
+    };
+    public IEnumerable<System.Type> Constants => new List<System.Type>
+    {
+
+    };
+    public IEnumerable<System.Type> Models => new List<System.Type>
+    {
+
+    };
+}
