0a3fa12d63e5bf1049df5157e68c05f8bcec88fd
tombuildsstuff <git@tom.ibuildstuff.eu>
2023-08-03 11:58:21 +0200

    tools/importer-rest-api-specs: adding Common IDs for App Service, App Service Plan & App Service Environment
    Dependenent on https://github.com/hashicorp/go-azure-helpers/pull/172

---
+++ tools/importer-rest-api-specs/components/parser/resourceids/common_id_app_service.go
@@ -0,0 +1,28 @@
+package resourceids
+
+import (
+	"github.com/hashicorp/pandora/tools/importer-rest-api-specs/models"
+	"github.com/hashicorp/pandora/tools/sdk/resourcemanager"
+)
+
+var _ commonIdMatcher = commonIdAppService{}
+
+type commonIdAppService struct{}
+
+func (c commonIdAppService) id() models.ParsedResourceId {
+	name := "AppService"
+	return models.ParsedResourceId{
+		CommonAlias: &name,
+		Constants:   map[string]resourcemanager.ConstantDetails{},
+		Segments: []resourcemanager.ResourceIdSegment{
+			models.StaticResourceIDSegment("subscriptions", "subscriptions"),
+			models.SubscriptionIDResourceIDSegment("subscriptionId"),
+			models.StaticResourceIDSegment("resourceGroups", "resourceGroups"),
+			models.ResourceGroupResourceIDSegment("resourceGroupName"),
+			models.StaticResourceIDSegment("providers", "providers"),
+			models.ResourceProviderResourceIDSegment("resourceProvider", "Microsoft.Web"),
+			models.StaticResourceIDSegment("sites", "sites"),
+			models.UserSpecifiedResourceIDSegment("siteName"),
+		},
+	}
+}
--- tools/importer-rest-api-specs/components/parser/resourceids/common_ids.go
+++ tools/importer-rest-api-specs/components/parser/resourceids/common_ids.go
@@ -49,6 +49,11 @@ var commonIdTypes = []commonIdMatcher{
 	commonIdAutomationCompilationJob{}, // (@stephybun) CompilationJobId segment is defined in three different ways `jobId`, `compilationJobId` and `compilationJobName`
 	commonIdProvisioningService{},      // (@jackofallops): Inconsistent user specified fields in the swagger - `provisioningServices/{resourceName}` vs `provisioningServices/{provisioningServiceName}`
 
+	// App Service
+	commonIdAppService{},
+	commonIdAppServiceEnvironment{},
+	commonIdAppServicePlan{},
+
 	// Key Vault
 	commonIdKeyVault{},
 	commonIdKeyVaultKey{},
