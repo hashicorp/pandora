38b971cc6379c16812cc0381605f000a76d0cf62
hc-github-team-tf-azure <>
2023-08-10 14:13:48 +0000

    data: regenerating based on the latest Swagger

---
+++ data/Pandora.Definitions.ResourceManager/Resources/v2021_07_01/SubscriptionFeatureRegistrations/Constant-SubscriptionFeatureRegistrationApprovalType.cs
@@ -0,0 +1,17 @@
+using Pandora.Definitions.Attributes;
+using System.ComponentModel;
+
+namespace Pandora.Definitions.ResourceManager.Resources.v2021_07_01.SubscriptionFeatureRegistrations;
+
+[ConstantType(ConstantTypeAttribute.ConstantType.String)]
+internal enum SubscriptionFeatureRegistrationApprovalTypeConstant
+{
+    [Description("ApprovalRequired")]
+    ApprovalRequired,
+
+    [Description("AutoApproval")]
+    AutoApproval,
+
+    [Description("NotSpecified")]
+    NotSpecified,
+}
