c41c8e7aebac895a89522c69d9802fea0ddc0282
hc-github-team-tf-azure <>
2023-08-11 05:05:59 +0000

    data: regenerating based on the latest Swagger

---
+++ data/Pandora.Definitions.ResourceManager/CosmosDB/v2021_10_15/CosmosDB/Operation-GremlinResourcesUpdateGremlinDatabaseThroughput.cs
@@ -0,0 +1,34 @@
+using Pandora.Definitions.Attributes;
+using Pandora.Definitions.CustomTypes;
+using Pandora.Definitions.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+
+namespace Pandora.Definitions.ResourceManager.CosmosDB.v2021_10_15.CosmosDB;
+
+internal class GremlinResourcesUpdateGremlinDatabaseThroughputOperation : Pandora.Definitions.Operations.PutOperation
+{
+    public override IEnumerable<HttpStatusCode> ExpectedStatusCodes() => new List<HttpStatusCode>
+        {
+                HttpStatusCode.Accepted,
+                HttpStatusCode.OK,
+        };
+
+    public override bool LongRunning() => true;
+
+    public override Type? RequestObject() => typeof(ThroughputSettingsUpdateParametersModel);
+
+    public override ResourceID? ResourceId() => new GremlinDatabaseId();
+
+    public override Type? ResponseObject() => typeof(ThroughputSettingsGetResultsModel);
+
+    public override string? UriSuffix() => "/throughputSettings/default";
+
+
+}
