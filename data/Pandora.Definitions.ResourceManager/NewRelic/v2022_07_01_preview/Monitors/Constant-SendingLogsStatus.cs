ee60798a9e8ae5cbf5359bffa75a5738d92c5c52
hc-github-team-tf-azure <>
2023-08-29 22:57:03 +0000

    data: regenerating based on the latest Swagger

---
+++ data/Pandora.Definitions.ResourceManager/NewRelic/v2022_07_01_preview/Monitors/Constant-SendingLogsStatus.cs
@@ -0,0 +1,14 @@
+using Pandora.Definitions.Attributes;
+using System.ComponentModel;
+
+namespace Pandora.Definitions.ResourceManager.NewRelic.v2022_07_01_preview.Monitors;
+
+[ConstantType(ConstantTypeAttribute.ConstantType.String)]
+internal enum SendingLogsStatusConstant
+{
+    [Description("Disabled")]
+    Disabled,
+
+    [Description("Enabled")]
+    Enabled,
+}
---
+++ data/Pandora.Definitions.ResourceManager/NewRelic/v2022_07_01_preview/Monitors/Model-AppServiceInfo.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using Pandora.Definitions.Attributes;
+using Pandora.Definitions.Attributes.Validation;
+using Pandora.Definitions.CustomTypes;
+
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+
+namespace Pandora.Definitions.ResourceManager.NewRelic.v2022_07_01_preview.Monitors;
+
+
+internal class AppServiceInfoModel
+{
+    [JsonPropertyName("agentStatus")]
+    public string? AgentStatus { get; set; }
+
+    [JsonPropertyName("agentVersion")]
+    public string? AgentVersion { get; set; }
+
+    [JsonPropertyName("azureResourceId")]
+    public string? AzureResourceId { get; set; }
+}
