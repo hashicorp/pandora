36b5c65fb286fe0dd0eb42147108c406c90bd7d6
hc-github-team-tf-azure <>
2023-09-01 14:12:10 +0000

    data: regenerating based on the latest Swagger

---
+++ data/Pandora.Definitions.ResourceManager/Compute/v2023_04_02/ApiVersionDefinition-GenerationSetting.cs
@@ -0,0 +1,6 @@
+namespace Pandora.Definitions.ResourceManager.Compute.v2023_04_02;
+
+public partial class Definition
+{
+    public bool Generate => true;
+}
---
+++ data/Pandora.Definitions.ResourceManager/Compute/v2023_04_02/Snapshots/Operation-GrantAccess.cs
@@ -0,0 +1,34 @@
+using Pandora.Definitions.Attributes;
+using Pandora.Definitions.CustomTypes;
+using Pandora.Definitions.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+
+namespace Pandora.Definitions.ResourceManager.Compute.v2023_04_02.Snapshots;
+
+internal class GrantAccessOperation : Pandora.Definitions.Operations.PostOperation
+{
+    public override IEnumerable<HttpStatusCode> ExpectedStatusCodes() => new List<HttpStatusCode>
+        {
+                HttpStatusCode.Accepted,
+                HttpStatusCode.OK,
+        };
+
+    public override bool LongRunning() => true;
+
+    public override Type? RequestObject() => typeof(GrantAccessDataModel);
+
+    public override ResourceID? ResourceId() => new SnapshotId();
+
+    public override Type? ResponseObject() => typeof(AccessUriModel);
+
+    public override string? UriSuffix() => "/beginGetAccess";
+
+
+}
