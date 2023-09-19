ea09696ff45690faf1ec72cf57df2c30040786b3
Tom Bamford <tom@bamford.io>
2023-09-19 21:53:05 +0100

    Data: remove old Microsoft Graph type definitions to allow regeneration using new naming patterns

--- data/Pandora.Definitions.MicrosoftGraph.StableV1/ServicePrincipals/StableV1/ApiVersionDefinition-GenerationSettings.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: MPL-2.0
-
-namespace Pandora.Definitions.MicrosoftGraph.StableV1.ServicePrincipals.StableV1;
-
-public partial class Definition
-{
-    public bool Generate => true;
-}
