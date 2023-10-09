a6bae4832975ee17ed84845d905326d65103a940
Steph <steph@hashicorp.com>
2023-10-09 11:47:26 +0200

    remove unused functions

--- tools/importer-rest-api-specs/components/dataapigeneratorjson/helpers.go
+++ tools/importer-rest-api-specs/components/dataapigeneratorjson/helpers.go
@@ -27,19 +27,6 @@ func (s Generator) workingDirectoryForResource(resource string) string {
 	return path.Join(dir, resource)
 }
 
-func fileExistsAtPath(filePath string) (*bool, error) {
-	file, err := os.Stat(filePath)
-	if err != nil && !os.IsNotExist(err) {
-		return nil, fmt.Errorf("checking for a file at %q: %+v", filePath, err)
-	}
-
-	result := false
-	if file != nil && file.Size() > 0 {
-		result = true
-	}
-	return &result, nil
-}
-
 func RecreateDirectory(directory string, logger hclog.Logger) error {
 	logger.Trace(fmt.Sprintf("Deleting any existing directory at %q..", directory))
 	if err := os.RemoveAll(directory); err != nil {
@@ -57,23 +44,6 @@ func normalizeApiVersion(input string) string {
 	normalized := strings.ReplaceAll(input, "-", "_")     // e.g. 2020-01-01-preview -> 2020_01_01_preview
 	normalized = strings.ReplaceAll(normalized, ".", "_") // e.g. 1.0 -> 1_0
 	return fmt.Sprintf("v%s", normalized)
-}
-
-func writeToFile(fileName, fileContents string) error {
-	existing, err := os.Open(fileName)
-	if os.IsExist(err) {
-		return fmt.Errorf("existing file exists at %q", fileName)
-	}
-	existing.Close()
-
-	file, err := os.Create(fileName)
-	if err != nil {
-		return fmt.Errorf("creating %q: %+v", fileName, err)
-	}
-
-	file.WriteString(fileContents)
-	file.Close()
-	return nil
 }
 
 func writeJsonToFile(fileName string, fileContents []byte) error {
