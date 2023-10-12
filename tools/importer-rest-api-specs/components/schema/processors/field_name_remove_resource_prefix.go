c6b7d937f1bccd74e2ac736fc6844b938234f915
tombuildsstuff <git@tom.ibuildstuff.eu>
2023-10-12 09:41:29 +0200

    tools/importer-rest-api-specs: updating the field name renamer such that when a field is just called `uri` it gets updated to `service_uri`

--- tools/importer-rest-api-specs/components/schema/processors/field_name_remove_resource_prefix.go
+++ tools/importer-rest-api-specs/components/schema/processors/field_name_remove_resource_prefix.go
@@ -1,6 +1,8 @@
 package processors
 
-import "strings"
+import (
+	"strings"
+)
 
 var _ FieldNameProcessor = fieldNameRemoveResourcePrefix{}
 
@@ -9,6 +11,14 @@ type fieldNameRemoveResourcePrefix struct{}
 func (fieldNameRemoveResourcePrefix) ProcessField(fieldName string, metadata FieldMetadata) (*string, error) {
 	if strings.HasPrefix(fieldName, metadata.TerraformDetails.ResourceName) {
 		updatedName := strings.Replace(fieldName, metadata.TerraformDetails.ResourceName, "", 1)
+
+		// However if the name is now just `Uri` then this field should be renamed to `ServiceUri`
+		// Whilst we COULD call this `DataPlaneUri` it's not guaranteed to be a Data Plane
+		// therefore Service likely makes more sense as a prefix.
+		if strings.EqualFold(updatedName, "Uri") {
+			updatedName = "ServiceUri"
+		}
+
 		return &updatedName, nil
 	}
 	return nil, nil
