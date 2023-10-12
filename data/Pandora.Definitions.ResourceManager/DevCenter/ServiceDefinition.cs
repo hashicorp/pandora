ee6f61390bf095eb09c5749d647c15b3d56010fa
hc-github-team-tf-azure <>
2023-10-12 15:11:48 +0000

    data: regenerating based on the latest Swagger

--- data/Pandora.Definitions.ResourceManager/DevCenter/ServiceDefinition.cs
+++ data/Pandora.Definitions.ResourceManager/DevCenter/ServiceDefinition.cs
@@ -12,10 +12,11 @@ public partial class Service : ServiceDefinition
 {
     public string Name => "DevCenter";
     public string? ResourceProvider => "Microsoft.DevCenter";
-    public string? TerraformPackageName => null;
+    public string? TerraformPackageName => "devcenter";
 
     public IEnumerable<TerraformResourceDefinition> TerraformResources => new List<TerraformResourceDefinition>
     {
-
+        new Terraform.DevCenterProjectResource(),
+        new Terraform.DevCenterResource(),
     };
 }
---
+++ data/Pandora.Definitions.ResourceManager/DevCenter/Terraform/DevCenterProject-Resource-Mappings.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using Pandora.Definitions.Interfaces;
+using Pandora.Definitions.Mappings;
+using Pandora.Definitions.ResourceManager.DevCenter.v2023_04_01.Projects;
+
+namespace Pandora.Definitions.ResourceManager.DevCenter.Terraform;
+
+public class DevCenterProjectResourceMappings : TerraformMappingDefinition
+{
+    public List<MappingType> Mappings => new List<MappingType>
+    {
+        Mapping.FromSchema<DevCenterProjectResourceSchema>(s => s.Name).ToResourceIdSegmentNamed("projectName"),
+        Mapping.FromSchema<DevCenterProjectResourceSchema>(s => s.ResourceGroupName).ToResourceIdSegmentNamed("resourceGroupName"),
+
+        Mapping.FromSchema<DevCenterProjectResourceSchema>(s => s.Description).ToSdkField<ProjectPropertiesModel>(m => m.Description).Direct(),
+        Mapping.FromSchema<DevCenterProjectResourceSchema>(s => s.DevCenterId).ToSdkField<ProjectPropertiesModel>(m => m.DevCenterId).Direct(),
+        Mapping.FromSchema<DevCenterProjectResourceSchema>(s => s.DevCenterUri).ToSdkField<ProjectPropertiesModel>(m => m.DevCenterUri).Direct(),
+        Mapping.FromSchema<DevCenterProjectResourceSchema>(s => s.Location).ToSdkField<ProjectModel>(m => m.Location).Direct(),
+        Mapping.FromSchema<DevCenterProjectResourceSchema>(s => s.MaxDevBoxesPerUser).ToSdkField<ProjectPropertiesModel>(m => m.MaxDevBoxesPerUser).Direct(),
+        Mapping.FromSchema<DevCenterProjectResourceSchema>(s => s.MaxDevBoxesPerUser).ToSdkField<ProjectUpdatePropertiesModel>(m => m.MaxDevBoxesPerUser).Direct(),
+        Mapping.FromSchema<DevCenterProjectResourceSchema>(s => s.Tags).ToSdkField<ProjectModel>(m => m.Tags).Direct(),
+        Mapping.FromSchema<DevCenterProjectResourceSchema>(s => s.Tags).ToSdkField<ProjectUpdateModel>(m => m.Tags).Direct(),
+        Mapping.FromSchemaModel<DevCenterProjectResourceSchema>().ToSdkField<ProjectModel>(m => m.Properties).ModelToModel(),
+        Mapping.FromSchemaModel<DevCenterProjectResourceSchema>().ToSdkField<ProjectUpdateModel>(m => m.Properties).ModelToModel(),
+    };
+}
