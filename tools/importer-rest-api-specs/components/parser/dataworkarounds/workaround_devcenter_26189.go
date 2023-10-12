1714b42be7c9636f3caf8d92b46d62867c3d2042
tombuildsstuff <git@tom.ibuildstuff.eu>
2023-10-12 11:16:35 +0200

    tools/importer-rest-api-specs: updating the workaround to also update projects

--- tools/importer-rest-api-specs/components/parser/dataworkarounds/workaround_devcenter_26189.go
+++ tools/importer-rest-api-specs/components/parser/dataworkarounds/workaround_devcenter_26189.go
@@ -2,7 +2,6 @@ package dataworkarounds
 
 import (
 	"fmt"
-
 	"github.com/hashicorp/pandora/tools/importer-rest-api-specs/models"
 )
 
@@ -19,7 +18,7 @@ type workaroundDevCenter26189 struct {
 }
 
 func (workaroundDevCenter26189) IsApplicable(apiDefinition *models.AzureApiDefinition) bool {
-	return apiDefinition.ServiceName == "DevCenter" && apiDefinition.ApiVersion == "2022-04-01"
+	return apiDefinition.ServiceName == "DevCenter" && apiDefinition.ApiVersion == "2023-04-01"
 }
 
 func (workaroundDevCenter26189) Name() string {
@@ -27,23 +26,52 @@ func (workaroundDevCenter26189) Name() string {
 }
 
 func (workaroundDevCenter26189) Process(apiDefinition models.AzureApiDefinition) (*models.AzureApiDefinition, error) {
-	resource, ok := apiDefinition.Resources["DevCenters"]
+	// First we need to patch the DevCenters issue (marking DevCenterUri as required)
+	devCentersResource, ok := apiDefinition.Resources["DevCenters"]
 	if !ok {
 		return nil, fmt.Errorf("expected a Resource named `DevCenters` but didn't get one")
 	}
 
-	model, ok := resource.Models["DevCenterProperties"]
+	devCenterModel, ok := devCentersResource.Models["DevCenterProperties"]
 	if !ok {
 		return nil, fmt.Errorf("expected a Model named `DevCenterProperties` but didn't get one")
 	}
-	field, ok := model.Fields["DevCenterUri"]
+	devCenterField, ok := devCenterModel.Fields["DevCenterUri"]
 	if !ok {
 		return nil, fmt.Errorf("expected a Field named `DevCenterUri` but didn't get one")
 	}
-	field.ReadOnly = true
+	devCenterField.ReadOnly = true
 
-	model.Fields["DevCenterUri"] = field
-	resource.Models["DevCenterProperties"] = model
-	apiDefinition.Resources["DevCenters"] = resource
+	devCenterModel.Fields["DevCenterUri"] = devCenterField
+	devCentersResource.Models["DevCenterProperties"] = devCenterModel
+	apiDefinition.Resources["DevCenters"] = devCentersResource
+
+	// Next we need to do the same thing for Projects to make the `DevCenterId` field Required
+	// and make `DevCenterUri` computed
+	projectsResource, ok := apiDefinition.Resources["Projects"]
+	if !ok {
+		return nil, fmt.Errorf("expected a Resource named `Projects` but didn't get one")
+	}
+	projectModel, ok := projectsResource.Models["ProjectProperties"]
+	if !ok {
+		return nil, fmt.Errorf("expected a Model named `ProjectProperties` but didn't get one")
+	}
+	projectDevCenterIdField, ok := projectModel.Fields["DevCenterId"]
+	if !ok {
+		return nil, fmt.Errorf("expected a Field named `DevCenterId` but didn't get one")
+	}
+	projectDevCenterIdField.Required = true
+	projectModel.Fields["DevCenterId"] = projectDevCenterIdField
+
+	projectDevCenterUriField, ok := projectModel.Fields["DevCenterUri"]
+	if !ok {
+		return nil, fmt.Errorf("expected a Field named `DevCenterUri` but didn't get one")
+	}
+	projectDevCenterUriField.ReadOnly = true
+	projectModel.Fields["DevCenterUri"] = projectDevCenterUriField
+
+	projectsResource.Models["ProjectProperties"] = projectModel
+	apiDefinition.Resources["Projects"] = projectsResource
+
 	return &apiDefinition, nil
 }
