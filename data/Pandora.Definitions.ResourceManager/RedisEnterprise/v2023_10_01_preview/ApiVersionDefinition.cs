f7c093401adcd1267be0c4bcf7deaa9486fb7269
hc-github-team-tf-azure <>
2023-10-19 09:31:24 +0000

    data: regenerating based on the latest Swagger

---
+++ data/Pandora.Definitions.ResourceManager/RedisEnterprise/v2023_10_01_preview/ApiVersionDefinition.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using Pandora.Definitions.Interfaces;
+
+namespace Pandora.Definitions.ResourceManager.RedisEnterprise.v2023_10_01_preview;
+
+public partial class Definition : ApiVersionDefinition
+{
+    public string ApiVersion => "2023-10-01-preview";
+    public bool Preview => true;
+    public Source Source => Source.ResourceManagerRestApiSpecs;
+
+    public IEnumerable<ResourceDefinition> Resources => new List<ResourceDefinition>
+    {
+        new Databases.Definition(),
+        new PrivateEndpointConnections.Definition(),
+        new PrivateLinkResources.Definition(),
+        new RedisEnterprise.Definition(),
+        new SKU.Definition(),
+    };
+}
