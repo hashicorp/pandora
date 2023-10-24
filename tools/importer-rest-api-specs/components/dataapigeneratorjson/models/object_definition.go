4fb2af3e86f0b6a8404e197c8a15151ebdd9c299
Steph <steph@hashicorp.com>
2023-10-24 13:59:03 +0200

    lower cased the json tags for the operations model and added documentation for the object definition model

--- tools/importer-rest-api-specs/components/dataapigeneratorjson/models/object_definition.go
+++ tools/importer-rest-api-specs/components/dataapigeneratorjson/models/object_definition.go
@@ -1,37 +1,30 @@
 package models
 
-import (
-	"github.com/hashicorp/pandora/tools/importer-rest-api-specs/models"
-)
+import "github.com/hashicorp/pandora/tools/importer-rest-api-specs/models"
 
 // ObjectDefinition specifies additional information about a specific Object and any associated nested Objects
 type ObjectDefinition struct {
+	// ObjectDefinitionType defines what kind of ObjectDefinition this is, such as a Reference, String or List
 	Type ObjectDefinitionType `json:"type"`
 
-	ReferenceName *string `json:"referenceName"` // NOTE: we could split this into ConstantName and ModelName in time, but not today.
+	// NOTE: we could split this into ConstantName and ModelName in time, but not today.
+	// ReferenceName is the name of the Constant or Model that this is a reference to
+	ReferenceName *string `json:"referenceName"`
 
 	// NestedItem is a nested ObjectDefinition when Type is a Dictionary or List
 	NestedItem *ObjectDefinition `json:"nestedItem,omitempty"`
 
 	// NOTE: these 3 fields were previously located against the Field but instead should be located against the Object Definition
 	// as such we'll need to update the Data API to account for this
-	MaxItems   *int        `json:"maxItems,omitempty"`
-	MinItems   *int        `json:"minItems,omitempty"`
+
+	// MaxItems specifies the maximum number of items a CSV/Dictionary/List can have, this field is only relevant for the Terraform Schema
+	MaxItems *int `json:"maxItems,omitempty"`
+
+	// MinItems specifies the minimum number of items a CSV/Dictionary/List can have, this field is only relevant for the Terraform Schema
+	MinItems *int `json:"minItems,omitempty"`
+
+	// DateFormat specifies the format a date field should have, this field is only relevant for the Terraform Schema
 	DateFormat *DateFormat `json:"dateFormat,omitempty"`
-}
-
-func ObjectDefinitionFromSchemaFieldFromImporterRestApiSpecs(input *models.ObjectDefinition) *ObjectDefinition {
-	if input == nil {
-		return nil
-	}
-
-	objectDefinition := ObjectDefinition{
-		ReferenceName: input.ReferenceName,
-		Type:          ObjectDefinitionType(input.Type),
-		NestedItem:    ObjectDefinitionFromSchemaFieldFromImporterRestApiSpecs(input.NestedItem),
-	}
-
-	return &objectDefinition
 }
 
 type ObjectDefinitionType string
@@ -75,3 +68,17 @@ const (
 	// TODO: others in the future https://github.com/hashicorp/pandora/issues/8 e.g.
 	// RFC3339NanoDateFormat DateFormat = "RFC3339Nano"
 )
+
+func ObjectDefinitionFromSchemaFieldFromImporterRestApiSpecs(input *models.ObjectDefinition) *ObjectDefinition {
+	if input == nil {
+		return nil
+	}
+
+	objectDefinition := ObjectDefinition{
+		ReferenceName: input.ReferenceName,
+		Type:          ObjectDefinitionType(input.Type),
+		NestedItem:    ObjectDefinitionFromSchemaFieldFromImporterRestApiSpecs(input.NestedItem),
+	}
+
+	return &objectDefinition
+}
--- tools/importer-rest-api-specs/components/dataapigeneratorjson/models/operations.go
+++ tools/importer-rest-api-specs/components/dataapigeneratorjson/models/operations.go
@@ -3,61 +3,61 @@ package models
 type Operation struct {
 	// Name specifies the Display Name for this Operation (e.g. `Create`) which is also
 	// valid as an identifier.
-	Name string `json:"Name"`
+	Name string `json:"name"`
 
 	// ContentType specifies the format of the information being sent with the Operation (e.g. `application/json; charset=utf-8`)
-	ContentType string `json:"ContentType"`
+	ContentType string `json:"contentType"`
 
 	// ExpectedStatusCodes specifies is a list of Status Codes which are expected to be returned (e.g. 200, 201)
-	ExpectedStatusCodes []int `json:"ExpectedStatusCodes"`
+	ExpectedStatusCodes []int `json:"expectedStatusCodes"`
 
 	// FieldContainingPaginationDetails is a reference to the field within the Response
 	// which contains the pagination details, (e.g. `nextLink`)
-	FieldContainingPaginationDetails *string `json:"FieldContainingPaginationDetails,omitempty"`
+	FieldContainingPaginationDetails *string `json:"fieldContainingPaginationDetails,omitempty"`
 
 	// LongRunning specifies if this is a Long Running Operation, meaning that Clients
 	// should follow any `Location` headers to track the result of this operation
-	LongRunning bool `json:"LongRunning"`
+	LongRunning bool `json:"longRunning"`
 
 	// HTTPMethod is the Method used for this operation, (e.g. `GET`, `POST`)
-	HTTPMethod string `json:"HTTPMethod"`
+	HTTPMethod string `json:"httpMethod"`
 
 	// Options is a list of options which can be specified for this operation
 	// these are querystring parameters such as 'limit' or 'forceDelete' or similar
-	Options []Option `json:"Options,omitempty"`
+	Options []Option `json:"options,omitempty"`
 
 	// ResourceId specifies the optional Resource ID used for this operation
-	ResourceId *string `json:"ResourceId,omitempty"`
+	ResourceId *string `json:"resourceId,omitempty"`
 
 	// RequestObject specifies the optional ObjectDefinition to be specified in the Request
-	RequestObject *ObjectDefinition `json:"RequestObject,omitempty"`
+	RequestObject *ObjectDefinition `json:"requestObject,omitempty"`
 
 	// RequestObject specifies the optional ObjectDefinition to be returned by the Request
-	ResponseObject *ObjectDefinition `json:"ResponseObject,omitempty"`
+	ResponseObject *ObjectDefinition `json:"responseObject,omitempty"`
 
 	// UriSuffix specifies the suffix which should be appended to the ResourceID for this operation
 	// (e.g. `/shutdown`)
-	UriSuffix *string `json:"UriSuffix,omitempty"`
+	UriSuffix *string `json:"uriSuffix,omitempty"`
 }
 
 type Option struct {
 	// HeaderName is the name of the Http Header which this Option should be set into
 	// (e.g. `If-Match`, `x-ms-client-request-id`)
-	HeaderName *string `json:"HeaderName,omitempty"`
+	HeaderName *string `json:"headerName,omitempty"`
 
 	// Optional specifies whether this Option could be specified in the Request
-	Optional bool `json:"Optional"`
+	Optional bool `json:"optional"`
 
 	// QueryStringName is the Key which should be used for this Option in the QueryString
 	// (e.g. `createTimeMax`, `isActive`)
-	QueryString *string `json:"QueryString,omitempty"`
+	QueryString *string `json:"queryString,omitempty"`
 
 	// Required specifies whether this Option must be specified in the Request
-	Required bool `json:"Required"`
+	Required bool `json:"required"`
 
 	// Field specifies the DisplayName of the Option (e.g. `ConstantOption`, `SecondVal`)
-	Field string `json:"Field"`
+	Field string `json:"field"`
 
 	// FieldType specifies what kind of Option this is (e.g. `String`, `Bool`)
-	FieldType ObjectDefinitionType `json:"FieldType"`
+	FieldType ObjectDefinitionType `json:"fieldType"`
 }
