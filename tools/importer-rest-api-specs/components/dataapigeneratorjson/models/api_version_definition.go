2b5605108201301a9da8b717d4f7c90e1c3afd63
tombuildsstuff <git@tom.ibuildstuff.eu>
2023-10-24 15:30:46 +0200

    tools/impoter-rest-api-specs: updating the struct tags to be all camelCase

--- tools/importer-rest-api-specs/components/dataapigeneratorjson/models/api_version_definition.go
+++ tools/importer-rest-api-specs/components/dataapigeneratorjson/models/api_version_definition.go
@@ -4,17 +4,17 @@ package models
 type ApiVersionDefinition struct {
 	// ApiVersion specifies the Version Number for this API.
 	// Example: `2020-01-01-preview`.
-	ApiVersion string `json:"ApiVersion"`
+	ApiVersion string `json:"apiVersion"`
 
 	// IsPreview specifies whether this is a Preview API version (otherwise it's a Stable API version).
 	IsPreview bool `json:"isPreview"`
 
+	// Generate specifies whether this API Version should be generated or not.
+	Generate bool `json:"generate"`
+
 	// Source specifies where the source data for this API version came from.
-	Source ApiDefinitionsSource `json:"Source"` // TODO: move this into the MetaData file?
+	Source ApiDefinitionsSource `json:"source"` // TODO: move this into the MetaData file?
 
 	// Resources specifies a list of Api Resource names that exist within this API version.
-	Resources []string `json:"Resources"`
-
-	// Generate specifies whether this API Version should be generated or not.
-	Generate bool `json:"Generate"`
+	Resources []string `json:"resources"`
 }
