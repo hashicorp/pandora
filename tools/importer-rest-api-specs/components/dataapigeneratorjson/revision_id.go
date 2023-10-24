d49c05198063adb8176c0ffcac250d49411c7bce
tombuildsstuff <git@tom.ibuildstuff.eu>
2023-10-24 11:54:24 +0200

    tools/importer-rest-api-specs: making the `MetaData` type common, so this can be surfaced and reused more easily

--- tools/importer-rest-api-specs/components/dataapigeneratorjson/revision_id.go
+++ tools/importer-rest-api-specs/components/dataapigeneratorjson/revision_id.go
@@ -3,20 +3,23 @@ package dataapigeneratorjson
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 	"path"
+
+	dataApiModels "github.com/hashicorp/pandora/tools/importer-rest-api-specs/components/dataapigeneratorjson/models"
 )
 
 func OutputMetaData(workingDirectory, swaggerGitSha string) error {
-	revision := map[string]string{
-		"SwaggerRevision": swaggerGitSha,
+	metaData := dataApiModels.MetaData{
+		GitRevision: pointer.To(swaggerGitSha),
 	}
 
-	data, err := json.MarshalIndent(&revision, "", `	`)
+	data, err := json.MarshalIndent(&metaData, "", "\t")
 	if err != nil {
 		return err
 	}
 
-	revisionIdFileName := path.Join(workingDirectory, "SwaggerRevision.json") // TODO: rename to metadata.json
+	revisionIdFileName := path.Join(workingDirectory, "metadata.json")
 	if err := writeJsonToFile(revisionIdFileName, data); err != nil {
 		return fmt.Errorf("writing Swagger Revision ID for %q: %+v", revisionIdFileName, err)
 	}
