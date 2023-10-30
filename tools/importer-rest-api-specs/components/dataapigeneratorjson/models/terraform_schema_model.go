f59d454b688f6eff5ac1da6f2538f15795df42dc
tombuildsstuff <git@tom.ibuildstuff.eu>
2023-10-30 08:56:43 +0100

    tools/importer-rest-api-specs: better documenting the TerraformSchemaModel
    Highlighting that at least one will exist per resource

--- tools/importer-rest-api-specs/components/dataapigeneratorjson/models/terraform_schema_model.go
+++ tools/importer-rest-api-specs/components/dataapigeneratorjson/models/terraform_schema_model.go
@@ -1,7 +1,15 @@
 package models
 
-// TerraformSchemaModel defines a model used in the Terraform Schema, which is used to output
-// both a Struct and the Terraform Schema itself.
+// TerraformSchemaModel defines a model used in the Terraform Schema for a Resource.
+//
+// The TerraformSchemaModel is output both the Typed Model (i.e. a Go struct for this
+// Resource) and as the Terraform Schema (that is the `Arguments()` and `Attributes()`
+// methods in the Typed SDK).
+//
+// A Resource will always have at least one TerraformSchemaModel, which represents
+// the Terraform Schema for that Resource. However, Nested Items (such as Lists/Sets)
+// will each have a TerraformSchemaModel - as such a complex Resource is likely to
+// have multiple TerraformSchemaModels used to define the full Terraform Schema.
 type TerraformSchemaModel struct {
 	// Fields specifies the Fields which exist within this Terraform Model.
 	Fields []TerraformSchemaField `json:"fields"`
