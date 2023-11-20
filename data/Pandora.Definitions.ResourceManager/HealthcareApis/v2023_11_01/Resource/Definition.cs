70fbb561b360d4526e6f67a20c6bf91ba8cf2f8c
hc-github-team-tf-azure <105039713+hc-github-team-tf-azure@users.noreply.github.com>
2023-11-20 13:53:49 +0100

    data: regenerating based on the latest Swagger (#3381)
    Co-authored-by: hc-github-team-tf-azure <>

---
+++ data/Pandora.Definitions.ResourceManager/HealthcareApis/v2023_11_01/Resource/Definition.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using Pandora.Definitions.Interfaces;
+
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+
+namespace Pandora.Definitions.ResourceManager.HealthcareApis.v2023_11_01.Resource;
+
+internal class Definition : ResourceDefinition
+{
+    public string Name => "Resource";
+    public IEnumerable<Interfaces.ApiOperation> Operations => new List<Interfaces.ApiOperation>
+    {
+        new ServicesCreateOrUpdateOperation(),
+        new ServicesDeleteOperation(),
+        new ServicesGetOperation(),
+        new ServicesUpdateOperation(),
+    };
+    public IEnumerable<System.Type> Constants => new List<System.Type>
+    {
+        typeof(KindConstant),
+        typeof(PrivateEndpointConnectionProvisioningStateConstant),
+        typeof(PrivateEndpointServiceConnectionStatusConstant),
+        typeof(ProvisioningStateConstant),
+        typeof(PublicNetworkAccessConstant),
+    };
+    public IEnumerable<System.Type> Models => new List<System.Type>
+    {
+        typeof(PrivateEndpointModel),
+        typeof(PrivateEndpointConnectionModel),
+        typeof(PrivateEndpointConnectionPropertiesModel),
+        typeof(PrivateLinkServiceConnectionStateModel),
+        typeof(ServiceAccessPolicyEntryModel),
+        typeof(ServiceAcrConfigurationInfoModel),
+        typeof(ServiceAuthenticationConfigurationInfoModel),
+        typeof(ServiceCorsConfigurationInfoModel),
+        typeof(ServiceCosmosDbConfigurationInfoModel),
+        typeof(ServiceExportConfigurationInfoModel),
+        typeof(ServiceImportConfigurationInfoModel),
+        typeof(ServiceOciArtifactEntryModel),
+        typeof(ServicesDescriptionModel),
+        typeof(ServicesPatchDescriptionModel),
+        typeof(ServicesPropertiesModel),
+        typeof(ServicesPropertiesUpdateParametersModel),
+    };
+}
---
+++ data/Pandora.Definitions.ResourceManager/HealthcareApis/v2023_11_01/WorkspacePrivateLinkResources/Operation-ListByWorkspace.cs
@@ -0,0 +1,24 @@
+using Pandora.Definitions.Attributes;
+using Pandora.Definitions.CustomTypes;
+using Pandora.Definitions.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+
+namespace Pandora.Definitions.ResourceManager.HealthcareApis.v2023_11_01.WorkspacePrivateLinkResources;
+
+internal class ListByWorkspaceOperation : Pandora.Definitions.Operations.GetOperation
+{
+    public override ResourceID? ResourceId() => new WorkspaceId();
+
+    public override Type? ResponseObject() => typeof(PrivateLinkResourceListResultDescriptionModel);
+
+    public override string? UriSuffix() => "/privateLinkResources";
+
+
+}
