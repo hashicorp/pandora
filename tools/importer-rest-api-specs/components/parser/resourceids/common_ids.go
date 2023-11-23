58e78633cc5291b97b69d3aeb2a2ed8f2eb871a3
tombuildsstuff <git@tom.ibuildstuff.eu>
2023-11-23 16:00:58 +0100

    tools/importer-rest-api-specs: HDInsight Cluster is now a Common ID
    Dependent on https://github.com/hashicorp/go-azure-helpers/pull/198

--- tools/importer-rest-api-specs/components/parser/resourceids/common_ids.go
+++ tools/importer-rest-api-specs/components/parser/resourceids/common_ids.go
@@ -49,6 +49,9 @@ var commonIdTypes = []commonIdMatcher{
 	commonIdAutomationCompilationJob{}, // (@stephybun) CompilationJobId segment is defined in three different ways `jobId`, `compilationJobId` and `compilationJobName`
 	commonIdProvisioningService{},      // (@jackofallops): Inconsistent user specified fields in the swagger - `provisioningServices/{resourceName}` vs `provisioningServices/{provisioningServiceName}`
 
+	// HDInsight
+	commonIdHDInsightCluster{},
+
 	// Key Vault
 	commonIdKeyVault{},
 	commonIdKeyVaultKey{},
