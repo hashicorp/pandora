b0c48c088a16758d4661dba08f40fdd9caf35bbc
hc-github-team-tf-azure <>
2023-11-24 15:43:31 +0000

    data: regenerating based on the latest Swagger

---
+++ data/Pandora.Definitions.ResourceManager/HDInsight/v2021_06_01/Clusters/Constant-ClusterKind.cs
@@ -0,0 +1,23 @@
+using Pandora.Definitions.Attributes;
+using System.ComponentModel;
+
+namespace Pandora.Definitions.ResourceManager.HDInsight.v2021_06_01.Clusters;
+
+[ConstantType(ConstantTypeAttribute.ConstantType.String)]
+internal enum ClusterKindConstant
+{
+    [Description("HBASE")]
+    HBase,
+
+    [Description("HADOOP")]
+    Hadoop,
+
+    [Description("INTERACTIVEHIVE")]
+    InteractiveHive,
+
+    [Description("KAFKA")]
+    Kafka,
+
+    [Description("SPARK")]
+    Spark,
+}
