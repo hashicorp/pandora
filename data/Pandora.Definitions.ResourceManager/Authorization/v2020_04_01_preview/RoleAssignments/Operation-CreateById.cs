cacc26e71fe92483dd9297b63173f9b29c384fe1
hc-github-team-tf-azure <>
2023-12-14 15:34:18 +0000

    data: regenerating based on the latest Swagger

---
+++ data/Pandora.Definitions.ResourceManager/Authorization/v2020_04_01_preview/RoleAssignments/Operation-CreateById.cs
@@ -0,0 +1,29 @@
+using Pandora.Definitions.Attributes;
+using Pandora.Definitions.CustomTypes;
+using Pandora.Definitions.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+
+namespace Pandora.Definitions.ResourceManager.Authorization.v2020_04_01_preview.RoleAssignments;
+
+internal class CreateByIdOperation : Pandora.Definitions.Operations.PutOperation
+{
+    public override IEnumerable<HttpStatusCode> ExpectedStatusCodes() => new List<HttpStatusCode>
+        {
+                HttpStatusCode.Created,
+        };
+
+    public override Type? RequestObject() => typeof(RoleAssignmentCreateParametersModel);
+
+    public override ResourceID? ResourceId() => new RoleIdId();
+
+    public override Type? ResponseObject() => typeof(RoleAssignmentModel);
+
+
+}
