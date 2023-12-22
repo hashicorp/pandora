c706ce82b1b76bdfc714d5fe9677403d60d45202
hc-github-team-tf-azure <>
2023-12-22 18:17:28 +0000

    data: regenerating based on the latest Swagger

---
+++ data/Pandora.Definitions.ResourceManager/NetApp/v2023_07_01/Restore/Constant-RelationshipStatus.cs
@@ -0,0 +1,20 @@
+using Pandora.Definitions.Attributes;
+using System.ComponentModel;
+
+namespace Pandora.Definitions.ResourceManager.NetApp.v2023_07_01.Restore;
+
+[ConstantType(ConstantTypeAttribute.ConstantType.String)]
+internal enum RelationshipStatusConstant
+{
+    [Description("Failed")]
+    Failed,
+
+    [Description("Idle")]
+    Idle,
+
+    [Description("Transferring")]
+    Transferring,
+
+    [Description("Unknown")]
+    Unknown,
+}
---
+++ data/Pandora.Definitions.ResourceManager/NetApp/v2023_07_01/VolumeGroups/Constant-CoolAccessRetrievalPolicy.cs
@@ -0,0 +1,17 @@
+using Pandora.Definitions.Attributes;
+using System.ComponentModel;
+
+namespace Pandora.Definitions.ResourceManager.NetApp.v2023_07_01.VolumeGroups;
+
+[ConstantType(ConstantTypeAttribute.ConstantType.String)]
+internal enum CoolAccessRetrievalPolicyConstant
+{
+    [Description("Default")]
+    Default,
+
+    [Description("Never")]
+    Never,
+
+    [Description("OnRead")]
+    OnRead,
+}
