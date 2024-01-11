453c089fc9d9aad8f4bd0abe4dd47d4b82f5103e
Steph <steph@hashicorp.com>
2024-01-11 11:50:50 +0100

    add helper to convert from title to snake case

--- tools/importer-rest-api-specs/components/helpers/title_case_test.go
+++ tools/importer-rest-api-specs/components/helpers/title_case_test.go
@@ -1,1 +1,35 @@
 package helpers
+
+import "testing"
+
+func TestConvertFromSnakeToTitleCase(t *testing.T) {
+	testData := []struct {
+		input    string
+		expected string
+	}{
+		{
+			input:    "oh_lawd_he_coming",
+			expected: "OhLawdHeComing",
+		},
+		{
+			input:    "dns_name",
+			expected: "DnsName",
+		},
+		{
+			input:    "ThisIsAlreadyTitleCased",
+			expected: "ThisIsAlreadyTitleCased",
+		},
+		{
+			input:    "1_2_3_4",
+			expected: "1234",
+		},
+	}
+
+	for _, v := range testData {
+		t.Logf("[DEBUG] Testing %s", v.input)
+		actual := ConvertFromSnakeToTitleCase(v.input)
+		if actual != v.expected {
+			t.Fatalf("Expected %s but got %s", v.expected, actual)
+		}
+	}
+}
