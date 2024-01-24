571f38382f3b78a1dcfc3855abf2cff9ba6ef143
tombuildsstuff <git@tom.ibuildstuff.eu>
2024-01-24 09:52:40 +0100

    `tools/generator-go-sdk`: skipping outputting the unmarshal from the response object when an LRO is in use
    This means that we don't attempt to unmarshal the initial HTTP Response - but allows the consumer to decide
    whether the initial HTTP Response or the result of the LRO should be unmarshalled as required.

--- tools/generator-go-sdk/generator/templater_methods.go
+++ tools/generator-go-sdk/generator/templater_methods.go
@@ -2,6 +2,7 @@ package generator
 
 import (
 	"fmt"
+	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 	"sort"
 	"strings"
 
@@ -532,6 +533,14 @@ func (c methodsPandoraTemplater) marshalerTemplate() (*string, error) {
 
 func (c methodsPandoraTemplater) unmarshalerTemplate(data ServiceGeneratorData) (*string, error) {
 	var output string
+
+	if c.operation.LongRunning {
+		// Long Running operations shouldn't be attempted to be unmarshalled until the LRO is completed
+		// in the event this needs to be unmarshalled early - the Response Object being exposed means that
+		// we can opt to do this by calling `Unmarshal` by hand on a case-by-case basis.
+		return pointer.To(""), nil
+	}
+
 	if c.operation.ResponseObject != nil {
 		golangTypeName, err := golangTypeNameForObjectDefinition(*c.operation.ResponseObject)
 		if err != nil {
