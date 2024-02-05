11037add0a1f8be83b4e22ce1d0971508b710955
stephybun <steph@hashicorp.com>
2024-02-05 11:16:20 +0100

    apply schema overrides to parent resource ID (#3757)

--- tools/importer-rest-api-specs/components/schema/fields_resource_id_test.go
+++ tools/importer-rest-api-specs/components/schema/fields_resource_id_test.go
@@ -543,6 +543,180 @@ func TestTopLevelFieldsWithinResourceId_KubernetesTrustedAccessRoleBinding(t *te
 	checkResourceIdMappingExistsBetween(t, actualMappings.ResourceId, "Name", "trustedAccessRoleBindingName", false)
 }
 
+func TestTopLevelFieldsWithinResourceId_ParentIdSchemaOverride(t *testing.T) {
+	input := resourcemanager.ResourceIdDefinition{
+		Segments: []resourcemanager.ResourceIdSegment{
+			{
+				FixedValue: stringPointer("subscriptions"),
+				Name:       "subscriptions",
+				Type:       resourcemanager.StaticSegment,
+			},
+			{
+				Name: "subscriptionId",
+				Type: resourcemanager.SubscriptionIdSegment,
+			},
+			{
+				FixedValue: stringPointer("resourceGroups"),
+				Name:       "resourceGroups",
+				Type:       resourcemanager.StaticSegment,
+			},
+			{
+				Name: "resourceGroupName",
+				Type: resourcemanager.ResourceGroupSegment,
+			},
+			{
+				FixedValue: stringPointer("providers"),
+				Name:       "providers",
+				Type:       resourcemanager.StaticSegment,
+			},
+			{
+				FixedValue: stringPointer("Microsoft.ContainerService"),
+				Name:       "staticMicrosoftContainerService",
+				Type:       resourcemanager.ResourceProviderSegment,
+			},
+			{
+				FixedValue: stringPointer("managedClusters"),
+				Name:       "staticManagedClusters",
+				Type:       resourcemanager.StaticSegment,
+			},
+			{
+				Name: "managedClusterName",
+				Type: resourcemanager.UserSpecifiedSegment,
+			},
+			{
+				FixedValue: stringPointer("trustedAccessRoleBindings"),
+				Name:       "trustedAccessRoleBindings",
+				Type:       resourcemanager.StaticSegment,
+			},
+			{
+				Name: "trustedAccessRoleBindingName",
+				Type: resourcemanager.UserSpecifiedSegment,
+			},
+		},
+	}
+	inputMappings := resourcemanager.MappingDefinition{
+		Fields:     []resourcemanager.FieldMappingDefinition{},
+		ResourceId: []resourcemanager.ResourceIdMappingDefinition{},
+	}
+	inputResourceBuildInfo := models.ResourceBuildInfo{
+		Overrides: []models.Override{
+			{
+				Name:        "kubernetes_cluster_id",
+				UpdatedName: pointer.To("renamed_cluster_id"),
+			},
+		},
+	}
+	actualFields, actualMappings, err := Builder{
+		resourceIds: map[string]resourcemanager.ResourceIdDefinition{
+			"KubernetesClusterId": {
+				CommonAlias: pointer.To("KubernetesCluster"),
+				Id:          "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.ContainerService/managedClusters/{managedClusterName}",
+				Segments: []resourcemanager.ResourceIdSegment{
+					{
+						FixedValue: stringPointer("subscriptions"),
+						Name:       "subscriptions",
+						Type:       resourcemanager.StaticSegment,
+					},
+					{
+						Name: "subscriptionId",
+						Type: resourcemanager.SubscriptionIdSegment,
+					},
+					{
+						FixedValue: stringPointer("resourceGroups"),
+						Name:       "resourceGroups",
+						Type:       resourcemanager.StaticSegment,
+					},
+					{
+						Name: "resourceGroupName",
+						Type: resourcemanager.ResourceGroupSegment,
+					},
+					{
+						FixedValue: stringPointer("providers"),
+						Name:       "providers",
+						Type:       resourcemanager.StaticSegment,
+					},
+					{
+						FixedValue: stringPointer("Microsoft.ContainerService"),
+						Name:       "staticMicrosoftContainerService",
+						Type:       resourcemanager.ResourceProviderSegment,
+					},
+					{
+						FixedValue: stringPointer("managedClusters"),
+						Name:       "staticManagedClusters",
+						Type:       resourcemanager.StaticSegment,
+					},
+					{
+						Name: "managedClusterName",
+						Type: resourcemanager.UserSpecifiedSegment,
+					},
+				},
+			},
+		},
+	}.identifyTopLevelFieldsWithinResourceID(input, &inputMappings, "Kubernetes Cluster Trusted Access Role Binding", &inputResourceBuildInfo, hclog.New(hclog.DefaultOptions))
+	if err != nil {
+		t.Fatalf("error: %+v", err)
+	}
+
+	// first check we've mapped the field across
+	if actualFields == nil {
+		t.Fatalf("expected actualFields to be non-nil but was nil")
+	}
+	if len(*actualFields) != 2 {
+		t.Fatalf("expected actualFields to contain 2 items but got %d", len(*actualFields))
+	}
+	_, ok := (*actualFields)["ResourceGroupName"]
+	if ok {
+		t.Fatalf("expected the field `ResourceGroupName` be absent")
+	}
+	kubernetesClusterId, ok := (*actualFields)["RenamedClusterId"]
+	if !ok {
+		t.Fatalf("expected there to be a field called `RenamedClusterId`")
+	}
+	if !kubernetesClusterId.Required {
+		t.Fatalf("expected the field ResourceGroupName to be Required but it wasn't")
+	}
+	if !kubernetesClusterId.ForceNew {
+		t.Fatalf("expected the field ResourceGroupName to be ForceNew but it wasn't")
+	}
+	if kubernetesClusterId.ObjectDefinition.Type != resourcemanager.TerraformSchemaFieldTypeString {
+		t.Fatalf("expected KubernetesClusterId to be a String Field but got %q", string(kubernetesClusterId.ObjectDefinition.Type))
+	}
+	if kubernetesClusterId.Documentation.Markdown != "Specifies the Renamed Cluster Id within which this Kubernetes Cluster Trusted Access Role Binding should exist." {
+		t.Fatalf("expected the description for `RenamedClusterId` to be `Specifies the Renamed Cluster Id within which this Kubernetes Cluster Trusted Access Role Binding should exist.` but got %q", kubernetesClusterId.Documentation.Markdown)
+	}
+
+	name, ok := (*actualFields)["Name"]
+	if !ok {
+		t.Fatalf("expected there to be a field called `Name` but there wasn't")
+	}
+	if !name.Required {
+		t.Fatalf("expected the field Name to be Required but it wasn't")
+	}
+	if !name.ForceNew {
+		t.Fatalf("expected the field Name to be ForceNew but it wasn't")
+	}
+	if name.ObjectDefinition.Type != resourcemanager.TerraformSchemaFieldTypeString {
+		t.Fatalf("expected Name to be a String Field but got %q", string(name.ObjectDefinition.Type))
+	}
+	if name.Documentation.Markdown != "Specifies the name of this Kubernetes Cluster Trusted Access Role Binding." {
+		t.Fatalf("expected the description for `Name` to be `Specifies the name of this Kubernetes Cluster Trusted Access Role Binding.` but got %q", name.Documentation.Markdown)
+	}
+
+	// then check the mappings
+	if actualMappings == nil {
+		t.Fatalf("expected actualMappings to be non-nil but was nil")
+	}
+	if len(actualMappings.Fields) > 0 {
+		t.Fatalf("expected no field mappings but got %d", len(actualMappings.Fields))
+	}
+	if len(actualMappings.ResourceId) != 3 {
+		t.Fatalf("expected there to be 3 ResourceId mappings but got %d", len(actualMappings.ResourceId))
+	}
+	checkResourceIdMappingExistsBetween(t, actualMappings.ResourceId, "RenamedClusterId", "resourceGroupName", true)
+	checkResourceIdMappingExistsBetween(t, actualMappings.ResourceId, "RenamedClusterId", "managedClusterName", true)
+	checkResourceIdMappingExistsBetween(t, actualMappings.ResourceId, "Name", "trustedAccessRoleBindingName", false)
+}
+
 func TestTopLevelFieldsWithinResourceId_SchemaOverride(t *testing.T) {
 	input := resourcemanager.ResourceIdDefinition{
 		Segments: []resourcemanager.ResourceIdSegment{
