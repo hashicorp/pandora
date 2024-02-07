b2b698688c0d661bd5674b59746030ed8bef8066
tombuildsstuff <git@tom.ibuildstuff.eu>
2024-02-07 14:59:17 +0100

    `tools/data-api-sdk`: implementing the `Available Services` endpoint
    Since the models returned by this endpoint are only for use with the API and aren't useful outside
    of the SDK, I've opted to house these within the `v1` package - meaning that `models` contains the
    types intended to be (re)used across the codebase.

--- tools/data-api-sdk/v1/available_services.go
+++ tools/data-api-sdk/v1/available_services.go
@@ -2,11 +2,18 @@ package v1
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"net/http"
 )
 
 type GetAvailableServicesResponse struct {
-	// TODO
+	// HttpResponse is the raw HTTP Response
+	HttpResponse *http.Response
+
+	// Model is a map of Service Name to AvailableServiceSummary, which can be used to determine
+	// the API Versions available for this Service.
+	Model map[string]AvailableServiceSummary
 }
 
 type AvailableServiceSummary struct {
@@ -19,5 +26,29 @@ type AvailableServiceSummary struct {
 
 // GetAvailableServices returns the list of available Services within this Source Data Type.
 func (c *Client) GetAvailableServices(ctx context.Context) (*GetAvailableServicesResponse, error) {
-	return nil, fmt.Errorf("not yet implemented")
+	uri := fmt.Sprintf("%s/v1/services", c.endpoint)
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, uri, nil)
+	if err != nil {
+		return nil, fmt.Errorf("building request to the %q endpoint: %+v", uri, err)
+	}
+
+	out := GetAvailableServicesResponse{}
+	out.HttpResponse, err = c.client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("performing request to %q: %+v", uri, err)
+	}
+
+	if out.HttpResponse.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("expected a 200 OK but got %d %s for %q", out.HttpResponse.StatusCode, out.HttpResponse.Status, uri)
+	}
+
+	var response struct {
+		Services map[string]AvailableServiceSummary `json:"services"`
+	}
+	if err := json.NewDecoder(out.HttpResponse.Body).Decode(&response); err != nil {
+		return nil, err
+	}
+
+	out.Model = response.Services
+	return &out, nil
 }
