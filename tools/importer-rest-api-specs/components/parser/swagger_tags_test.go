344c1c2dda27de362e7b86fd4a7931959e8d8f86
tombuildsstuff <git@tom.ibuildstuff.eu>
2024-02-16 12:04:57 +0100

    `tools/importer-rest-api-specs`: updating the swagger tags tests to use the new assertion funcs

--- tools/importer-rest-api-specs/components/parser/swagger_tags_test.go
+++ tools/importer-rest-api-specs/components/parser/swagger_tags_test.go
@@ -3,113 +3,183 @@
 
 package parser
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/hashicorp/go-azure-helpers/lang/pointer"
+	"github.com/hashicorp/pandora/tools/importer-rest-api-specs/models"
+)
 
 func TestParsingOperationsUsingTheSameSwaggerTagInDifferentCasings(t *testing.T) {
-	result, err := ParseSwaggerFileForTesting(t, "operations_single_tag_different_casing.json")
+	actual, err := ParseSwaggerFileForTesting(t, "operations_single_tag_different_casing.json")
 	if err != nil {
 		t.Fatalf("parsing: %+v", err)
 	}
-	if result == nil {
-		t.Fatal("result was nil")
-	}
-	if len(result.Resources) != 1 {
-		t.Fatalf("expected 1 resource but got %d", len(result.Resources))
-	}
 
-	resource, ok := result.Resources["Hello"]
-	if !ok {
-		t.Fatal("the Resource 'Hello' was not found")
-	}
+	expected := models.AzureApiDefinition{
+		ServiceName: "Example",
+		ApiVersion:  "2020-01-01",
+		Resources: map[string]models.AzureApiResource{
+			"Hello": {
+				Models: map[string]models.ModelDetails{
+					"Example": {
+						Fields: map[string]models.FieldDetails{
+							"Name": {
+								JsonName: "name",
+								ObjectDefinition: &models.ObjectDefinition{
+									Type: models.ObjectDefinitionString,
+								},
+								Required: false,
+							},
+						},
+					},
+				},
+				Operations: map[string]models.OperationDetails{
 
-	// sanity checking
-	if len(resource.Constants) != 0 {
-		t.Fatalf("expected 0 constants but got %d", len(resource.Constants))
+					"First": {
+						ContentType:         "application/json",
+						ExpectedStatusCodes: []int{200},
+						Method:              "PUT",
+						OperationId:         "Hello_First",
+						RequestObject: &models.ObjectDefinition{
+							ReferenceName: pointer.To("Example"),
+							Type:          models.ObjectDefinitionReference,
+						},
+						// https://github.com/hashicorp/pandora/issues/3807
+						UriSuffix: pointer.To("/someotheruri"),
+					},
+					"PutBar": {
+						ContentType:         "application/json",
+						ExpectedStatusCodes: []int{200},
+						Method:              "PUT",
+						OperationId:         "Hello_PutBar",
+						RequestObject: &models.ObjectDefinition{
+							ReferenceName: pointer.To("Example"),
+							Type:          models.ObjectDefinitionReference,
+						},
+						UriSuffix: pointer.To("/bar"),
+					},
+					"PutFoo": {
+						ContentType:         "application/json",
+						ExpectedStatusCodes: []int{200},
+						Method:              "PUT",
+						OperationId:         "hello_PutFoo",
+						RequestObject: &models.ObjectDefinition{
+							ReferenceName: pointer.To("Example"),
+							Type:          models.ObjectDefinitionReference,
+						},
+						UriSuffix: pointer.To("/foo"),
+					},
+					"Second": {
+						ContentType:         "application/json",
+						ExpectedStatusCodes: []int{200},
+						Method:              "PATCH",
+						OperationId:         "hello_Second",
+						RequestObject: &models.ObjectDefinition{
+							ReferenceName: pointer.To("Example"),
+							Type:          models.ObjectDefinitionReference,
+						},
+						// https://github.com/hashicorp/pandora/issues/3807
+						UriSuffix: pointer.To("/someotheruri"),
+					},
+				},
+			},
+		},
 	}
-	if len(resource.Models) != 1 {
-		t.Fatalf("expected 1 model but got %d", len(resource.Models))
-	}
-	if len(resource.Operations) != 4 {
-		t.Fatalf("expected 4 Operations but got %d", len(resource.Operations))
-	}
-	if len(resource.ResourceIds) != 0 {
-		t.Fatalf("expected 0 Resource IDs but got %d", len(resource.ResourceIds))
-	}
-	expectedOperations := []string{
-		"First",
-		"PutBar",
-		"PutFoo",
-		"Second",
-	}
-	for _, expected := range expectedOperations {
-		if _, ok := resource.Operations[expected]; !ok {
-			t.Fatalf("expected there to be an operation named %q but didn't get one", expected)
-		}
-	}
+	validateParsedSwaggerResultMatches(t, expected, actual)
 }
 
 func TestParsingOperationsOnResources(t *testing.T) {
-	result, err := ParseSwaggerFileForTesting(t, "operations_on_resources.json")
+	actual, err := ParseSwaggerFileForTesting(t, "operations_on_resources.json")
 	if err != nil {
 		t.Fatalf("parsing: %+v", err)
 	}
-	if result == nil {
-		t.Fatal("result was nil")
-	}
-	if len(result.Resources) != 2 {
-		t.Fatalf("expected 2 resource but got %d", len(result.Resources))
-	}
 
-	resource, ok := result.Resources["Hello"]
-	if !ok {
-		t.Fatal("the Resource 'Hello' was not found")
+	expected := models.AzureApiDefinition{
+		ServiceName: "Example",
+		ApiVersion:  "2020-01-01",
+		Resources: map[string]models.AzureApiResource{
+			"Hello": {
+				Models: map[string]models.ModelDetails{
+					"Example": {
+						Fields: map[string]models.FieldDetails{
+							"Name": {
+								JsonName: "name",
+								ObjectDefinition: &models.ObjectDefinition{
+									Type: models.ObjectDefinitionString,
+								},
+								Required: false,
+							},
+						},
+					},
+				},
+				Operations: map[string]models.OperationDetails{
+					"First": {
+						ContentType:         "application/json",
+						ExpectedStatusCodes: []int{200},
+						Method:              "PUT",
+						OperationId:         "Hello_First",
+						RequestObject: &models.ObjectDefinition{
+							ReferenceName: pointer.To("Example"),
+							Type:          models.ObjectDefinitionReference,
+						},
+						// https://github.com/hashicorp/pandora/issues/3807
+						UriSuffix: pointer.To("/someotheruri"),
+					},
+					"PutBar": {
+						ContentType:         "application/json",
+						ExpectedStatusCodes: []int{200},
+						Method:              "PUT",
+						OperationId:         "Hello_PutBar",
+						RequestObject: &models.ObjectDefinition{
+							ReferenceName: pointer.To("Example"),
+							Type:          models.ObjectDefinitionReference,
+						},
+						UriSuffix: pointer.To("/bar"),
+					},
+					"Second": {
+						ContentType:         "application/json",
+						ExpectedStatusCodes: []int{200},
+						Method:              "PATCH",
+						OperationId:         "hello_Second",
+						RequestObject: &models.ObjectDefinition{
+							ReferenceName: pointer.To("Example"),
+							Type:          models.ObjectDefinitionReference,
+						},
+						// https://github.com/hashicorp/pandora/issues/3807
+						UriSuffix: pointer.To("/someotheruri"),
+					},
+				},
+			},
+			"HelloOperations": {
+				Models: map[string]models.ModelDetails{
+					"Example": {
+						Fields: map[string]models.FieldDetails{
+							"Name": {
+								JsonName: "name",
+								ObjectDefinition: &models.ObjectDefinition{
+									Type: models.ObjectDefinitionString,
+								},
+								Required: false,
+							},
+						},
+					},
+				},
+				Operations: map[string]models.OperationDetails{
+					"HelloRestart": {
+						ContentType:         "application/json",
+						ExpectedStatusCodes: []int{200},
+						Method:              "POST",
+						OperationId:         "Hello_Restart",
+						RequestObject: &models.ObjectDefinition{
+							ReferenceName: pointer.To("Example"),
+							Type:          models.ObjectDefinitionReference,
+						},
+						UriSuffix: pointer.To("/foo"),
+					},
+				},
+			},
+		},
 	}
-
-	// sanity checking
-	if len(resource.Constants) != 0 {
-		t.Fatalf("expected 0 constants but got %d", len(resource.Constants))
-	}
-	if len(resource.Models) != 1 {
-		t.Fatalf("expected 1 model but got %d", len(resource.Models))
-	}
-	if len(resource.Operations) != 3 {
-		t.Fatalf("expected 3 Operations but got %d", len(resource.Operations))
-	}
-	if len(resource.ResourceIds) != 0 {
-		t.Fatalf("expected 0 Resource IDs but got %d", len(resource.ResourceIds))
-	}
-	expectedOperations := []string{
-		"First",
-		"PutBar",
-		"Second",
-	}
-	for _, expected := range expectedOperations {
-		if _, ok := resource.Operations[expected]; !ok {
-			t.Fatalf("expected there to be an operation named %q but didn't get one", expected)
-		}
-	}
-
-	resourceOperation, ok := result.Resources["HelloOperations"]
-	if !ok {
-		t.Fatal("the Resource 'HelloOperations' was not found")
-	}
-
-	// sanity checking
-	if len(resourceOperation.Constants) != 0 {
-		t.Fatalf("expected 0 constants but got %d", len(resource.Constants))
-	}
-	if len(resourceOperation.Models) != 1 {
-		t.Fatalf("expected 1 model but got %d", len(resource.Models))
-	}
-	if len(resourceOperation.Operations) != 1 {
-		t.Fatalf("expected 1 Operations but got %d", len(resource.Operations))
-	}
-	if len(resourceOperation.ResourceIds) != 0 {
-		t.Fatalf("expected 0 Resource IDs but got %d", len(resource.ResourceIds))
-	}
-
-	if _, ok := resourceOperation.Operations["HelloRestart"]; !ok {
-		t.Fatalf("expected there to be an operation named `Restart` but didn't get one")
-	}
-
+	validateParsedSwaggerResultMatches(t, expected, actual)
 }
