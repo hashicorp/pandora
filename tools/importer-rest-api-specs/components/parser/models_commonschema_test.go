ce79ce0a0b7835e11a32848c075386a77dcf5653
tombuildsstuff <git@tom.ibuildstuff.eu>
2024-02-19 15:12:45 +0100

    `tools/importer-rest-api-specs`: adding tests covering `commonschema` for `identitysystemassigned`

--- tools/importer-rest-api-specs/components/parser/models_commonschema_test.go
+++ tools/importer-rest-api-specs/components/parser/models_commonschema_test.go
@@ -7,6 +7,54 @@ import (
 	"github.com/hashicorp/pandora/tools/importer-rest-api-specs/models"
 )
 
+func TestParseModel_CommonSchema_IdentitySystemAssigned(t *testing.T) {
+	actual, err := ParseSwaggerFileForTesting(t, "model_commonschema_identitysystemassigned.json")
+	if err != nil {
+		t.Fatalf("parsing: %+v", err)
+	}
+
+	expected := models.AzureApiDefinition{
+		ServiceName: "Example",
+		ApiVersion:  "2020-01-01",
+		Resources: map[string]models.AzureApiResource{
+			"Resource": {
+				Models: map[string]models.ModelDetails{
+					"Model": {
+						Fields: map[string]models.FieldDetails{
+							"Identity": {
+								JsonName:        "identity",
+								CustomFieldType: pointer.To(models.CustomFieldTypeSystemAssignedIdentity),
+								Required:        false,
+							},
+							"Name": {
+								JsonName: "name",
+								ObjectDefinition: &models.ObjectDefinition{
+									Type: models.ObjectDefinitionString,
+								},
+								Required: false,
+							},
+						},
+					},
+				},
+				Operations: map[string]models.OperationDetails{
+					"Test": {
+						ContentType:         "application/json",
+						ExpectedStatusCodes: []int{200},
+						Method:              "GET",
+						OperationId:         "Resource_Test",
+						ResponseObject: &models.ObjectDefinition{
+							ReferenceName: pointer.To("Model"),
+							Type:          models.ObjectDefinitionReference,
+						},
+						UriSuffix: pointer.To("/example"),
+					},
+				},
+			},
+		},
+	}
+	validateParsedSwaggerResultMatches(t, expected, actual)
+}
+
 func TestParseModel_CommonSchema_Location(t *testing.T) {
 	actual, err := ParseSwaggerFileForTesting(t, "model_commonschema_location.json")
 	if err != nil {
@@ -26,6 +74,13 @@ func TestParseModel_CommonSchema_Location(t *testing.T) {
 								CustomFieldType: pointer.To(models.CustomFieldTypeLocation),
 								Required:        false,
 							},
+							"Name": {
+								JsonName: "name",
+								ObjectDefinition: &models.ObjectDefinition{
+									Type: models.ObjectDefinitionString,
+								},
+								Required: false,
+							},
 						},
 					},
 				},
@@ -33,17 +88,13 @@ func TestParseModel_CommonSchema_Location(t *testing.T) {
 					"Test": {
 						ContentType:         "application/json",
 						ExpectedStatusCodes: []int{200},
-						Method:              "PUT",
+						Method:              "GET",
 						OperationId:         "Resource_Test",
-						RequestObject: &models.ObjectDefinition{
-							ReferenceName: pointer.To("Model"),
-							Type:          models.ObjectDefinitionReference,
-						},
 						ResponseObject: &models.ObjectDefinition{
 							ReferenceName: pointer.To("Model"),
 							Type:          models.ObjectDefinitionReference,
 						},
-						UriSuffix: pointer.To("/foo"),
+						UriSuffix: pointer.To("/example"),
 					},
 				},
 			},
