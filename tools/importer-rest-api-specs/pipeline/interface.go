50d4bcf825999da9ac5a952fa10a2679f9e2edc5
tombuildsstuff <git@tom.ibuildstuff.eu>
2024-02-21 13:01:50 +0100

    `tools/importer-rest-api-specs`: removing the unused `data-api-endpoint` param
    This was present but is no longer needed

--- tools/importer-rest-api-specs/pipeline/interface.go
+++ tools/importer-rest-api-specs/pipeline/interface.go
@@ -14,7 +14,6 @@ import (
 
 type RunInput struct {
 	ConfigFilePath           string
-	DataApiEndpoint          *string
 	JustOutputSegments       bool
 	JustParseData            bool
 	Logger                   hclog.Logger
