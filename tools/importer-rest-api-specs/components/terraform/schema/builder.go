3d8afa72f1f434ee81bd72e9c79a093a76e9ce70
tombuildsstuff <git@tom.ibuildstuff.eu>
2024-02-21 17:50:07 +0100

    nit: moar linting

--- tools/importer-rest-api-specs/components/terraform/schema/builder.go
+++ tools/importer-rest-api-specs/components/terraform/schema/builder.go
@@ -6,10 +6,9 @@ package schema
 import (
 	"fmt"
 
+	"github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/pandora/tools/importer-rest-api-specs/components/terraform/helpers"
 	"github.com/hashicorp/pandora/tools/importer-rest-api-specs/components/terraform/schema/processors"
-
-	"github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/pandora/tools/importer-rest-api-specs/models"
 	"github.com/hashicorp/pandora/tools/sdk/resourcemanager"
 )
