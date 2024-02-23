e026df8b92e54bab1417cbf1fcf35b401bc78d0d
tombuildsstuff <git@tom.ibuildstuff.eu>
2024-02-23 11:40:45 +0100

    tools/sdk: updating to use `models.SDKOperation` rather than `OperationDetails`

--- tools/sdk/resourcemanager/api_operations.go
+++ tools/sdk/resourcemanager/api_operations.go
@@ -6,6 +6,7 @@ package resourcemanager
 import (
 	"encoding/json"
 	"fmt"
+
 	"github.com/hashicorp/pandora/tools/data-api-sdk/v1/models"
 )
 
@@ -31,48 +32,9 @@ func (c ApiOperationsClient) Get(input ResourceSummary) (*ApiOperationDetails, e
 }
 
 type ApiOperationDetails struct {
-	// Operations is a map of key (Operation Name) to value (ApiOperation)
+	// Operations is a map of key (Operation Name) to value (SDKOperation)
 	// for the Operations supported by this API
-	Operations map[string]ApiOperation `json:"operations"`
-}
-
-type ApiOperation struct {
-	// ContentType is an optional ContentType which should be used for when making a
-	// Request to this Operation
-	ContentType *string `json:"contentType"`
-
-	// ExpectedStatusCodes is a list of Status Codes which are expected to be returned
-	// by this API Version
-	ExpectedStatusCodes []int `json:"expectedStatusCodes"`
-
-	// LongRunning specifies if this is a Long Running Operation, meaning that Clients
-	// should follow any `Location` headers to track the result of this operation
-	LongRunning bool `json:"longRunning"`
-
-	// Method is the HTTP Method used for this operation, e.g. GET, POST, PATCH
-	Method string `json:"method"`
-
-	// RequestObject defines the optional Object which must be specified in the Request
-	RequestObject *models.SDKObjectDefinition `json:"requestObject"`
-
-	// ResourceIdName is the optional name of the Resource ID type used for this
-	// operation, used either on it's own or in conjunction with the `suffix`
-	ResourceIdName *string `json:"resourceIdName"`
-
-	// ResponseObject defines the optional Object which can be returned in the Response
-	ResponseObject *models.SDKObjectDefinition `json:"responseObject"`
-
-	// FieldContainingPaginationDetails is a reference to the field within the Response
-	// which contains the pagination details, that is a 'nextLink' or similar
-	FieldContainingPaginationDetails *string `json:"fieldContainingPaginationDetails"`
-
-	// Options is a map of options which can be specified for this operation
-	// these are querystring parameters such as 'limit' or 'forceDelete' or similar
-	Options map[string]models.SDKOperationOption `json:"options"`
-
-	// UriSuffix is a suffix which should be appended to the Resource ID for this operation
-	// for example `/shutdown`
-	UriSuffix *string `json:"uriSuffix"`
+	Operations map[string]models.SDKOperation `json:"operations"`
 }
 
 type MetaData struct {
