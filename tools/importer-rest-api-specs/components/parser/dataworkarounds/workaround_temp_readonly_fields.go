089df3ebf26bdc5748745d248a7d27a5a67329ce
tombuildsstuff <git@tom.ibuildstuff.eu>
2024-02-28 09:57:21 +0100

    `tools/importer-rest-api-specs`: refactor and fixing DevCenter

--- tools/importer-rest-api-specs/components/parser/dataworkarounds/workaround_temp_readonly_fields.go
+++ tools/importer-rest-api-specs/components/parser/dataworkarounds/workaround_temp_readonly_fields.go
@@ -29,67 +29,61 @@ func (w workaroundTempReadOnlyFields) Name() string {
 
 func (w workaroundTempReadOnlyFields) Process(apiDefinition models.AzureApiDefinition) (*models.AzureApiDefinition, error) {
 	if apiDefinition.ServiceName == "DevCenter" && apiDefinition.ApiVersion == "2023-04-01" {
-		resource, ok := apiDefinition.Resources["Projects"]
-		if !ok {
-			return nil, fmt.Errorf("expected an APIResource `Projects` but didn't get one")
+		definition, err := w.markFieldAsComputed(apiDefinition, "Projects", "ProjectProperties", "DevCenterUri")
+		if err != nil {
+			return nil, err
 		}
-		model, ok := resource.Models["ProjectProperties"]
-		if !ok {
-			return nil, fmt.Errorf("expected a Model `ProjectProperties` but didn't get one")
-		}
-		devCenterUri, ok := model.Fields["DevCenterUri"]
-		if !ok {
-			return nil, fmt.Errorf("expected a Field `DevCenterUri` but didn't get one")
-		}
-		devCenterUri.Optional = false
-		devCenterUri.ReadOnly = true
-		devCenterUri.Required = false
-		model.Fields["DevCenterUri"] = devCenterUri
 
-		resource.Models["ProjectProperties"] = model
-		apiDefinition.Resources["Projects"] = resource
-		return &apiDefinition, nil
+		definition, err = w.markFieldAsComputed(*definition, "DevCenters", "DevCenterProperties", "DevCenterUri")
+		if err != nil {
+			return nil, err
+		}
+
+		return definition, nil
 	}
 
 	if apiDefinition.ServiceName == "ManagedIdentity" && apiDefinition.ApiVersion == "2023-01-31" {
-		resource, ok := apiDefinition.Resources["ManagedIdentities"]
-		if !ok {
-			return nil, fmt.Errorf("expected an APIResource `ManagedIdentities` but didn't get one")
+		definition, err := w.markFieldAsComputed(apiDefinition, "ManagedIdentities", "UserAssignedIdentityProperties", "ClientId")
+		if err != nil {
+			return nil, err
 		}
 
-		model, ok := resource.Models["UserAssignedIdentityProperties"]
-		if !ok {
-			return nil, fmt.Errorf("expected a Model `UserAssignedIdentityProperties` but didn't get one")
+		definition, err = w.markFieldAsComputed(*definition, "ManagedIdentities", "UserAssignedIdentityProperties", "PrincipalId")
+		if err != nil {
+			return nil, err
 		}
 
-		clientId, ok := model.Fields["ClientId"]
-		if !ok {
-			return nil, fmt.Errorf("expected a Field `ClientId` but didn't get one")
+		definition, err = w.markFieldAsComputed(*definition, "ManagedIdentities", "UserAssignedIdentityProperties", "TenantId")
+		if err != nil {
+			return nil, err
 		}
-		clientId.Optional = true
-		clientId.ReadOnly = true
-		model.Fields["ClientId"] = clientId
 
-		principalId, ok := model.Fields["PrincipalId"]
-		if !ok {
-			return nil, fmt.Errorf("expected a Field `PrincipalId` but didn't get one")
-		}
-		principalId.Optional = true
-		principalId.ReadOnly = true
-		model.Fields["PrincipalId"] = principalId
-
-		tenantId, ok := model.Fields["TenantId"]
-		if !ok {
-			return nil, fmt.Errorf("expected a Field `TenantId` but didn't get one")
-		}
-		tenantId.Optional = true
-		tenantId.ReadOnly = true
-		model.Fields["TenantId"] = tenantId
-
-		resource.Models["UserAssignedIdentityProperties"] = model
-		apiDefinition.Resources["ManagedIdentities"] = resource
-		return &apiDefinition, nil
+		return definition, nil
 	}
 
 	return nil, fmt.Errorf("unexpected Service %q / API Version %q", apiDefinition.ServiceName, apiDefinition.ApiVersion)
+}
+
+func (w workaroundTempReadOnlyFields) markFieldAsComputed(input models.AzureApiDefinition, apiResourceName, modelName, fieldName string) (*models.AzureApiDefinition, error) {
+	resource, ok := input.Resources[apiResourceName]
+	if !ok {
+		return nil, fmt.Errorf("expected an APIResource %q but didn't get one", apiResourceName)
+	}
+	model, ok := resource.Models[modelName]
+	if !ok {
+		return nil, fmt.Errorf("expected a Model %q but didn't get one", modelName)
+	}
+	field, ok := model.Fields[fieldName]
+	if !ok {
+		return nil, fmt.Errorf("expected a Field %q but didn't get one", fieldName)
+	}
+	field.Optional = false
+	field.ReadOnly = true
+	field.Required = false
+	field.Sensitive = false
+	model.Fields[fieldName] = field
+
+	resource.Models[modelName] = model
+	input.Resources[apiResourceName] = resource
+	return &input, nil
 }
