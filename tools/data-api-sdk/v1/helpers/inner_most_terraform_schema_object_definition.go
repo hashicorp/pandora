364019b05f0970d66e4d4f3a051722e65f7c062b
tombuildsstuff <git@tom.ibuildstuff.eu>
2024-03-01 13:16:22 +0100

    `tools/sdk`: implementing `InnerMostTerraformSchemaObjectDefinition`

---
+++ tools/data-api-sdk/v1/helpers/inner_most_terraform_schema_object_definition.go
@@ -0,0 +1,20 @@
+package helpers
+
+//
+
+import "github.com/hashicorp/pandora/tools/data-api-sdk/v1/models"
+
+// InnerMostTerraformSchemaObjectDefinition returns the inner most TerraformSchemaObjectDefinition.
+//
+// In the event of a TerraformSchemaObjectDefinition with no NestedObject, the current item will be returned.
+// In the event of a NestedObject being present, or a NestedItem having a NestedObject, this method
+// will recurse until it finds the TerraformSchemaObjectDefinition without a NestedObject.
+//
+// This is useful for obtaining the inner type for assignment purposes.
+func InnerMostTerraformSchemaObjectDefinition(input models.TerraformSchemaObjectDefinition) models.TerraformSchemaObjectDefinition {
+	if input.NestedObject != nil {
+		return InnerMostTerraformSchemaObjectDefinition(*input.NestedObject)
+	}
+
+	return input
+}
