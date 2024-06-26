01cdc2ccc894e5a939d24145a469dba1f9aa5c8a
tombuildsstuff <git@tom.ibuildstuff.eu>
2024-06-26 11:31:48 +0200

    `tools/importer-rest-api-specs`: refactoring the `testing` package to use the SDK Types
    This is another clean-cut using only the newer SDK types, the older logic will be cleaned up separately in a bit

---
+++ tools/importer-rest-api-specs/internal/components/terraform/testing/dependencies_test.go
@@ -0,0 +1,46 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package testing
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestDependencies_VirtualNetworkID(t *testing.T) {
+	dependencies := testDependencies{
+		variables: testVariables{},
+	}
+
+	ref, deps, err := DetermineDependencies("virtual_network_id", "example", &dependencies)
+	if err != nil {
+		t.Fatalf("unexpected error: %+v", err)
+	}
+	if ref == nil {
+		t.Fatalf("reference was nil")
+	}
+
+	if !strings.EqualFold(*ref, "example_virtual_network.test.id") {
+		t.Fatalf("expected %s got %s for dependency reference", "example_virtual_network.test.id", *ref)
+	}
+
+	if !deps.needsResourceGroup {
+		t.Fatalf("expected Resource Group dependency to be set")
+	}
+
+	if !deps.needsVirtualNetwork {
+		t.Fatalf("expected Virtual Network dependency to be set")
+	}
+}
+
+func TestDependencies_NonExistentID(t *testing.T) {
+	dependencies := testDependencies{
+		variables: testVariables{},
+	}
+
+	_, _, err := DetermineDependencies("non_existent_id", "example", &dependencies)
+	if err == nil {
+		t.Fatal("expected an error but got none")
+	}
+}
---
+++ tools/importer-rest-api-specs/internal/components/terraform/testing/generate.go
@@ -0,0 +1,70 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package testing
+
+import (
+	"fmt"
+
+	sdkModels "github.com/hashicorp/pandora/tools/data-api-sdk/v1/models"
+	"github.com/hashicorp/pandora/tools/sdk/config/definitions"
+)
+
+type testBuilder struct {
+	// providerPrefix is the prefix used for resources in this Terraform Provider (e.g. `azurerm`)
+	providerPrefix string
+
+	// resourceLabel is the label for this resource, without the providerPrefix (e.g. `load_test`)
+	resourceLabel string
+
+	// details is the Terraform Resource Details for this Resource
+	details sdkModels.TerraformResourceDefinition
+}
+
+func newTestBuilder(providerPrefix, resourceLabel string, details sdkModels.TerraformResourceDefinition) testBuilder {
+	return testBuilder{
+		providerPrefix: providerPrefix,
+		resourceLabel:  resourceLabel,
+		details:        details,
+	}
+}
+
+// generateTestsForResource builds a TerraformResourceTestsDefinition for the specified Terraform Resource
+func (tb testBuilder) generateTestsForResource(testData definitions.ResourceTestDataDefinition) (*sdkModels.TerraformResourceTestsDefinition, error) {
+	dependencies := testDependencies{
+		variables: testVariables{},
+	}
+	basicConfig, err := tb.generateBasicTest(&dependencies, testData)
+	if err != nil {
+		return nil, fmt.Errorf("generating basic test: %+v", err)
+	}
+	requiresImportConfig, err := tb.generateRequiresImportTest()
+	if err != nil {
+		return nil, fmt.Errorf("generating requires import test: %+v", err)
+	}
+
+	complete, err := tb.generateCompleteTest(&dependencies, testData)
+	if err != nil {
+		return nil, fmt.Errorf("generating complete test: %+v", err)
+	}
+
+	templateConfig := tb.generateTemplateConfigForDependencies(dependencies)
+	variablesConfig := generateTemplateForLocalVariables(dependencies.variables)
+	templateConfig = fmt.Sprintf("%s\n%s", variablesConfig, templateConfig)
+
+	out := sdkModels.TerraformResourceTestsDefinition{
+		BasicConfiguration:          *basicConfig,
+		RequiresImportConfiguration: *requiresImportConfig,
+		Generate:                    true,
+		OtherTests:                  &map[string][]sdkModels.TerraformTestDefinition{},
+		TemplateConfiguration:       &templateConfig,
+		// TODO: we should split variables config out into it's own property too
+	}
+
+	// Complete is an Optional test, therefore only output it if it's needed
+	if complete != nil {
+		out.CompleteConfiguration = complete
+	}
+
+	return &out, nil
+}
