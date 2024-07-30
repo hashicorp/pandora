449383dae01f6852ee05232701f62b0feb226780
Tom Bamford <tom@bamford.io>
2024-07-30 13:24:53 +0100

    importer-msgraph-metadata: blacklist more services due to inscrutable/unsupported naming

--- tools/importer-msgraph-metadata/components/blacklisted/blacklist.go
+++ tools/importer-msgraph-metadata/components/blacklisted/blacklist.go
@@ -1,12 +1,19 @@
 package blacklisted
 
 import (
-	"github.com/hashicorp/pandora/tools/importer-msgraph-metadata/components/parser"
+	"fmt"
 	"strings"
+
+	"github.com/hashicorp/pandora/tools/importer-msgraph-metadata/components/parser"
 )
 
 func Resource(resource *parser.Resource) bool {
 	if resource.Service == "Groups" {
+
+		// Repeating ID segments which are not supported
+		if resource.Name == "SiteSite" {
+			return true
+		}
 
 		// GroupSiteTermStore resources have repeating ID segments which are not supported at this time
 		if strings.Contains(resource.Name, "TermStore") {
@@ -20,11 +27,45 @@ func Resource(resource *parser.Resource) bool {
 
 	}
 
-	if resource.Service == "Users" {
+	if resource.Service == "IdentityGovernance" {
+
+		// Repeating ID segments which are not supported at this time
+		prefixes := []string{
+			"EntitlementManagementAccessPackageResourceRoleScopeRoleResourceScopeResourceRole",
+			"EntitlementManagementAccessPackageResourceRoleScopeScopeResourceRoleResourceScope",
+			"EntitlementManagementCatalogResourceRoleResourceScopeResourceRole",
+			"EntitlementManagementCatalogResourceScopeResourceRoleResourceScope",
+			"EntitlementManagementResourceRequestCatalogResourceRoleResourceScopeResource",
+			"EntitlementManagementResourceRequestCatalogResourceScopeResourceRoleResourceScope",
+			"EntitlementManagementResourceRequestResourceRoleResourceScope",
+			"EntitlementManagementResourceRequestResourceScopeResourceRole",
+			"EntitlementManagementResourceRoleScopeRoleResourceScopeResourceRole",
+			"EntitlementManagementResourceRoleScopeScopeResourceRoleResourceScope",
+		}
+		for _, prefix := range prefixes {
+			if strings.HasPrefix(resource.Name, prefix) || strings.HasPrefix(resource.Name, fmt.Sprintf("%s%s", resource.Service, prefix)) {
+				return true
+			}
+		}
+
+	}
+
+	if resource.Service == "Me" || resource.Service == "Users" {
 
 		// Onenote resources have repeating ID segments which are not supported at this time
 		if strings.Contains(resource.Name, "Onenote") {
 			return true
+		}
+
+		// Repeating ID segments which are not supported at this time
+		prefixes := []string{
+			"PendingAccessReviewInstanceDecisionInstanceStageDecision",
+			"PendingAccessReviewInstanceStageDecisionInstanceDecision",
+		}
+		for _, prefix := range prefixes {
+			if strings.HasPrefix(resource.Name, prefix) || strings.HasPrefix(resource.Name, fmt.Sprintf("%s%s", resource.Service, prefix)) {
+				return true
+			}
 		}
 
 	}
