b239bb8163b75612489890974b2851fd1833ea50
Lucia Lu <qixialu@hotmail.com>
2026-03-03 10:42:43 +1100

    Add workaround for managed devops pools identity

---
+++ tools/importer-rest-api-specs/internal/components/apidefinitions/parser/dataworkarounds/workaround_manageddevopspools_40905.go
@@ -0,0 +1,54 @@
+package dataworkarounds
+
+import (
+	"errors"
+	"fmt"
+
+	sdkModels "github.com/hashicorp/pandora/tools/data-api-sdk/v1/models"
+)
+
+var _ workaround = workaroundManagedDevopsPools40905{}
+
+type workaroundManagedDevopsPools40905 struct{}
+
+func (workaroundManagedDevopsPools40905) IsApplicable(serviceName string, apiVersion sdkModels.APIVersion) bool {
+	return serviceName == "DevOpsInfrastructure" && apiVersion.APIVersion == "2025-01-21"
+}
+
+func (workaroundManagedDevopsPools40905) Name() string {
+	return "ManagedDevopsPools / 40905"
+}
+
+func (workaroundManagedDevopsPools40905) Process(input sdkModels.APIVersion) (*sdkModels.APIVersion, error) {
+	resource, ok := input.Resources["Pools"]
+	if !ok {
+		return nil, errors.New("expected a resource named `Pools` but didn't get one")
+	}
+
+	models := []string{
+        "Pool",
+        "PoolUpdate",
+    }
+
+	for _, modelName := range models {
+        model, ok := resource.Models[modelName]
+        if !ok {
+            return nil, fmt.Errorf("couldn't find model `%s`", modelName)
+        }
+
+        identityField, ok := model.Fields["Identity"]
+        if !ok {
+            return nil, fmt.Errorf("couldn't find the field `Identity` within model `%s`", modelName)
+        }
+
+        identityField.ObjectDefinition.Type = sdkModels.UserAssignedIdentityMapSDKObjectDefinitionType
+        identityField.ObjectDefinition.ReferenceName = nil
+
+        model.Fields["Identity"] = identityField
+        resource.Models[modelName] = model
+    }
+
+	input.Resources["Pools"] = resource
+
+	return &input, nil
+}
