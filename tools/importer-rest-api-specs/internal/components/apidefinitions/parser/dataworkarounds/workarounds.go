4a8391a9b2536bd2b8be9d7e1e1b751e10c83f46
sreallymatt <106555974+sreallymatt@users.noreply.github.com>
2026-03-17 08:08:17 -0600

    Workaround for Microsoft.Web - CreateOrUpdate_Certificate operation (#5273)

--- tools/importer-rest-api-specs/internal/components/apidefinitions/parser/dataworkarounds/workarounds.go
+++ tools/importer-rest-api-specs/internal/components/apidefinitions/parser/dataworkarounds/workarounds.go
@@ -8,10 +8,12 @@ var workarounds = []workaround{
 
 	// Common workarounds
 	// https://github.com/Azure/azure-rest-api-specs/issues/22758
-	commonWorkaroundIsLRO{"RecoveryServicesBackup", []string{"2023-02-01"}, []string{"ProtectedItems"}, []string{"CreateOrUpdate", "Delete"}},
-	commonWorkaroundIsLRO{"RecoveryServicesBackup", []string{"2025-02-01"}, []string{"ProtectedItems"}, []string{"Delete"}},
+	commonWorkaroundIsLRO{"RecoveryServicesBackup", []string{"2023-02-01"}, []string{"ProtectedItems"}, []string{"CreateOrUpdate", "Delete"}, nil},
+	commonWorkaroundIsLRO{"RecoveryServicesBackup", []string{"2025-02-01"}, []string{"ProtectedItems"}, []string{"Delete"}, nil},
 	// https://github.com/Azure/azure-rest-api-specs/issues/37325
-	commonWorkaroundIsLRO{"RecoveryServicesBackup", []string{"2025-02-01"}, []string{"ProtectionContainers"}, []string{"Unregister"}},
+	commonWorkaroundIsLRO{"RecoveryServicesBackup", []string{"2025-02-01"}, []string{"ProtectionContainers"}, []string{"Unregister"}, nil},
+	// https://github.com/Azure/azure-rest-api-specs/issues/41432
+	commonWorkaroundIsLRO{"Web", []string{"2023-01-01", "2023-12-01", "2024-11-01", "2025-05-01"}, []string{"Certificates"}, []string{"CreateOrUpdate"}, []int{202}},
 
 	// Workarounds
 	workaroundAlertsManagement{},
