33f0fd3a1d7190b0f0605f2642f6c99939ed0987
jackofallops <ste@hashicorp.com>
2026-06-18 16:14:56 +0200

    update test to account for arbitrary JSONimpl

--- tools/generator-go-sdk/internal/generator/templater_models_discriminators_test.go
+++ tools/generator-go-sdk/internal/generator/templater_models_discriminators_test.go
@@ -91,9 +91,9 @@ func (s BaseModeOfTransitImpl) ModeOfTransit() BaseModeOfTransitImpl {
 
 var _ ModeOfTransit = RawModeOfTransitImpl{}
 
-// RawModeOfTransitImpl is returned when the Discriminated Value doesn't match any of the defined types
-// NOTE: this should only be used when a type isn't defined for this type of Object (as a workaround)
-// and is used only for Deserialization (e.g. this cannot be used as a Request Payload).
+// RawModeOfTransitImpl is returned when the Discriminated Value doesn't match any of the defined types.
+// It can also be used as a Request Payload to provide a raw JSON payload, which is useful
+// for preserving arbitrary/extensible JSON properties across a round-trip.
 type RawModeOfTransitImpl struct {
 	modeOfTransit BaseModeOfTransitImpl
 	Type string
@@ -102,6 +102,10 @@ type RawModeOfTransitImpl struct {
 
 func (s RawModeOfTransitImpl) ModeOfTransit() BaseModeOfTransitImpl {
 	return s.modeOfTransit
+}
+
+func (s RawModeOfTransitImpl) MarshalJSON() ([]byte, error) {
+	return json.Marshal(s.Values)
 }
 
 func UnmarshalModeOfTransitImplementation(input []byte) (ModeOfTransit, error) {
