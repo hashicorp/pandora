9c9d7e510f269b510e63eab2d5880e311400e030
Lucia Lu <qixialu@hotmail.com>
2026-06-30 11:34:23 +1000

    Keep both versions for conventions

--- tools/importer-rest-api-specs/internal/components/apidefinitions/parser/dataworkarounds/workaround_paloaltonetworks_38348.go
+++ tools/importer-rest-api-specs/internal/components/apidefinitions/parser/dataworkarounds/workaround_paloaltonetworks_38348.go
@@ -12,7 +12,7 @@ var _ workaround = workaroundPaloAltoNetworks38348{}
 type workaroundPaloAltoNetworks38348 struct{}
 
 func (workaroundPaloAltoNetworks38348) IsApplicable(serviceName string, apiVersion sdkModels.APIVersion) bool {
-	return serviceName == "PaloAltoNetworks" && apiVersion.APIVersion == "2025-10-08"
+	return serviceName == "PaloAltoNetworks" && (apiVersion.APIVersion == "2025-05-23" || apiVersion.APIVersion == "2025-10-08")
 }
 
 func (workaroundPaloAltoNetworks38348) Name() string {
@@ -20,10 +20,20 @@ func (workaroundPaloAltoNetworks38348) Name() string {
 }
 
 func (workaroundPaloAltoNetworks38348) Process(input sdkModels.APIVersion) (*sdkModels.APIVersion, error) {
-	resource, ok := input.Resources["FirewallResources"]
-	if !ok {
-		return nil, errors.New("expected a resource named `FirewallResources` but didn't get one")
+	// The resource (tag) name differs between API Versions - in `2025-05-23` it's `Firewalls`
+	// whereas in `2025-10-08` it's `FirewallResources`.
+	resourceName := ""
+	for _, candidate := range []string{"Firewalls", "FirewallResources"} {
+		if _, ok := input.Resources[candidate]; ok {
+			resourceName = candidate
+			break
+		}
 	}
+	if resourceName == "" {
+		return nil, errors.New("expected a resource named `Firewalls` or `FirewallResources` but didn't get one")
+	}
+
+	resource := input.Resources[resourceName]
 
 	models := []string{
 		"FirewallResource",
@@ -48,7 +58,7 @@ func (workaroundPaloAltoNetworks38348) Process(input sdkModels.APIVersion) (*sdk
 		resource.Models[modelName] = model
 	}
 
-	input.Resources["FirewallResources"] = resource
+	input.Resources[resourceName] = resource
 
 	return &input, nil
 }
